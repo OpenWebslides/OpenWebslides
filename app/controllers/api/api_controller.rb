2101fc91dc52b72065146f41c610dc75c7e7e11f
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-06 11:23:23 +0100

    API error handling

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,14 +2,31 @@
 module Api
   class ApiController < ApplicationController
     include DeviseTokenAuth::Concerns::SetUserByToken
+    include Pundit
 
     before_action :ensure_json_request
+    after_action :verify_authorized, :except => :index
+    after_action :verify_policy_scoped, :only => :index
+
+    rescue_from Exception, :with => :handle_error
 
     private
 
     def ensure_json_request
       return if request.format == :json
       head :not_acceptable
+    end
+
+    def handle_error(exception)
+      Rails.logger.error exception
+
+      raise exception
+    rescue Pundit::NotAuthorizedError
+      head :not_authorized
+    rescue ActiveRecord::RecordNotFound
+      head :not_found
+    rescue
+      head :internal_server_error
     end
   end
 end
