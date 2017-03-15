93762cd514ccd6fe9e5e684f72c5b2d3e700b8c5
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-15 09:38:42 +0100

    Add Provider#type

--- lib/openwebslides/provider/github.rb
+++ lib/openwebslides/provider/github.rb
@@ -1,2 +1,10 @@
 # frozen_string_literal: true
 require_relative 'github/repository'
+
+module OpenWebslides
+  module Provider
+    def self.type
+      'github'
+    end
+  end
+end
--- lib/openwebslides/provider/ssh.rb
+++ lib/openwebslides/provider/ssh.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 require_relative 'ssh/connection'
 require_relative 'ssh/repository'
+
+module OpenWebslides
+  module Provider
+    def self.type
+      'ssh'
+    end
+  end
+end
