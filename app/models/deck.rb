e8005f2a3756f70abb8feaa0104aaf9dcd7fe83b
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-15 16:18:46 +0100

    Generate unique canonical name

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -4,7 +4,7 @@ class Deck < ApplicationRecord
   # Properties
   #
   validates :name, :presence => true
-  validates :canonical_name, :uniqueness => true
+  validates :canonical_name, :presence => true, :uniqueness => true
 
   enum :state => [:public_access, :protected_access, :private_access]
   validates :state, :presence => true
@@ -17,19 +17,35 @@ class Deck < ApplicationRecord
   ##
   # Callbacks
   #
+  before_validation :generate_canonical_name
   before_create :create_repository
   before_destroy :destroy_repository
 
   ##
   # Methods
   #
+  def generate_canonical_name
+    self.canonical_name = "#{owner.email.parameterize}-#{name.parameterize}"
+    return unless self.class.exists? :canonical_name => canonical_name
+
+    i = 2
+    candidate = nil
+    loop do
+      i += 1
+      candidate = "#{canonical_name}-#{i}"
+      break unless self.class.exists? :canonical_name => candidate
+    end
+
+    self.canonical_name = candidate
+  end
+
   def create_repository
-    repo = OpenWebslides::Repository.new(self)
+    repo = OpenWebslides::Repository.new self
     repo.init
   end
 
   def destroy_repository
-    repo = OpenWebslides::Repository.new(self)
+    repo = OpenWebslides::Repository.new self
     repo.destroy
   end
 end
