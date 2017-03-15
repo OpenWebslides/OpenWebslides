e2ee5e67100e0f0cb2ab60e0a8e741fc242c0c0d
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-15 11:09:04 +0100

    Rename upstream to repository

--- db/migrate/20170308153820_add_unique_index_to_repository.rb
+++ db/migrate/20170308153820_add_unique_index_to_repository.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class AddUniqueIndexToUpstream < ActiveRecord::Migration[5.0]
+class AddUniqueIndexToRepository < ActiveRecord::Migration[5.0]
   def up
     add_index :decks, :repository, :unique => true
   end
