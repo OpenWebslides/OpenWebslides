192753389221f7837904786133ad3109b32d0d36
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-03-22 11:50:25 +0100

    Correct routes for react-router v4

---
+++ webpack/modules/sign_in/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function SignIn() {
+  return (
+    <div>
+      <h3>Sign in Form</h3>
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+      </form>
+    </div>
+  );
+}
