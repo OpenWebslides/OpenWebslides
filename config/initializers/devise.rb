34953d9dec8a9d0c3cb51268c6f1d1c151a97f82
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-22 13:48:20 +0100

    Add GitHub OAuth2 secrets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'openwebslides/configuration'
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -249,6 +251,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  oauth_config = OpenWebslides::Configuration.oauth2
+  config.omniauth :github, oauth_config.github_id, oauth_config.github_secret, :scope => 'user:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
