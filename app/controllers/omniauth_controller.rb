f450c96ea715abfa6670a432fbd94791a9c73c42
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-29 13:08:52 +0200

    Create OmniAuth account and identity when applicable

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -5,6 +5,8 @@ class OmniauthController < ApplicationController
     sync_information
     create_token
 
+    @resource.save
+
     # response.headers['X-Access-Token'] = @resource.token
     response.headers['X-Access-Token'] = 'mytoken'
     redirect_to '/'
@@ -16,34 +18,56 @@ class OmniauthController < ApplicationController
     @auth_hash ||= request.env['omniauth.auth']
   end
 
-  def retrieve_identity # rubocop:disable AbcSize
+  def retrieve_identity
     raise unless auth_hash['info']['email']
 
-    identity = Identity.where(:uid => auth_hash['uid'], :provider => auth_hash['provider']).first
+    find_or_create_user
+    find_or_create_identity
+
+    Rails.logger.info "Authenticated user #{@resource.email} with provider #{@identity.provider}"
+  end
+
+  def find_or_create_user
     @resource = User.find_by :email => auth_hash['info']['email']
 
-    if identity
-      # Existing user and identity
-    elsif @resource
-      # Existing user
-      Identity.new :user => @resource, :uid => auth_hash['uid'], :provider => auth_hash['provider']
-    else
-      # New user
-      @resource = User.new :email => auth_hash['info']['email']
+    return if @resource
 
-      set_random_password
-    end
+    # New user
+    attrs = {
+      :name => auth_hash['info']['name'],
+      :email => auth_hash['info']['email']
+    }
+    @resource = User.new attrs
+
+    set_random_password
+
+    @resource.save
+
+    Rails.logger.info "Authenticated new user #{@resource.email}"
+  end
+
+  def find_or_create_identity
+    @identity = @resource.identities.where(:uid => auth_hash['uid'], :provider => auth_hash['provider']).first
+
+    return if @identity
+
+    # New identity
+    @identity = @resource.identities.build :uid => auth_hash['uid'], :provider => auth_hash['provider']
+    @identity.save
+
+    Rails.logger.info "Authenticated user #{@resource.email} with new provider #{@identity.provider}"
   end
 
   def set_random_password
     # Set crazy password for new oauth users. this is only used to prevent access via email sign-in.
-
-    # password = SecureRandom.urlsafe_base64(nil, false)
-    # @resource.password = password
-    # @resource.password_confirmation = password
+    password = SecureRandom.urlsafe_base64(nil, false)
+    @resource.password = password
+    @resource.password_confirmation = password
   end
 
-  def sync_information; end
+  def sync_information
+    @resource.name ||= auth_hash['info']['name']
+  end
 
   def create_token; end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
                     :format => { :with => /\A[^@]+@[^@]+\z/ },
                     :uniqueness => true
 
-  has_many :identities
+  has_many :identities, :dependent => :destroy
 
-  has_many :decks
+  has_many :decks, :dependent => :destroy
 
   has_and_belongs_to_many :contributions, :class_name => 'Deck'
 end
