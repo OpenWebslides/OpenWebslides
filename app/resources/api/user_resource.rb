7c39c1bd61fac9e3616461fa521482b5297ae05a
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-29 14:45:28 +0200

    Create Identity on user creation

--- app/resources/api/user_resource.rb
+++ app/resources/api/user_resource.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
 module Api
   class UserResource < JSONAPI::Resource
-    include Pundit::Resource
+    # include Pundit::Resource
 
-    attributes :name, :email, :password, :password_confirmation
+    ##
+    # Properties
+    #
+    attributes :name, :email, :password
 
     has_many :decks
     has_many :contributions
+
+    ##
+    # Callbacks
+    #
+    after_create :create_identity
+
+    ##
+    # Methods
+    #
+    def fetchable_fields
+      super - [:password]
+    end
+
+    def create_identity
+      identity = @model.identities.build :uid => @model.email, :provider => 'email'
+      identity.save
+    end
   end
 end
