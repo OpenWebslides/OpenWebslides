6ac311705925b90ade030666aad843d920e611a4
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-29 11:59:47 +0200

    Add password_digest attribute to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
+  has_secure_password
+
   validates :name, :presence => true
   validates :email, :presence => true,
                     :format => { :with => /\A[^@]+@[^@]+\z/ },
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322082939) do
+ActiveRecord::Schema.define(version: 20170329095737) do
 
   create_table "decks", force: :cascade do |t|
     t.string   "name"
@@ -54,10 +54,11 @@ ActiveRecord::Schema.define(version: 20170322082939) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       default: "", null: false
-    t.string   "email",      default: "", null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "name",            default: "", null: false
+    t.string   "email",           default: "", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.string   "password_digest", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
