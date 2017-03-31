ac2df903f371d24d4efea94369211f56915d51e7
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-31 14:46:38 +0200

    Annotate models

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 class Identity < ApplicationRecord
+  ##
+  # Properties
+  #
   validates :uid, :presence => true, :uniqueness => { :scope => :provider }
   validates :provider, :presence => true
 
   belongs_to :user
+
+  ##
+  # Callbacks
+  #
+  ##
+  # Methods
+  #
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 class Tag < ApplicationRecord
+  ##
+  # Properties
+  #
   validates :name, :presence => true
 
   has_and_belongs_to_many :decks
+
+  ##
+  # Callbacks
+  #
+  ##
+  # Methods
+  #
 end
