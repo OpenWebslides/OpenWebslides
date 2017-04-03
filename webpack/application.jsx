7d383ba190f71dd7c321f4de6ca874c12ea5acdc
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-04-03 10:35:16 +0200

    Use react-router v3 instead of v4

--- webpack/application.jsx
+++ webpack/application.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { Router, browserHistory } from 'react-router';
 
 import routes from './routes';
 import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router history={browserHistory}>
       {routes}
-    </BrowserRouter>
+    </Router>
   </Provider>, document.getElementById('main'));
