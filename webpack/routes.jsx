83b0b5ddf06a2e6cf2cd4e931f9ff871e70d981b
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-04-13 17:55:03 +0200

    Updates routes for react-router v3

--- webpack/routes.jsx
+++ webpack/routes.jsx
@@ -1,18 +1,17 @@
-// Libraries
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
-// Components
 import Layout from './modules/layout';
 import Feed from './modules/feed';
 import Signup from './modules/signup';
 import Signin from './modules/signin';
 
-
 export default (
   <Layout>
-    <Route exact path="/" component={Feed} />
-    <Route path="/signup" component={Signup} />
-    <Route path="/signin" component={Signin} />
+    <Route path="/">
+      <IndexRoute component={Feed} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/signin" component={Signin} />
+    </Route>
   </Layout>
 );
