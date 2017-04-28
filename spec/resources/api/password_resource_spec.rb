bef0fbe0624704c4d259b2adad1f2fcd8c16b963
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-04-28 16:26:23 +0200

    Add auth resource specs

---
+++ spec/resources/api/password_resource_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::PasswordResource, :type => :resource do
+  it 'should be abstract' do
+    expect(described_class.abstract).to be true
+  end
+
+  describe 'fields' do
+    it 'should have a valid set of fields' do
+      expect(described_class.fields).to match_array %i[id reset_password_token email password]
+    end
+
+    it 'should have a valid set of creatable fields' do
+      expect(described_class.creatable_fields).to match_array %i[email]
+    end
+
+    it 'should have a valid set of updatable fields' do
+      expect(described_class.updatable_fields).to match_array %i[reset_password_token password]
+    end
+  end
+end
