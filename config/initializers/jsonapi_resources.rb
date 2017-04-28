699799a477b4c23de443f09d0692b689b8ca54b1
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-04-28 14:20:31 +0200

    Change JSON key format to camelized

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -4,6 +4,7 @@ JSONAPI.configure do |config|
   config.default_processor_klass = JSONAPI::Authorization::AuthorizingProcessor
   config.exception_class_whitelist = [Pundit::NotAuthorizedError, JWT::Auth::UnauthorizedError]
   config.default_paginator = :offset
+  config.json_key_format = :camelized_key
 
   # Add missing HTTP status codes
   JSONAPI::UNAUTHORIZED = '401'
