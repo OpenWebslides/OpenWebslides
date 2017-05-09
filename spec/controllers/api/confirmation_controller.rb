a476f5fec5addef6587b13fef9a69b4129522b29
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-05-09 14:42:06 +0200

    Add confirmation API authentication specs

---
+++ spec/controllers/api/confirmation_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::ConfirmationController do
+  let(:user) { create :user, :confirmed }
+
+  describe 'authentication' do
+    describe 'POST create' do
+      it 'allows unauthenticated requests but does not return a token' do
+        post_unauthenticated :create
+
+        expect(response.status).not_to eq 401
+        expect(token response).to be_nil
+      end
+
+      it 'allows authenticated requests but does not return a token' do
+        post_authenticated user, :create
+
+        expect(response.status).not_to eq 401
+        expect(token response).to be_nil
+      end
+    end
+  end
+end
