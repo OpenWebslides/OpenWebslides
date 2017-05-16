fd49529096ef9e88bfd486e8b02b1fea66a06a8d
Luca Dorigo <dorigoluca@gmail.com>
2017-05-16 14:07:20 +0200

    Moved the respective texts for the feed elements to the 'constants' file

--- webpack/app/constants/feedConstants.js
+++ webpack/app/constants/feedConstants.js
@@ -3,5 +3,9 @@ export const feedElementTypes = {
   DECK_UPDATED: 'Deck updated',
   ALL: 'Everything',
 };
+export const inlineFeedElementType = {
+  DECK_CREATED: 'created a new deck:',
+  DECK_UPDATED: 'updated the deck:',
+};
 
 export default feedElementTypes;
