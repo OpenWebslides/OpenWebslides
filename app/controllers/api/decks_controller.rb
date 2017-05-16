de6edc0fb40ecbb58abd2d6b9b6761e1dabaffa2
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-05-16 16:27:49 +0200

    API: stream HTML as RESTful representation of deck

--- app/controllers/api/decks_controller.rb
+++ app/controllers/api/decks_controller.rb
@@ -2,5 +2,19 @@
 
 module Api
   class DecksController < ApiController
+    MEDIA_TYPE = 'text/html'
+
+    before_action :authenticate_user,
+                  :only => %i[create update destroy create_relationship update_relationship destroy_relationship]
+
+    after_action :renew_token
+
+    def show
+      return super unless media_types_for('Accept').include? MEDIA_TYPE
+
+      deck = Deck.find params[:id]
+      raise Pundit::NotAuthorizedError unless DeckPolicy.new(current_user, deck).show?
+      send_file File.join OpenWebslides::Configuration.repository_path, deck.canonical_name, 'index.html'
+    end
   end
 end
