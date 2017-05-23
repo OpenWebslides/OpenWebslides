d1d478d33598199c6de677b6d568dafd52dc7629
Luca Dorigo <dorigoluca@gmail.com>
2017-05-23 14:49:29 +0200

    Separate deck deletion and deck creation actions

--- webpack/app/actions/deckManagementActions.js
+++ webpack/app/actions/deckManagementActions.js
@@ -1,9 +1,6 @@
 export const DECK_DELETION_REQUEST = 'DECK_DELETION_REQUEST';
 export const DECK_DELETION_REQUEST_SUCCESS = 'DECK_DELETION_REQUEST_SUCCESS';
 export const DECK_DELETION_REQUEST_FAILURE = 'DECK_DELETION_REQUEST_FAILURE';
-export const DECK_CREATION_REQUEST = 'DECK_CREATION_REQUEST';
-export const DECK_CREATION_REQUEST_SUCCESS = 'DECK_CREATION_REQUEST_SUCCESS';
-export const DECK_CREATION_REQUEST_FAILURE = 'DECK_CREATION_REQUEST_FAILURE';
 
 export function requestDeckDeletion(deckID) {
   return {
@@ -11,21 +8,10 @@ export function requestDeckDeletion(deckID) {
     meta: deckID,
   };
 }
-export function requestDeckCreation(info) {
-  return {
-    type: DECK_CREATION_REQUEST,
-    meta: info,
-  };
-}
+
 export function deckDeletionFailure(message) {
   return {
     type: DECK_DELETION_REQUEST_FAILURE,
-    payload: message,
-  };
-}
-export function deckCreationFailure(message) {
-  return {
-    type: DECK_CREATION_REQUEST_FAILURE,
     payload: message,
   };
 }
