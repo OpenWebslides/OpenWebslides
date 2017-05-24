c002c1b0068fe55f0f6fda5303e6a3d0ba0dff8e
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-05-24 13:08:25 +0200

    API: add auth API route specs

--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -1,1 +1,61 @@
 # frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'routing', :type => :routing do
+  describe 'API' do
+    describe 'authentication' do
+      it 'routes password reset endpoint' do
+        route = '/api/password'
+
+        expect(:get => route).not_to be_routable
+        expect(:patch => route).to route_to 'api/password#update'
+        expect(:put => route).to route_to 'api/password#update'
+        expect(:post => route).to route_to 'api/password#create'
+        expect(:delete => route).not_to be_routable
+      end
+
+      it 'routes token endpoint' do
+        route = '/api/token'
+
+        expect(:get => route).not_to be_routable
+        expect(:patch => route).not_to be_routable
+        expect(:put => route).not_to be_routable
+        expect(:post => route).to route_to 'api/token#create'
+        expect(:delete => route).to route_to 'api/token#destroy'
+      end
+
+      it 'routes confirmation endpoint' do
+        route = '/api/confirmation'
+
+        expect(:get => route).not_to be_routable
+        expect(:patch => route).not_to be_routable
+        expect(:put => route).not_to be_routable
+        expect(:post => route).to route_to 'api/confirmation#create'
+        expect(:delete => route).not_to be_routable
+      end
+    end
+
+    describe 'notifications' do
+      it 'routes notifications endpoint' do
+        route = '/api/notifications'
+
+        expect(:get => route).to route_to 'api/notifications#index'
+        expect(:patch => route).not_to be_routable
+        expect(:put => route).not_to be_routable
+        expect(:post => route).not_to be_routable
+        expect(:delete => route).not_to be_routable
+      end
+
+      it 'routes notification endpoint' do
+        route = '/api/notifications/foo'
+
+        expect(:get => route).to route_to 'api/notifications#show', :id => 'foo'
+        expect(:patch => route).not_to be_routable
+        expect(:put => route).not_to be_routable
+        expect(:post => route).not_to be_routable
+        expect(:delete => route).not_to be_routable
+      end
+    end
+  end
+end
