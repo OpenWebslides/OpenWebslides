17f86e5637115303d612373dbee1c6ba7d848b32
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-05-29 14:11:39 +0200

    Adds i18next and react-i18next

--- webpack/application.jsx
+++ webpack/application.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import { I18nextProvider } from 'react-i18next';
+
 import routes from './routes';
 import configureStore from './configureStore';
+import i18n from './i18n';
 
 const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      {routes}
-    </Router>
-  </Provider>,
+  <I18nextProvider i18n={i18n}>
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        {routes}
+      </Router>
+    </Provider>
+  </I18nextProvider>,
   document.getElementById('main'),
 );
