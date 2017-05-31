adc484736e0a643a389e9962de99a3aba8a32859
Dromin <contact@dromin.be>
2017-05-31 16:48:42 +0200

    Basic styles for DeckNavigator; added SlideContainer for resizeable slides (does not work in edge cases atm).

--- webpack/app/presentationals/DeckNavigator.jsx
+++ webpack/app/presentationals/DeckNavigator.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import SlideContainer from './SlideContainer';
+
 function DeckNavigator(props) {
   return (
     <div
       className={`c_deck-navigator c_deck-navigator--${props.cssIdentifier}`}
     >
       <div className="c_deck-navigator__wrapper">
-        <p>[deck navigator goes here]</p>
+        <ol className="o_list c_deck-navigator__list">
+          {[1, 2, 3].map(() => (
+            <li className="o_list__item c_deck-navigator__item">
+              <SlideContainer />
+            </li>
+          ))}
+        </ol>
+        <p className="c_deck-navigator__controls">
+          <button className="c_deck-navigator__add-button o_action o_action--add">
+            Add slide
+          </button>
+        </p>
       </div>
     </div>
   );
