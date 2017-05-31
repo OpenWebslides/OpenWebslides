b471f46b21f485324628faa3ed0f0b4120245c9a
Luca Dorigo <dorigoluca@gmail.com>
2017-05-31 15:39:04 +0200

    actually implemented api call

--- webpack/app/api/requestUserDecksCall.js
+++ webpack/app/api/requestUserDecksCall.js
@@ -2,23 +2,18 @@ import asyncFetch from 'api/helpers/asyncFetch';
 import getBaseRequestConfig from 'api/helpers/baseRequestConfig';
 
 async function requestUserDecks(userID) {
-  // const NOTIFICATIONS_API_URL =
-  //   'http://localhost:5000/api/notifications?sort=-createdAt';
-  // const PARAMETER = `&page[offset]=${offset}`;
-  //
-  // const baseRequestConfig = getBaseRequestConfig();
-  //
-  // const requestConfig = Object.assign({}, baseRequestConfig, {
-  //   method: 'GET',
-  // });
-  //
-  // const response = await asyncFetch(
-  //   NOTIFICATIONS_API_URL + PARAMETER,
-  //   requestConfig,
-  // );
-  // const responseBody = await response.json();
-  //
-  // return responseBody.data;
+  const USER_DECKS_URL = `http://localhost:5000/api/users/${userID}/decks`;
+
+  const baseRequestConfig = getBaseRequestConfig();
+
+  const requestConfig = Object.assign({}, baseRequestConfig, {
+    method: 'GET',
+  });
+
+  const response = await asyncFetch(USER_DECKS_URL, requestConfig);
+  const responseBody = await response.json();
+
+  return responseBody.data;
 }
 
 export default requestUserDecks;
