20743231eafc5c457b73b51aafe178c35001a0f8
Dromin <contact@dromin.be>
2017-06-01 15:42:57 +0200

    #103 - Updated form field components (still needs work); update SignUpForm; added basic styling for forms.

--- webpack/app/presentationals/formFields/NewPasswordField.jsx
+++ webpack/app/presentationals/formFields/NewPasswordField.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import InputField from './InputField';
+
 function checkPasswordStrength(passwordFieldValue) {
+  // #TODO add this to extraErrors
   const value = passwordFieldValue.trim();
 
   if (value.length >= 6 && value.length < 8) {
@@ -13,24 +16,35 @@ function checkPasswordStrength(passwordFieldValue) {
   return <div> Too short!</div>;
 }
 
-function passwordField({
-  input,
-  placeholder,
-  meta: { touched, active, error },
-}) {
+function NewPasswordField(props) {
+  const newProps = {
+    ...props,
+    type: 'password',
+  };
   return (
     <div>
-      <input {...input} type="password" placeholder={placeholder} />
-      {!active && touched && error && <span>{error}</span>}
-      {active && checkPasswordStrength(input.value)}
+      <InputField {...newProps} />
+      {props.meta.active && checkPasswordStrength(props.input.value)}
     </div>
   );
 }
 
-passwordField.propTypes = {
+NewPasswordField.propTypes = {
+  // type: PropTypes.string,
+  // label: PropTypes.string.isRequired,
+  // placeholder: PropTypes.string,
   input: PropTypes.objectOf(String).isRequired,
-  meta: PropTypes.objectOf(Boolean).isRequired,
-  placeholder: PropTypes.string.isRequired,
+  meta: PropTypes.shape({
+    touched: PropTypes.bool,
+    active: PropTypes.bool,
+    error: PropTypes.string,
+    extraErrors: PropTypes.arrayOf(String),
+  }).isRequired,
 };
 
-export default passwordField;
+NewPasswordField.defaultProps = {
+  type: 'text',
+  placeholder: '',
+};
+
+export default NewPasswordField;
