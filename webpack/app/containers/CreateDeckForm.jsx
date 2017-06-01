d7f54e270e3ec468421040c33add045b9f4ddbe7
Luca Dorigo <dorigoluca@gmail.com>
2017-06-01 17:09:43 +0200

    Fixed to allow actual use of userID

--- webpack/app/containers/CreateDeckForm.jsx
+++ webpack/app/containers/CreateDeckForm.jsx
@@ -9,4 +9,66 @@ import inputField from 'presentationals/formFields/InputField';
 
 // Actions
 
-import { DECK_CREATION_REQUEST } from 'actions/createDeckActions';
+import { requestDeckCreation } from 'actions/createDeckActions';
+
+function validate(values) {
+  const { title, description } = values;
+
+  const errors = {};
+
+  if (!title || title.trim() === '') {
+    errors.title = 'Title is required';
+  } else if (title.trim().length < 6) {
+    errors.title = 'Title is too short (min. 6 characters)';
+  }
+
+  if (!description || description.trim() === '') {
+    errors.description = 'Description is required';
+  }
+
+  return errors;
+}
+
+// Submit Validation
+function validateAndSubmit(values, dispatch) {
+  return new Promise((resolve, reject) => {
+    dispatch(requestDeckCreation({ values, resolve, reject }));
+  });
+}
+
+class CreateDeckForm extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(validateAndSubmit)}>
+          <Field component={inputField} name="title" placeholder="Title" />
+          <Field
+            component={inputField}
+            name="description"
+            placeholder="Description"
+          />
+          <button type="submit"> Create Deck</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+CreateDeckForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+const createDeckReduxForm = reduxForm({
+  form: 'createDeckForm',
+  validate,
+  getFormState: state => state.vendor.forms,
+  onSubmitSuccess: () => browserHistory.push('/'),
+})(CreateDeckForm);
+
+function mapStateToProps(state) {
+  return {
+    authState: state.local.auth,
+  };
+}
+
+export default connect(mapStateToProps)(createDeckReduxForm);
