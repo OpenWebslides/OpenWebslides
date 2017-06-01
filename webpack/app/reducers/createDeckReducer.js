70ece8061d036be8f3e4a3c6b83c3faafc595ef0
Luca Dorigo <dorigoluca@gmail.com>
2017-06-01 15:13:42 +0200

    added reducer for deck creation page

---
+++ webpack/app/reducers/createDeckReducer.js
@@ -0,0 +1,37 @@
+import Immutable from 'seamless-immutable';
+import {
+  DECK_CREATION_REQUEST,
+  DECK_CREATION_REQUEST_FAILURE,
+  DECK_CREATION_REQUEST_SUCCESS,
+} from 'actions/createDeckActions';
+
+const initialState = Immutable({
+  sentCreationRequest: false,
+  creationErrorMessage: '',
+  creationSucceeded: false,
+});
+
+function createDeckReducer(state = initialState, action) {
+  switch (action.type) {
+    case DECK_CREATION_REQUEST:
+      return Immutable.merge(state, {
+        sentCreationRequest: true,
+        creationErrorMessage: '',
+      });
+    case DECK_CREATION_REQUEST_FAILURE:
+      return Immutable.merge(state, {
+        sentCreationRequest: false,
+        creationErrorMessage: action.payload,
+      });
+    case DECK_CREATION_REQUEST_SUCCESS:
+      return Immutable.merge(state, {
+      sentCreationRequest: false,
+      creationErrorMessage: '',
+      creationSucceeded: true,
+    });
+    default:
+      return state;
+  }
+}
+
+export default createDeckReducer;
