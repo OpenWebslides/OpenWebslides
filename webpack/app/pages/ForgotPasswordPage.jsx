437758ed0e71d25b7c7da6e3fab1ed3d49e74f20
Dromin <contact@dromin.be>
2017-06-01 11:33:04 +0200

    #103 - Add layout divs to existing pages; still involves a lot of copy-paste and needs to be refactored later. See also #104.

--- webpack/app/pages/ForgotPasswordPage.jsx
+++ webpack/app/pages/ForgotPasswordPage.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 
+import PageHeader from 'presentationals/PageHeader';
 import ForgotPasswordForm from 'containers/ForgotPasswordForm';
 
-function RequestResetPasswordPage(props) {
+function ForgotPasswordPage(props) {
   return (
-    <div>
-      <ForgotPasswordForm {...props} />
+    <div className="l_page l_page--forgot-password">
+      <div className="l_page__wrapper">
+        <div className="l_page__item l_page__item--header">
+          <div className="l_page__item__wrapper">
+            <PageHeader />
+          </div>
+        </div>
+        <div className="l_page__item l_page__item--main">
+          <div className="l_page__item__wrapper">
+            <main className="l_main">
+              <div className="l_main__wrapper">
+                <div className="l_main__item l_main__item--forgot-password">
+                  <div className="l_main__item__wrapper">
+                    <ForgotPasswordForm {...props} />
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default RequestResetPasswordPage;
+export default ForgotPasswordPage;
--- webpack/app/pages/ResetPasswordPage.jsx
+++ webpack/app/pages/ResetPasswordPage.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import PageHeader from 'presentationals/PageHeader';
 import ResetPasswordForm from 'containers/ResetPasswordForm';
 
 function ResetPasswordPage(props) {
   return (
-    <div>
-      <ResetPasswordForm {...props} />
+    <div className="l_page l_page--reset-password">
+      <div className="l_page__wrapper">
+        <div className="l_page__item l_page__item--header">
+          <div className="l_page__item__wrapper">
+            <PageHeader />
+          </div>
+        </div>
+        <div className="l_page__item l_page__item--main">
+          <div className="l_page__item__wrapper">
+            <main className="l_main">
+              <div className="l_main__wrapper">
+                <div className="l_main__item l_main__item--reset-password">
+                  <div className="l_main__item__wrapper">
+                    <ResetPasswordForm {...props} />
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
