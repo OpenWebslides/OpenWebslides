6cd88154d6b542a284b1411ee996666d98b0b8c6
Luca Dorigo <dorigoluca@gmail.com>
2017-06-01 15:14:30 +0200

    Added page content for deck creation page

---
+++ webpack/app/pages/CreateDeckPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CreateDeckContainer from 'containers/CreateDeckContainer';
+
+function feedPage() {
+  return (
+    <div>
+      <CreateDeckContainer />
+    </div>
+  );
+}
+
+export default feedPage;
