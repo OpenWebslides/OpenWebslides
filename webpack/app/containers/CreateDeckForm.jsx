016e7d182dd9a57d93991fcd040cba0946ee38bc
Luca Dorigo <dorigoluca@gmail.com>
2017-06-06 14:10:01 +0200

    Displays a warning when trying to create deck when not connected

--- webpack/app/containers/CreateDeckForm.jsx
+++ webpack/app/containers/CreateDeckForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { browserHistory } from 'react-router';
+import { browserHistory, Link } from 'react-router';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 
@@ -38,25 +38,47 @@ function validateAndSubmit(values, dispatch) {
 
 class CreateDeckForm extends Component {
   render() {
-    return (
-      <div>
-        <form onSubmit={this.props.handleSubmit(validateAndSubmit)}>
-          <Field component={inputField} name="title" placeholder="Title" />
-          <Field
-            component={inputField}
-            name="description"
-            placeholder="Description"
-          />
-          <button type="submit"> Create Deck</button>
-        </form>
-      </div>
-    );
+    let toDisplay;
+
+    if (this.props.authState.id) {
+      toDisplay = (
+        <div>
+          <form onSubmit={this.props.handleSubmit(validateAndSubmit)}>
+            <Field component={inputField} name="title" placeholder="Title" />
+            <Field
+              component={inputField}
+              name="description"
+              placeholder="Description"
+            />
+            <button type="submit"> Create Deck</button>
+          </form>
+        </div>
+      );
+    } else {
+      // If The user is not signed-in, show a link to the signin page:
+
+      toDisplay = (
+        <div>
+          <p> You need to be signed in to create decks! </p>
+          <Link to={'/signin'}> Sign in here </Link>
+        </div>
+      );
+    }
+
+    return toDisplay;
   }
 }
 
 CreateDeckForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
+  authState: PropTypes.shape({
+    id: PropTypes.number,
+  }),
 };
+
+CreateDeckForm.defaultProps = {
+  authState: null,
+}
 
 const createDeckReduxForm = reduxForm({
   form: 'createDeckForm',
