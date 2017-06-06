4dd05c6abc165d0ec4dcfade01ffbb5c3e034d68
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-06 14:35:26 +0200

    Remove redundant error classes

--- lib/openwebslides/error.rb
+++ lib/openwebslides/error.rb
@@ -3,20 +3,7 @@
 module OpenWebslides
   class Error < StandardError; end
 
-  ##
-  # Generic errors
-  #
-  class NotImplementedError < Error; end
-
-  ##
-  # Core errors
-  #
   class CommandError < Error; end
   class ConfigurationError < Error; end
   class RepoExistsError < Error; end
-
-  ##
-  # Unconfirmed account
-  #
-  class UnconfirmedError < Error; end
 end
