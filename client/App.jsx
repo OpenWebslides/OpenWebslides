74b58488377824459c363057c0ce9e982ed59664
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-08 11:18:45 +0200

    Optimizes Hot Module Replacement

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-import { AppContainer } from 'react-hot-loader';
-
 import { I18nextProvider } from 'react-i18next';
 
 import routes from './routes';
@@ -13,7 +10,7 @@ import './assets/stylesheets/scss/application.scss';
 
 const store = configureStore();
 
-function App() {
+export default function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
@@ -23,21 +20,4 @@ function App() {
       </Provider>
     </I18nextProvider>
   );
-}
-
-function render(Component) {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('react-container'),
-  );
-}
-
-render(App);
-
-if (module.hot) {
-  module.hot.accept(App, () => {
-    render(App);
-  });
 }
