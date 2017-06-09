f22ca7a7fb766bef4148c1aec3beffffdd2f4323
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-09 14:14:51 +0200

    Remove lib/openwebslides repository logic

--- lib/openwebslides.rb
+++ lib/openwebslides.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 
 require 'openwebslides/configuration'
-require 'openwebslides/error'
-require 'openwebslides/repository'
-
-require 'openwebslides/provider/ssh'
-require 'openwebslides/provider/github'
