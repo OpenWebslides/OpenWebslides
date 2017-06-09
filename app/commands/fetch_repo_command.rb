01ba39d09172638f0bd305550981ff51d6fe9bc0
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-09 13:54:40 +0200

    Remove old commands

--- app/commands/fetch_repo_command.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class FetchRepoCommand < RepoCommand
-  def execute
-    doc = Nokogiri::HTML5 File.read content_file
-    doc.at('body').children.to_html.strip
-  end
-end
