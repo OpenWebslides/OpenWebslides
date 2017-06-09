63bc9b3b2f4d2a951975eed74142c04a71e4fb44
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-09 14:14:51 +0200

    Remove lib/openwebslides repository logic

---
+++ app/errors/openwebslides/configuration_error.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OpenWebslides
+  class ConfigurationError < Error; end
+end
--- lib/openwebslides/error.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module OpenWebslides
-  class Error < StandardError; end
-
-  class CommandError < Error; end
-  class ConfigurationError < Error; end
-  class RepoExistsError < Error; end
-end
