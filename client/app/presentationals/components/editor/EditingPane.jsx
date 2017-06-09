ddd01662a1503515367a75256fb82ef9ea99805b
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-09 11:04:43 +0200

    Fixes naming

--- client/app/presentationals/components/editor/EditingPane.jsx
+++ client/app/presentationals/components/editor/EditingPane.jsx
@@ -1,40 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
-import SlideMode from './SlideMode';
-// import EditorContentView from './EditorContentView';
-import ModeMenu from './ModeMenu';
+import EditorViewsPanel from './EditorViewsPanel';
+import parseDeckObject from '../../lib/parseDeckObject';
 
-function ViewPane(props) {
-  return (
-    <div
-      className={`c_editor-views-panel c_editor-views-panel--${props.cssIdentifier}`}
-    >
-      <div className="c_editor-views-panel__wrapper">
-        <div className="c_editor-views-panel__switcher">
-          <ModeMenu />
+class EditingPane extends Component {
+  componentDidMount() {
+    this.activeSlide = 'hm?';
+  }
+
+  shouldComponentUpdate(nextProps) {
+    if (this.selectedSlideId === nextProps.activeDeck.slides.selectedSlide) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    const { activeDeck } = this.props;
+    if (!_.isEmpty(activeDeck)) {
+      const selectedSlideId = this.props.activeDeck.slides.selectedSlide;
+      const editorState = parseDeckObject(this.props.activeDeck);
+      const activeSlide = editorState.filter(
+        slide => slide.props.id === selectedSlideId,
+      );
+      return (
+        <div
+          className={`c_slide-editor c_slide-editor--${this.props
+            .cssIdentifier}`}
+        >
+          <div className="c_slide-editor__wrapper">
+            {/* <div className="c_slide-editor__item c_slide-editor__item--toolbar">
+          <p>[Toolbar goes here]</p>
+        </div> */}
+            <div className="c_slide-editor__item c_slide-editor__item--views-panel">
+              <EditorViewsPanel activeSlide={activeSlide} />
+            </div>
+            {/* #TODO
+        <div className="c_slide-editor__item c_slide-editor__item--slide-health">
+          <p>[Slide health goes here]</p>
+        </div> */}
+          </div>
         </div>
-        <div className="c_editor-views-panel__views-list">
-          <div className="c_editor-views-panel__views-item">
-            <SlideMode>{props.activeSlide}</SlideMode>
+      );
+    }
+    return (
+      <div
+        className={`c_slide-editor c_slide-editor--${this.props.cssIdentifier}`}
+      >
+        <div className="c_slide-editor__wrapper">
+          {/* <div className="c_slide-editor__item c_slide-editor__item--toolbar">
+          <p>[Toolbar goes here]</p>
+        </div> */}
+          <div className="c_slide-editor__item c_slide-editor__item--views-panel">
+            <EditorViewsPanel />
           </div>
           {/* #TODO
-          <div className="c_editor-views-panel__views-item">
-            <EditorContentView />
-          </div> */}
+        <div className="c_slide-editor__item c_slide-editor__item--slide-health">
+          <p>[Slide health goes here]</p>
+        </div> */}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-ViewPane.propTypes = {
+EditingPane.propTypes = {
   cssIdentifier: PropTypes.string,
-  activeSlide: PropTypes.arrayOf(PropTypes.element).isRequired,
+  activeDeck: PropTypes.objectOf(Object).isRequired,
 };
 
-ViewPane.defaultProps = {
+EditingPane.defaultProps = {
   cssIdentifier: 'default',
 };
 
-export default ViewPane;
+function mapStateToProps(state) {
+  return {
+    activeDeck: state.entities.decks,
+  };
+}
+
+export default connect(mapStateToProps)(EditingPane);
