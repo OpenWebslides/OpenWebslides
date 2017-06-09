e2c955f2fa1dfe242591326539a6c1d8006f6707
Luca Dorigo <dorigoluca@gmail.com>
2017-06-09 14:17:19 +0200

    fixed url for deck deletion

--- client/app/api/deleteDeckApi.js
+++ client/app/api/deleteDeckApi.js
@@ -1,7 +1,7 @@
 import asyncFetch from 'api/helpers/asyncFetch';
 import getBaseRequestConfig from 'api/helpers/baseRequestConfig';
 
-export const DECK_DELETION_URL = 'http://localhost:5000/api/decks';
+export const DECK_DELETION_URL = 'http://localhost:5000/api/decks/';
 
 async function deleteDeck(deckId, token) {
   const baseRequestConfig = getBaseRequestConfig();
@@ -13,11 +13,8 @@ async function deleteDeck(deckId, token) {
       Accept: 'application/vnd.api+json',
       'Content-Type': 'application/vnd.api+json',
     },
-    body: JSON.stringify({
-      id: deckId,
-    }),
   });
-  return asyncFetch(DECK_DELETION_URL, requestConfig);
+  return asyncFetch(DECK_DELETION_URL + deckId, requestConfig);
 }
 
 export default deleteDeck;
