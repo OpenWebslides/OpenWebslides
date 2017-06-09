fb68ac5a1ce060df60cad96981d1c40ec687d25f
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-09 10:40:02 +0200

    First refactoring of presentationals

--- client/app/containers/EmailSigninForm.jsx
+++ client/app/containers/EmailSigninForm.jsx
@@ -7,7 +7,7 @@ import i18n from 'i18next';
 import { translate } from 'react-i18next';
 
 // Fields
-import InputField from 'presentationals/formFields/InputField';
+import InputField from 'presentationals/form-fields/InputField';
 
 // Actions
 import { emailSigninUser } from 'actions/signinActions';
