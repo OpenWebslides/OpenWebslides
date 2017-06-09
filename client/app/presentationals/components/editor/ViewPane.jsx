f139e81213dfdbe3758d8e5aa5809ad59c067078
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-09 11:04:43 +0200

    Fixes naming

---
+++ client/app/presentationals/components/editor/ViewPane.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SlideMode from './SlideMode';
+// import EditorContentView from './EditorContentView';
+import ModeMenu from './ModeMenu';
+
+function ViewPane(props) {
+  return (
+    <div
+      className={`c_editor-views-panel c_editor-views-panel--${props.cssIdentifier}`}
+    >
+      <div className="c_editor-views-panel__wrapper">
+        <div className="c_editor-views-panel__switcher">
+          <ModeMenu />
+        </div>
+        <div className="c_editor-views-panel__views-list">
+          <div className="c_editor-views-panel__views-item">
+            <SlideMode>{props.activeSlide}</SlideMode>
+          </div>
+          {/* #TODO
+          <div className="c_editor-views-panel__views-item">
+            <EditorContentView />
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ViewPane.propTypes = {
+  cssIdentifier: PropTypes.string,
+  activeSlide: PropTypes.arrayOf(PropTypes.element).isRequired,
+};
+
+ViewPane.defaultProps = {
+  cssIdentifier: 'default',
+};
+
+export default ViewPane;
