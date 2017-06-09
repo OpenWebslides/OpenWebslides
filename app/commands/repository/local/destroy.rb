2a3770a5522de3bbcaf0119852ff80b0ce4f6fa3
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-09 14:13:01 +0200

    Add repo destroy commands

---
+++ app/commands/repository/local/destroy.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Repository
+  module Local
+    ##
+    # Destroy repository directory
+    #
+    class Destroy < Command
+      def execute
+        FileUtils.rm_r repo_path, :secure => true
+      end
+
+      private
+
+      def repo_path
+        File.join OpenWebslides::Configuration.repository_path, @receiver.canonical_name
+      end
+    end
+  end
+end
