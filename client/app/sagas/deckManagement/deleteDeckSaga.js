7de198cb672be20ab9604ea670f85a3c52705634
Luca Dorigo <dorigoluca@gmail.com>
2017-06-09 14:17:51 +0200

    working saga

---
+++ client/app/sagas/deckManagement/deleteDeckSaga.js
@@ -0,0 +1,27 @@
+import { takeLatest, call, put, select } from 'redux-saga/effects';
+
+import {
+  DECK_DELETION_REQUEST,
+  DECK_DELETION_REQUEST_FAILURE,
+  DECK_DELETION_REQUEST_SUCCESS,
+} from 'actions/deckManagementActions';
+import deleteDeckApi from 'api/deleteDeckApi';
+
+const authState = state => state.local.auth;
+
+export function* deleteDeckFlow(action) {
+  try {
+    const { authToken } = yield select(authState);
+    const deckId = action.meta;
+    yield call(deleteDeckApi, deckId, authToken);
+    yield put({ type: DECK_DELETION_REQUEST_SUCCESS });
+  } catch (error) {
+    yield put({ type: DECK_DELETION_REQUEST_FAILURE, payload: error.message });
+  }
+}
+
+function* deleteDeckWatcher() {
+  yield takeLatest(DECK_DELETION_REQUEST, deleteDeckFlow);
+}
+
+export default deleteDeckWatcher;
