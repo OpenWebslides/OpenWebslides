a31ca58cd6df17a4e9cc9e046e25025be9d6f14c
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-12 14:59:52 +0200

    Splits ConfirmEmail presentational and container

--- client/app/containers/ConfirmEmailContainer.jsx
+++ client/app/containers/ConfirmEmailContainer.jsx
@@ -1,46 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import queryString from 'query-string';
 
-// Actions
 import { confirmEmail } from 'actions/confirmEmailActions';
 
-// Presentationals
-import SuccessMessage from 'presentationals/components/email-confirmation/SuccessMessage';
-import FailureMessage from 'presentationals/components/email-confirmation/FailureMessage';
-import WaitingMessage from 'presentationals/components/email-confirmation/WaitingMessage';
-
-class EmailConfirmation extends Component {
-  componentDidMount() {
-    const confirmationToken = queryString.parse(this.props.location.search)
-      .confirmation_token;
-    this.props.confirmEmail(confirmationToken);
-  }
-
-  render() {
-    const { emailConfirmed } = this.props;
-
-    switch (emailConfirmed) {
-      case 'success': {
-        return <SuccessMessage />;
-      }
-      case 'failed': {
-        return <FailureMessage />;
-      }
-      default: {
-        return <WaitingMessage />;
-      }
-    }
-  }
-}
-
-EmailConfirmation.propTypes = {
-  location: PropTypes.objectOf(Object).isRequired,
-  confirmEmail: PropTypes.func.isRequired,
-  emailConfirmed: PropTypes.string.isRequired,
-};
+import ConfirmEmail from 'presentationals/components/email-confirmation/ConfirmEmail';
 
 function mapStateToProps(state) {
   return {
@@ -52,4 +15,4 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({ confirmEmail }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(EmailConfirmation);
+export default connect(mapStateToProps, mapDispatchToProps)(ConfirmEmail);
