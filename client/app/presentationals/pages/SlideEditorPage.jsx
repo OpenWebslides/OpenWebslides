6b655de78f1f6e4b57066ec966bd98b48b57e01a
Dromin <contact@dromin.be>
2017-06-12 15:39:46 +0200

    Updates SlideEditorPage to use DefaultLayout. See #104.

--- client/app/presentationals/pages/SlideEditorPage.jsx
+++ client/app/presentationals/pages/SlideEditorPage.jsx
@@ -1,38 +1,18 @@
 import React from 'react';
 
-import PageHeader from 'presentationals/components/PageHeader';
+import DefaultLayout from 'presentationals/layouts/DefaultLayout';
 import DeckNavigator from 'presentationals/components/editor/NavigationPane';
 import SlideEditor from 'presentationals/components/editor/EditingPane';
 
 function SlideEditorPage() {
   return (
-    <div className="l_page l_page--slide-editor">
-      <div className="l_page__wrapper">
-        <div className="l_page__item l_page__item--header">
-          <div className="l_page__item__wrapper">
-            <PageHeader />
-          </div>
-        </div>
-        <div className="l_page__item l_page__item--main">
-          <div className="l_page__item__wrapper">
-            <main className="l_main">
-              <div className="l_main__wrapper">
-                <div className="l_main__item l_main__item--deck-navigator">
-                  <div className="l_main__item__wrapper">
-                    <DeckNavigator />
-                  </div>
-                </div>
-                <div className="l_main__item l_main__item--slide-editor">
-                  <div className="l_main__item__wrapper">
-                    <SlideEditor />
-                  </div>
-                </div>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    </div>
+    <DefaultLayout
+      cssIdentifier="slide-editor"
+      components={{
+        'deck-navigator': DeckNavigator,
+        'slide-editor': SlideEditor,
+      }}
+    />
   );
 }
 
