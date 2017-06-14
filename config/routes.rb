9ae14975b5426f4c0378e7f34150d745edacde7d
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-14 14:41:58 +0200

    Add Asset routing

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     # Users and decks
     jsonapi_resources :users
     jsonapi_resources :decks
+    jsonapi_resources :assets, :except => :index
 
     # Social feed
     jsonapi_resources :notifications, :only => %i[index show] do end
