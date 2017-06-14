889d184a3e72e94cbf54f81610e5a6a43d593cd7
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-14 13:45:37 +0200

    Add Asset resource

--- app/resources/api/deck_resource.rb
+++ app/resources/api/deck_resource.rb
@@ -9,6 +9,7 @@ module Api
 
     has_one :owner, :foreign_key => 'user_id'
     has_many :collaborators
+    has_many :assets
 
     ##
     # Filters
@@ -23,11 +24,11 @@ module Api
     # Methods
     #
     def self.creatable_fields(context = {})
-      super(context) - %i[collaborators]
+      super(context) - %i[collaborators assets]
     end
 
     def self.updatable_fields(context = {})
-      super(context) - %i[collaborators]
+      super(context) - %i[collaborators assets]
     end
   end
 end
