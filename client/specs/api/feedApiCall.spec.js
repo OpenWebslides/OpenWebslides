96508a91ef37e53e1c0fb23ea0641f5a6de48b9f
Luca Dorigo <dorigoluca@gmail.com>
2017-06-14 14:45:54 +0200

    Added tests for the feed api call

---
+++ client/specs/api/feedApiCall.spec.js
@@ -0,0 +1,28 @@
+import faker from 'faker';
+
+import asyncFetch from '../../app/api/helpers/asyncFetch';
+
+import requestFeedNotifications from '../../app/api/feedApiCall';
+
+jest.mock('api/helpers/asyncFetch');
+
+describe('get feed notifications Api Call', () => {
+  it('has a happy path', async () => {
+    asyncFetch.mockReturnValue({
+      status: 200,
+      json: () => ({ data: 'sample' }),
+    });
+    const response = await requestFeedNotifications(234);
+
+    expect(response).toEqual('sample');
+
+    const calledUrl = asyncFetch.mock.calls[0][0];
+
+    expect(calledUrl).toEqual(
+      'http://localhost:5000/api/notifications?sort=-createdAt&page[offset]=234',
+    );
+
+    const requestConfig = asyncFetch.mock.calls[0][1];
+    expect(requestConfig.method).toEqual('GET');
+  });
+});
