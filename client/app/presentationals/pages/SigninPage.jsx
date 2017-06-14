a63803d2ecc188ea832fa7d71eb363c69244559b
Dromin <contact@dromin.be>
2017-06-14 15:22:28 +0200

    Updated other pages to use DefaultLayout. Fixes #104.

--- client/app/presentationals/pages/SigninPage.jsx
+++ client/app/presentationals/pages/SigninPage.jsx
@@ -2,35 +2,23 @@ import React from 'react';
 import { translate } from 'react-i18next';
 import PropTypes from 'prop-types';
 
-import PageHeader from 'presentationals/components/PageHeader';
+import DefaultLayout from 'presentationals/layouts/DefaultLayout';
 import EmailSigninForm from 'containers/EmailSigninContainer';
 import OauthLinks from 'presentationals/components/oauth/OauthLinks';
 
 function SigninPage() {
   return (
-    <div className="l_page l_page--signin">
-      <div className="l_page__wrapper">
-        <div className="l_page__item l_page__item--header">
-          <div className="l_page__item__wrapper">
-            <PageHeader />
+    <DefaultLayout
+      cssIdentifier="signin"
+      components={{
+        signin: (
+          <div>
+            <EmailSigninForm />
+            <OauthLinks />
           </div>
-        </div>
-        <div className="l_page__item l_page__item--main">
-          <div className="l_page__item__wrapper">
-            <main className="l_main">
-              <div className="l_main__wrapper">
-                <div className="l_main__item l_main__item--signin">
-                  <div className="l_main__item__wrapper">
-                    <EmailSigninForm />
-                    <OauthLinks />
-                  </div>
-                </div>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    </div>
+        ),
+      }}
+    />
   );
 }
 
