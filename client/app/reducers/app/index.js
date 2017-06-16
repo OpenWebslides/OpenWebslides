9a7dd24becfbaf9cfb5443b31f484fb6ee48a7f1
Luca Dorigo <dorigoluca@gmail.com>
2017-06-16 14:57:49 +0200

    Some more fixes after rebasing

--- client/app/reducers/app/index.js
+++ client/app/reducers/app/index.js
@@ -4,12 +4,14 @@ import authentication from 'reducers/app/authenticationReducer';
 import confirmEmail from 'reducers/app/confirmEmailReducer';
 import editor from 'reducers/app/editorReducer';
 import feed from 'reducers/app/feedReducer';
+import deckManagement from 'reducers/app/deckManagementReducer';
 
 const app = combineReducers({
   authentication,
   confirmEmail,
   editor,
   feed,
+  deckManagement,
 });
 
 export default app;
