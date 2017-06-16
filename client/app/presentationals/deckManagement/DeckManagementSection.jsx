ea31b4c2757107a1dad0cc67e78854bedadc07a2
Luca Dorigo <dorigoluca@gmail.com>
2017-06-16 15:19:43 +0200

    changed react-router to react-router-dom

--- client/app/presentationals/deckManagement/DeckManagementSection.jsx
+++ client/app/presentationals/deckManagement/DeckManagementSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 // Presentationals:
 import NeedSigninWarning from 'presentationals/objects/NeedSigninWarning';
