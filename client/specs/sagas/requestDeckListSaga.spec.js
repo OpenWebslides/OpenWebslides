58b62c6b8ecf34418efa7461ba4bd84e63298aef
Luca Dorigo <dorigoluca@gmail.com>
2017-06-16 12:54:10 +0200

    Added stub for test of requestDeckListSaga

---
+++ client/specs/sagas/requestDeckListSaga.spec.js
@@ -0,0 +1,89 @@
+import { call, put } from 'redux-saga/effects';
+import faker from 'faker';
+
+import {
+  REQUEST_DECK_LIST,
+  REQUEST_DECK_LIST_SUCCESS,
+  REQUEST_DECK_LIST_FAILURE,
+} from 'actions/deckManagementActions';
+
+import requestUserDecksCall from 'api/requestUserDecksCall';
+
+import { requestDeckListFlow } from 'sagas/deckManagement/requestDeckListSaga';
+
+function fakeResponseElement() {
+  const id = faker.random.number();
+  return {
+    data: [
+      {
+        id,
+        type: 'decks',
+        links: {
+          self: `http://localhost:5000/api/decks/${id}`,
+        },
+        attributes: {
+          name: faker.lorem.sentence(),
+          state: 'public_access',
+          description: faker.lorem.sentence(),
+          template: 'shower-ows',
+        },
+        relationships: {
+          owner: {
+            links: {
+              self: `http://localhost:5000/api/decks/${id}/relationships/owner`,
+              related: `http://localhost:5000/api/decks/${id}/owner`,
+            },
+          },
+          collaborators: {
+            links: {
+              self: `http://localhost:5000/api/decks/${id}/relationships/collaborators`,
+              related: `http://localhost:5000/api/decks/${id}/collaborators`,
+            },
+          },
+        },
+      },
+    ],
+  };
+}
+function fakeResponseList(amount) {
+  let res = [];
+  for (let i = 0; i < amount; i += 1) {
+    res = res.concat(fakeResponseElement());
+  }
+  return res;
+}
+
+describe('Request for deck list Saga', () => {
+  describe('Request list of own decks flow', () => {
+    const dummyError = { message: 'blablabla' };
+    const dummyResponse = fakeResponseList(12);
+
+    it('has a happy path', () => {
+      const generator = requestDeckListFlow({
+        meta: 12,
+      });
+
+      expect(generator.next().value).toEqual(call(requestUserDecksCall, 12));
+
+      const resultListOfDecks = [];
+
+      for (let i = 0; i < 12; i += 1) {
+        resultListOfDecks.push({
+          id: dummyResponse[i].data[0].id,
+          name: dummyResponse[i].data[0].attributes.name,
+          description: dummyResponse[i].data[0].attributes.description,
+        });
+      }
+
+      expect(generator.next(dummyResponse).value).toEqual(
+        put({
+          type: REQUEST_DECK_LIST_SUCCESS,
+          payload: {
+            listOfDecks: resultListOfDecks,
+          },
+        }),
+      );
+
+    });
+  });
+});
