97a86d71f1ba1a574ee62a900a805716696d0a62
Luca Dorigo <dorigoluca@gmail.com>
2017-06-20 15:17:28 +0200

    Tests for requestDeckListSaga are now working correctly

--- client/specs/sagas/requestDeckListSaga.spec.js
+++ client/specs/sagas/requestDeckListSaga.spec.js
@@ -2,7 +2,6 @@ import { call, put } from 'redux-saga/effects';
 import faker from 'faker';
 
 import {
-  REQUEST_DECK_LIST,
   REQUEST_DECK_LIST_SUCCESS,
   REQUEST_DECK_LIST_FAILURE,
 } from 'actions/deckManagementActions';
@@ -14,35 +13,31 @@ import { requestDeckListFlow } from 'sagas/deckManagement/requestDeckListSaga';
 function fakeResponseElement() {
   const id = faker.random.number();
   return {
-    data: [
-      {
-        id,
-        type: 'decks',
+    id,
+    type: 'decks',
+    links: {
+      self: `http://localhost:3000/api/decks/${id}`,
+    },
+    attributes: {
+      name: faker.lorem.sentence(),
+      state: 'public_access',
+      description: faker.lorem.sentence(),
+      template: 'shower-ows',
+    },
+    relationships: {
+      owner: {
         links: {
-          self: `http://localhost:3000/api/decks/${id}`,
-        },
-        attributes: {
-          name: faker.lorem.sentence(),
-          state: 'public_access',
-          description: faker.lorem.sentence(),
-          template: 'shower-ows',
-        },
-        relationships: {
-          owner: {
-            links: {
-              self: `http://localhost:3000/api/decks/${id}/relationships/owner`,
-              related: `http://localhost:3000/api/decks/${id}/owner`,
-            },
-          },
-          collaborators: {
-            links: {
-              self: `http://localhost:3000/api/decks/${id}/relationships/collaborators`,
-              related: `http://localhost:3000/api/decks/${id}/collaborators`,
-            },
-          },
+          self: `http://localhost:3000/api/decks/${id}/relationships/owner`,
+          related: `http://localhost:3000/api/decks/${id}/owner`,
         },
       },
-    ],
+      collaborators: {
+        links: {
+          self: `http://localhost:3000/api/decks/${id}/relationships/collaborators`,
+          related: `http://localhost:3000/api/decks/${id}/collaborators`,
+        },
+      },
+    },
   };
 }
 function fakeResponseList(amount) {
@@ -60,21 +55,21 @@ describe('Request for deck list Saga', () => {
 
     it('has a happy path', () => {
       const generator = requestDeckListFlow({
-        meta: 12,
+        meta: 'someID',
       });
-
-      expect(generator.next().value).toEqual(call(requestUserDecksCall, 12));
+      expect(generator.next().value).toEqual(
+        call(requestUserDecksCall, 'someID'),
+      );
 
       const resultListOfDecks = [];
 
       for (let i = 0; i < 12; i += 1) {
         resultListOfDecks.push({
-          id: dummyResponse[i].data[0].id,
-          name: dummyResponse[i].data[0].attributes.name,
-          description: dummyResponse[i].data[0].attributes.description,
+          id: dummyResponse[i].id,
+          name: dummyResponse[i].attributes.name,
+          description: dummyResponse[i].attributes.description,
         });
       }
-
       expect(generator.next(dummyResponse).value).toEqual(
         put({
           type: REQUEST_DECK_LIST_SUCCESS,
@@ -84,6 +79,43 @@ describe('Request for deck list Saga', () => {
         }),
       );
 
+      expect(generator.next().done).toBeTruthy();
+    });
+    it('throws an error if the list is undefined', () => {
+      const generator = requestDeckListFlow({
+        meta: 'someID',
+      });
+      expect(generator.next().value).toEqual(
+        call(requestUserDecksCall, 'someID'),
+      );
+
+      expect(
+        generator.throw(new Error('Received undefined list.')).value,
+      ).toEqual(
+        put({
+          type: REQUEST_DECK_LIST_FAILURE,
+          payload: {
+            message: 'Received undefined list.',
+          },
+        }),
+      );
+      expect(generator.next().done).toBeTruthy();
+    });
+
+    it('throws an error if the call returns an error', () => {
+      const generator = requestDeckListFlow({
+        meta: 'someID',
+      });
+      expect(generator.next().value).toEqual(call(requestUserDecksCall, 'someID'));
+
+      expect(generator.throw(dummyError).value).toEqual(
+        put({
+          type: REQUEST_DECK_LIST_FAILURE,
+          payload: dummyError,
+        }),
+      );
+
+      expect(generator.next().done).toBeTruthy();
     });
   });
 });
