25508be7090f8c11bc34abfcaee8b0f0bb0a9f74
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-21 16:05:46 +0200

    Add updateSlide action

--- client/app/actions/slideActions.js
+++ client/app/actions/slideActions.js
@@ -2,6 +2,7 @@
 export const FETCH_SLIDES = 'FETCH_SLIDES';
 export const FETCH_SLIDES_SUCCESS = 'FETCH_SLIDES_SUCCESS';
 export const FETCH_SLIDES_FAILURE = 'FETCH_SLIDES_FAILURE';
+export const UPDATE_SLIDE = 'UPDATE_SLIDE';
 
 export const ADD_SLIDE = 'ADD_SLIDE';
 
@@ -12,6 +13,13 @@ export function fetchSlides(deckId) {
   return {
     type: FETCH_SLIDES,
     meta: { deckId },
+  };
+}
+
+export function updateSlide(content) {
+  return {
+    type: UPDATE_SLIDE,
+    payload: { content },
   };
 }
 
