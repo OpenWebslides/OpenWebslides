123d6f841f1f539882d34759b342eaa87f27f8c5
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-21 10:26:35 +0200

    Conversion tool: copy assets

---
+++ app/commands/repository/asset/copy.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+
+module Repository
+  module Asset
+    ##
+    # Copy asset file
+    #
+    class Copy < AssetCommand
+      attr_accessor :file
+
+      def execute
+        raise OpenWebslides::ArgumentError, 'Filename not specified' unless @file
+        raise OpenWebslides::ArgumentError, 'File does not exist' unless File.exist? @file
+
+        FileUtils.cp @file, asset_file
+      end
+    end
+  end
+end
