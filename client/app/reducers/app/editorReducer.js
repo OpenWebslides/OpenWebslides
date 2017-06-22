6aeb8f6429f173eb27cb8727d2049c99d117be23
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-22 12:11:00 +0200

    Adds updated SetActiveSlide action

--- client/app/reducers/app/editorReducer.js
+++ client/app/reducers/app/editorReducer.js
@@ -1,12 +1,15 @@
 import Immutable from 'seamless-immutable';
 
+import { SET_ACTIVE_SLIDE } from 'actions/slideActions';
+
 const initialState = Immutable({
-  currentDeck: null,
-  currentSlide: null,
+  activeSlide: null,
 });
 
 function editor(state = initialState, action) {
   switch (action.type) {
+    case SET_ACTIVE_SLIDE:
+      return { ...state, activeSlide: action.payload.slideId };
     default:
       return state;
   }
