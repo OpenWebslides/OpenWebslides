76713baff09fe16377e7f915e1cd64b40d22f6d2
Luca Dorigo <dorigoluca@gmail.com>
2017-06-23 14:20:18 +0200

    Removed asyncFetch file and moved the function inside the api helper

--- client/app/api/helpers/apiHelper.js
+++ client/app/api/helpers/apiHelper.js
@@ -1,4 +1,5 @@
-import loadState from 'client/localStorage';
+import loadState from 'localStorage';
+import { ApiError, ValidationError } from './errors';
 
 function getAuthToken() {
   // We load it from the local storage.
@@ -6,6 +7,31 @@ function getAuthToken() {
   const loadedState = loadState();
 
   return loadedState.app.auth.token;
+}
+
+async function asyncFetch(url, requestConfig = {}) {
+  const response = await fetch(url, requestConfig);
+  const { statusText, status } = response;
+  let responseBody;
+
+  switch (status) {
+    case 400:
+    case 401:
+    case 403:
+      throw new ApiError(statusText, status);
+    case 422:
+      responseBody = await response.json();
+      throw new ValidationError(statusText, status, responseBody.errors);
+    case 500:
+    case 501:
+    case 502:
+    case 503:
+    case 504:
+    case 505:
+      throw new ApiError(statusText, status);
+    default:
+      return response;
+  }
 }
 
 function ApiRequest() {
