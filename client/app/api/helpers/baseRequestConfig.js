26b642d1b9435bd673489e6ebe8e78bd3e805e2d
Luca Dorigo <dorigoluca@gmail.com>
2017-06-23 14:59:41 +0200

    Modified email confirmation api call to use the helper

--- client/app/api/helpers/baseRequestConfig.js
+++
@@ -1,9 +0,0 @@
-// Base configuration for API calls
-export default function getBaseRequestConfig() {
-  return {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/vnd.api+json',
-    },
-  };
-}
