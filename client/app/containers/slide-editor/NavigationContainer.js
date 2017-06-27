ecbff3abcf92b49e4bb5db3100690f1a58a73dea
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-27 12:55:09 +0200

    Updates containers for new reducer structure and new actions

--- client/app/containers/slide-editor/NavigationContainer.js
+++ client/app/containers/slide-editor/NavigationContainer.js
@@ -1,18 +1,23 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { addSlide, setActiveSlide } from 'actions/slideActions';
+import { addSlide, setActiveSlide, deleteSlide } from 'actions/slideActions';
 
 import NavigationPane from 'presentationals/components/editor/NavigationPane';
 
 function mapStateToProps(state) {
   return {
     slides: state.entities.slides.byId,
+    slideSequence: state.app.editor.slides.sequence,
+    activeSlide: state.app.editor.slides.active,
   };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ addSlide, setActiveSlide }, dispatch);
+  return bindActionCreators(
+    { addSlide, deleteSlide, setActiveSlide },
+    dispatch,
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavigationPane);
