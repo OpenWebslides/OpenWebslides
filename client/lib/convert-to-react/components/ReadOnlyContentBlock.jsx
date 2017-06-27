3ca2e5aa8b99bcddb7a70942bee56673df32bca4
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-27 12:57:52 +0200

    Saves temporary state of Editor Blocks

--- client/lib/convert-to-react/components/ReadOnlyContentBlock.jsx
+++ client/lib/convert-to-react/components/ReadOnlyContentBlock.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Editor } from 'draft-js';
+import { convertToHTML } from 'draft-convert';
 
-export default function ReadOnlyContentBlock(props) {
-  const { contentBlock } = props;
-  return <Editor readOnly editorState={contentBlock.data} />;
+export default class ReadOnlyContentBlock extends Component {
+  shouldComponentUpdate(nextProps) {
+    const currentContent = this.props.contentBlock.data.getCurrentContent();
+    const nextContent = nextProps.contentBlock.data.getCurrentContent();
+
+    if (currentContent === nextContent) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    const { contentBlock } = this.props;
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: convertToHTML(contentBlock.data.getCurrentContent()),
+        }}
+      />
+    );
+  }
 }
 
 ReadOnlyContentBlock.propTypes = {
