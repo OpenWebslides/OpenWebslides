ef19bcb3dba7ffc3bcc5c9dc189825b3094229ac
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-27 12:56:50 +0200

    Updates entities reducers with new actions

--- client/app/reducers/entities/content-blocks/byIdReducer.js
+++ client/app/reducers/entities/content-blocks/byIdReducer.js
@@ -1,13 +1,26 @@
 import { FETCH_SLIDES_SUCCESS, UPDATE_SLIDE } from 'actions/slideActions';
+import { ADD_TITLE } from 'actions/contentBlockActions';
+import { EditorState } from 'draft-js';
 
 const initialState = {};
 
 function byId(state = initialState, action) {
   switch (action.type) {
+    case ADD_TITLE:
+      return {
+        ...state,
+        [action.payload.contentBlockId]: {
+          id: action.payload.contentBlockId,
+          data: EditorState.createEmpty(),
+        },
+      };
     case UPDATE_SLIDE:
       return {
         ...state,
-        [action.payload.id]: { data: action.payload.content },
+        [action.payload.id]: {
+          id: action.payload.id,
+          data: action.payload.content,
+        },
       };
 
     case FETCH_SLIDES_SUCCESS:
--- client/app/reducers/entities/slides/byIdReducer.js
+++ client/app/reducers/entities/slides/byIdReducer.js
@@ -1,11 +1,38 @@
-import { FETCH_SLIDES_SUCCESS } from 'actions/slideActions';
+import _ from 'lodash';
+
+import {
+  FETCH_SLIDES_SUCCESS,
+  ADD_SLIDE,
+  DELETE_SLIDE,
+} from 'actions/slideActions';
 import { ADD_TITLE } from 'actions/contentBlockActions';
 
 const initialState = {};
 
 function byId(state = initialState, action) {
   switch (action.type) {
+    case ADD_SLIDE:
+      return {
+        ...state,
+        [action.payload.slideSequence]: {
+          content: [],
+        },
+      };
+
+    case DELETE_SLIDE:
+      return _.omit(state, action.payload.slideId);
+
     case ADD_TITLE:
+      return {
+        ...state,
+        [action.payload.slideId]: {
+          content: state[action.payload.slideId].content.concat({
+            id: action.payload.contentBlockId,
+            type: 'contentBlock',
+          }),
+        },
+      };
+
     case FETCH_SLIDES_SUCCESS:
       return action.payload.slides;
     default:
