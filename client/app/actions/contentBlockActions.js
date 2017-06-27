3b81b7fac52303fbc6d095c5ca8232493944b658
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-27 15:26:24 +0200

    Adds deleteContentBlocks action

--- client/app/actions/contentBlockActions.js
+++ client/app/actions/contentBlockActions.js
@@ -1,12 +1,20 @@
 // Action types
 export const ADD_TITLE = 'ADD_TITLE';
 export const SET_ACTIVE_CONTENT_BLOCK = 'SET_ACTIVE_CONTENT_BLOCK';
+export const DELETE_CONTENT_BLOCKS = 'DELETE_CONTENT_BLOCKS';
 
 // Action Creators
 export function setActiveContentBlock(contentBlockId) {
   return {
     type: SET_ACTIVE_CONTENT_BLOCK,
     payload: { contentBlockId },
+  };
+}
+
+export function deleteContentBlocks(contentBlocksIds) {
+  return {
+    type: DELETE_CONTENT_BLOCKS,
+    payload: { contentBlocksIds },
   };
 }
 
