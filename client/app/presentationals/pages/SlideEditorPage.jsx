05b28233e540e6856b6473ab1370a359cf40ab9b
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-27 17:39:18 +0200

    Adds first draft of fetchDeckApi

--- client/app/presentationals/pages/SlideEditorPage.jsx
+++ client/app/presentationals/pages/SlideEditorPage.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 import DefaultLayout from 'presentationals/layouts/DefaultLayout';
-import DeckNavigationPane from 'presentationals/components/editor/DeckNavigationPane';
-import SlideEditingPane from 'presentationals/components/editor/SlideEditingPane';
+import Editor from 'containers/slide-editor/EditorContainer';
 
 function SlideEditorPage() {
   return (
     <DefaultLayout
       cssIdentifier="slide-editor"
       components={{
-        'deck-navigator': <DeckNavigationPane />,
-        'slide-editor': <SlideEditingPane />,
+        editor: <Editor />,
       }}
     />
   );
