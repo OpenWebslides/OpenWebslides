ad1d005a693c14b6ac7d44aef7c8a12abe053b8b
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-27 12:53:59 +0200

    Adds new action creators

--- client/app/actions/contentBlockActions.js
+++ client/app/actions/contentBlockActions.js
@@ -1,10 +1,18 @@
 // Action types
 export const ADD_TITLE = 'ADD_TITLE';
+export const SET_ACTIVE_CONTENT_BLOCK = 'SET_ACTIVE_CONTENT_BLOCK';
 
 // Action Creators
-export function addTitle(slideId) {
+export function setActiveContentBlock(contentBlockId) {
+  return {
+    type: SET_ACTIVE_CONTENT_BLOCK,
+    payload: { contentBlockId },
+  };
+}
+
+export function addTitle(slideId, contentBlockId) {
   return {
     type: ADD_TITLE,
-    payload: { [slideId]: { content: {} } },
+    payload: { slideId, contentBlockId },
   };
 }
