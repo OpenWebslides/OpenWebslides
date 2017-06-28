62e29fdfdd3cb4ee4552c474278cc001dcd63635
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-28 10:37:45 +0200

    Splits up container/presentational, adds new inline style buttons

--- client/app/presentationals/components/editor/EditingPane.jsx
+++ client/app/presentationals/components/editor/EditingPane.jsx
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import ViewsPane from './ViewsPane';
 import { RichUtils } from 'draft-js';
-import { updateSlide } from 'actions/slideActions';
 
-import { addTitle } from 'actions/contentBlockActions';
+import ViewsPane from './ViewsPane';
 
-class EditingPane extends Component {
+export default class EditingPane extends Component {
   constructor() {
     super();
-    this.handleToggleBold = this.handleToggleBold.bind(this);
+    this.handleToggleInlineStyle = this.handleToggleInlineStyle.bind(this);
     this.handleAddTitle = this.handleAddTitle.bind(this);
   }
 
-  handleToggleBold(e) {
+  handleToggleInlineStyle(e, type) {
     e.preventDefault();
-    const newState = RichUtils.toggleInlineStyle(
-      this.props.activeContentBlock.data,
-      'BOLD',
-    );
 
-    this.props.updateSlide(this.props.activeContentBlock.id, newState);
+    const { activeContentBlock } = this.props;
+
+    if (activeContentBlock) {
+      const newState = RichUtils.toggleInlineStyle(
+        activeContentBlock.data,
+        type,
+      );
+
+      this.props.updateSlide(activeContentBlock.id, newState);
+    }
   }
 
   handleAddTitle(e) {
     e.preventDefault();
-    const { contentBlockSequence, activeSlide } = this.props;
+    const { contentBlockSequence, activeSlideId } = this.props;
 
-    this.props.addTitle(activeSlide, contentBlockSequence);
+    this.props.addTitle(activeSlideId, contentBlockSequence);
   }
 
   render() {
@@ -43,7 +44,7 @@ class EditingPane extends Component {
     //       <p>[Toolbar goes here]</p>
     //     </div> */}
     //         <div className="c_slide-editor__item c_slide-editor__item--views-panel">
-    //           <ViewPane activeSlide={activeSlide} />
+    //           <ViewPane activeSlideId={activeSlideId} />
     //         </div>
     //         {/* #TODO
     //     <div className="c_slide-editor__item c_slide-editor__item--slide-health">
@@ -60,7 +61,19 @@ class EditingPane extends Component {
         <div className="c_slide-editor__wrapper">
           <div className="c_slide-editor__item c_slide-editor__item--toolbar">
             <button onMouseDown={this.handleAddTitle}>Add Title</button>
-            <button onMouseDown={this.handleToggleBold}>Set Bold</button>
+            <button onMouseDown={e => this.handleToggleInlineStyle(e, 'BOLD')}>
+              Bold
+            </button>
+            <button
+              onMouseDown={e => this.handleToggleInlineStyle(e, 'ITALIC')}
+            >
+              Italic
+            </button>
+            <button
+              onMouseDown={e => this.handleToggleInlineStyle(e, 'UNDERLINE')}
+            >
+              Underline
+            </button>
 
           </div>
           <div className="c_slide-editor__item c_slide-editor__item--views-panel">
@@ -77,24 +90,17 @@ class EditingPane extends Component {
 }
 
 EditingPane.propTypes = {
+  activeContentBlock: PropTypes.objectOf(Object),
+  activeSlideId: PropTypes.number,
+  addTitle: PropTypes.func.isRequired,
+  contentBlockSequence: PropTypes.number,
   cssIdentifier: PropTypes.string,
+  updateSlide: PropTypes.func.isRequired,
 };
 
 EditingPane.defaultProps = {
+  activeContentBlock: null,
+  activeSlideId: null,
+  contentBlockSequence: null,
   cssIdentifier: 'default',
 };
-
-function mapStateToProps(state) {
-  const activeContentBlock = state.app.editor.contentBlocks.active;
-  return {
-    activeSlide: state.app.editor.slides.active,
-    activeContentBlock: state.entities.contentBlocks.byId[activeContentBlock],
-    contentBlockSequence: state.app.editor.contentBlocks.sequence,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ addTitle, updateSlide }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(EditingPane);
