584c2f71ab3e0a2611bba07f32cb445d09a14852
Luca Dorigo <dorigoluca@gmail.com>
2017-06-28 09:41:31 +0200

    Added components for deck import

---
+++ client/app/presentationals/pages/ImportDeckPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ImportDeckForm from 'containers/ImportDeckFormContainer';
+
+function CreateDeckPage() {
+  return (
+    <div>
+      <ImportDeckForm />
+    </div>
+  );
+}
+
+export default CreateDeckPage;
