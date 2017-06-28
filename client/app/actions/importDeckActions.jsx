a60ebb9eab74c7dfab8ca353787609a51005423c
Luca Dorigo <dorigoluca@gmail.com>
2017-06-28 12:45:26 +0200

    Added more actions and reducers for uploading deck

--- client/app/actions/importDeckActions.jsx
+++ client/app/actions/importDeckActions.jsx
@@ -1,3 +1,8 @@
+export const SELECT_UPLOAD_FILE = 'SELECT_UPLOAD_FILE';
+export const START_UPLOAD = 'START_UPLOAD';
+export const ADVANCE_UPLOAD = 'ADVANCE_UPLOAD';
+export const UPLOAD_FAILURE = 'UPLOAD_FAILURE';
+export const UPLOAD_SUCCESS = 'UPLOAD_SUCCESS';
 export const DECK_IMPORT_REQUEST = 'DECK_IMPORT_REQUEST';
 export const DECK_IMPORT_REQUEST_SUCCESS = 'DECK_IMPORT_REQUEST_SUCCESS';
 export const DECK_IMPORT_REQUEST_FAILURE = 'DECK_IMPORT_REQUEST_FAILURE';
@@ -13,5 +18,11 @@ export function deckImportFailure(message) {
   return {
     type: DECK_IMPORT_REQUEST_FAILURE,
     payload: message,
+  };
+}
+export function selectUploadFile(file) {
+  return {
+    type: SELECT_UPLOAD_FILE,
+    payload: file,
   };
 }
