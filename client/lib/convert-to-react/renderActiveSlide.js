a77181ed134085747df5b603367c6a0ddfbcea0c
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-28 14:22:59 +0200

    Updates files for consistent naming of slide-editor instead of editor

--- client/lib/convert-to-react/renderActiveSlide.js
+++ client/lib/convert-to-react/renderActiveSlide.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Slide from 'presentationals/components/editor/Slide';
+import Slide from 'presentationals/components/slide-editor/Slide';
 import convertContent from 'lib/convert-to-react/convertContent';
 
 export default function renderActiveSlide({ content }) {
