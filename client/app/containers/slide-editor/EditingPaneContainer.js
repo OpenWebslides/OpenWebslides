c0378a3ef7acbc5e9b1b13cf33f549b8958042b4
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-28 14:22:59 +0200

    Updates files for consistent naming of slide-editor instead of editor

--- client/app/containers/slide-editor/EditingPaneContainer.js
+++ client/app/containers/slide-editor/EditingPaneContainer.js
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 import { updateSlide } from 'actions/slideActions';
 import { addTitle } from 'actions/contentBlockActions';
 
-import EditingPane from 'presentationals/components/editor/EditingPane';
+import EditingPane from 'presentationals/components/slide-editor/EditingPane';
 
 function mapStateToProps(state) {
   const activeContentBlock = state.app.editor.contentBlocks.active;
+  const activeSlideId = parseInt(state.app.editor.slides.active, 10);
+
   return {
-    activeSlideId: state.app.editor.slides.active,
+    activeSlideId,
     activeContentBlock: state.entities.contentBlocks.byId[activeContentBlock],
     contentBlockSequence: state.app.editor.contentBlocks.sequence,
   };
