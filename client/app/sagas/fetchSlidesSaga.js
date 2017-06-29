03da426bcd18559c9d81ed417a6ef9b2cfd0b990
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-29 10:18:39 +0200

    Adds temp mock of html string

--- client/app/sagas/fetchSlidesSaga.js
+++ client/app/sagas/fetchSlidesSaga.js
@@ -4,9 +4,25 @@ import { FETCH_SLIDES } from 'actions/slideActions';
 import convertToState from 'lib/convert-to-state';
 import fetchDeckApi from 'api/fetchDeckApi';
 
+const HTMLResponse = `
+  <header class="caption">
+    <h1>Presentation Title</h1>
+    <p><a href="">Author</a>, <a href="http://www.ugent.be/">Ghent University</a>.</p>
+  </header>
+  <section class="slide">
+    <h1>First Slide</h1>
+  </section>
+  <section class="slide">
+    <p>This is a subtitle with <em>emphasized text</em> and even <strong>strong text</strong></p>
+  </section>
+  <div class="progress"></div>
+  <footer class="badge">
+    <a href="https://github.com/OpenWebSlides/EmptySlidedeck">Fork me on GitHub</a>
+  </footer>'`;
+
 function* doFetchDeck(action) {
   try {
-    const HTMLResponse = yield call(fetchDeckApi, action.meta.deckId);
+    // const HTMLResponse = yield call(fetchDeckApi, action.meta.deckId);
 
     const state = yield convertToState(HTMLResponse);
 
