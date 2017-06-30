78649199aff6edef98dca36f0c19413eb5b24da0
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-06-30 11:58:23 +0200

    Generalizes action to incorporate new content blocks

--- client/app/reducers/entities/content-blocks/byIdReducer.js
+++ client/app/reducers/entities/content-blocks/byIdReducer.js
@@ -1,19 +1,30 @@
 import { EditorState } from 'draft-js';
+import { convertFromHTML } from 'draft-convert';
 import _ from 'lodash';
 
+import contentBlockConfigs from 'lib/content-block-configs';
+
 import { FETCH_SLIDES_SUCCESS, UPDATE_SLIDE } from 'actions/slideActions';
-import { ADD_TITLE, DELETE_CONTENT_BLOCKS } from 'actions/contentBlockActions';
+import {
+  ADD_CONTENT_BLOCK,
+  DELETE_CONTENT_BLOCKS,
+} from 'actions/contentBlockActions';
 
 const initialState = {};
 
 function byId(state = initialState, action) {
   switch (action.type) {
-    case ADD_TITLE:
+    case ADD_CONTENT_BLOCK:
       return {
         ...state,
         [action.payload.contentBlockId]: {
           id: action.payload.contentBlockId,
-          data: EditorState.createEmpty(),
+          type: action.payload.type,
+          data: EditorState.createWithContent(
+            convertFromHTML(
+              contentBlockConfigs[action.payload.type].createEmpty,
+            ),
+          ),
         },
       };
     case DELETE_CONTENT_BLOCKS:
