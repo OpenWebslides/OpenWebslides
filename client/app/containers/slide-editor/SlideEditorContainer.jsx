4723cbe3ae915309563c59d78e4b5ec345d9c13c
Dromin <contact@dromin.be>
2017-06-30 15:04:18 +0200

    Fixed slide editor components naming & fixed imports.

--- client/app/containers/slide-editor/SlideEditorContainer.jsx
+++ client/app/containers/slide-editor/SlideEditorContainer.jsx
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 
 import { fetchSlides } from 'actions/slideActions';
 
-import Editor from 'presentationals/components/slide-editor/Editor';
+import SlideEditor from 'presentationals/components/slide-editor/SlideEditor';
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchSlides }, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(Editor);
+export default connect(null, mapDispatchToProps)(SlideEditor);
--- client/app/presentationals/pages/SlideEditorPage.jsx
+++ client/app/presentationals/pages/SlideEditorPage.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
 import DefaultLayout from 'presentationals/layouts/DefaultLayout';
-import Editor from 'containers/slide-editor/EditorContainer';
+import SlideEditorContainer from 'containers/slide-editor/SlideEditorContainer';
 
 function SlideEditorPage() {
   return (
-    <DefaultLayout
-      cssIdentifier="slide-editor"
-      components={{
-        editor: <Editor />,
-      }}
-    />
+    <DefaultLayout cssIdentifier="slide-editor">
+      <SlideEditorContainer />
+    </DefaultLayout>
   );
 }
 
