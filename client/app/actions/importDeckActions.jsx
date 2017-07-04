c91c3bb6cbca8c52f74e9a62d45d9289737539e3
Luca Dorigo <dorigoluca@gmail.com>
2017-07-04 10:09:22 +0200

    Working form to select file to be imported.

--- client/app/actions/importDeckActions.jsx
+++ client/app/actions/importDeckActions.jsx
@@ -3,16 +3,9 @@ export const START_UPLOAD = 'START_UPLOAD';
 export const ADVANCE_UPLOAD = 'ADVANCE_UPLOAD';
 export const UPLOAD_FAILURE = 'UPLOAD_FAILURE';
 export const UPLOAD_SUCCESS = 'UPLOAD_SUCCESS';
-export const DECK_IMPORT_REQUEST = 'DECK_IMPORT_REQUEST';
+export const IMPORT_STARTED = 'IMPORT_STARTED';
 export const DECK_IMPORT_REQUEST_SUCCESS = 'DECK_IMPORT_REQUEST_SUCCESS';
 export const DECK_IMPORT_REQUEST_FAILURE = 'DECK_IMPORT_REQUEST_FAILURE';
-
-export function requestDeckImport(info) {
-  return {
-    type: DECK_IMPORT_REQUEST,
-    meta: info,
-  };
-}
 
 export function deckImportFailure(message) {
   return {
@@ -24,5 +17,11 @@ export function selectUploadFile(file) {
   return {
     type: SELECT_UPLOAD_FILE,
     payload: file,
+  };
+}
+export function uploadFailure(message) {
+  return {
+    type: UPLOAD_FAILURE,
+    payload: message,
   };
 }
