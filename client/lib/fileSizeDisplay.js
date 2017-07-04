20cd324dc43be3ed48782a1fda200f209750268c
Luca Dorigo <dorigoluca@gmail.com>
2017-07-04 10:02:19 +0200

    Added utility to convert file size to human-readable form

---
+++ client/lib/fileSizeDisplay.js
@@ -0,0 +1,6 @@
+export default function fileSizeDisplay(size) {
+  const i = Math.floor(Math.log(size) / Math.log(1024));
+  return `${(size / 1024 ** i).toFixed(2) * 1} ${['B', 'kB', 'MB', 'GB', 'TB'][
+    i
+  ]}`;
+}
