41475391bcb32c3102f4cef4ed5006e9ab5948ae
Luca Dorigo <dorigoluca@gmail.com>
2017-07-05 10:16:34 +0200

    Moved sample deck to separate file

--- client/app/sagas/fetchDeckContentSaga.js
+++ client/app/sagas/fetchDeckContentSaga.js
@@ -3,26 +3,12 @@ import { FETCH_DECK_CONTENT } from 'actions/deckActions';
 
 import convertToPrint from 'lib/convert-to-print';
 
-const HTMLResponse = `
-  <header class="caption">
-    <h1>Presentation Title</h1>
-    <p><a href="">Author</a>, <a href="http://www.ugent.be/">Ghent University</a>.</p>
-  </header>
-  <section class="slide">
-    <h1>First Slide</h1>
-  </section>
-  <section class="slide">
-    <p>This is a subtitle with <em>emphasized text</em> and even <strong>strong text</strong></p>
-  </section>
-  <div class="progress"></div>
-  <footer class="badge">
-    <a href="https://github.com/OpenWebSlides/EmptySlidedeck">Fork me on GitHub</a>
-  </footer>'`;
+import { flamesDeck } from '../constants/exampleDecks';
+
 
 function* doFetchDeck() {
   try {
-    const state = yield convertToPrint(HTMLResponse);
-
+    const state = yield convertToPrint(flamesDeck);
     yield put({ type: 'FETCH_DECK_CONTENT_SUCCESS', payload: state });
   } catch (e) {
     console.log(e);
