c9bbffe98bdcac947acd71a07e65d64c70c1c301
Luca Dorigo <dorigoluca@gmail.com>
2017-07-05 16:02:59 +0200

    Added reducer for user imports

---
+++ client/app/reducers/app/userImportsReducer.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-case-declarations */
+import Immutable from 'seamless-immutable';
+
+import {
+  REQUEST_USER_IMPORTS,
+  REQUEST_USER_IMPORTS_SUCCESS,
+  REQUEST_USER_IMPORTS_FAILURE,
+} from 'actions/userImportsActions';
+
+const initialState = Immutable({
+  listOfUserImports: [],
+  sentRequestForList: false,
+  receivedList: false,
+  errorMessage: '',
+});
+
+function feedReducer(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_USER_IMPORTS:
+      return Immutable.merge(state, {
+        sentRequestForList: true,
+        receivedList: false,
+      });
+    case REQUEST_USER_IMPORTS_SUCCESS:
+      return Immutable.merge(state, {
+        sentRequestForList: false,
+        receivedList: true,
+        errorMessage: '',
+        listOfUserImports: action.payload.listOfUserImports,
+      });
+    case REQUEST_USER_IMPORTS_FAILURE:
+      return Immutable.merge(state, {
+        sentRequestForList: false,
+        receivedList: false,
+        errorMessage: action.payload,
+      });
+    default:
+      return state;
+  }
+}
+
+export default feedReducer;
