e91f4f3340a4206083b63b210ea10c4dbd00e119
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-05 13:08:15 +0200

    Allow uploading multipart/form-data

--- app/controllers/api/conversions_controller.rb
+++ app/controllers/api/conversions_controller.rb
@@ -4,7 +4,7 @@ require 'securerandom'
 
 module Api
   class ConversionsController < ApiController
-    MEDIA_TYPE = 'application/octet-stream'
+    MEDIA_TYPE = 'multipart/form-data'
 
     before_action :authenticate_user
 
@@ -15,10 +15,6 @@ module Api
         return handle_exceptions JSONAPI::Exceptions::UnsupportedMediaTypeError.new(request.content_type)
       end
 
-      return unless verify_accept_header
-
-      raise Api::ApiError, :detail => 'No Content-Disposition header' unless request.headers['HTTP_CONTENT_DISPOSITION']
-
       conversion = Conversion.new :user => current_user, :status => :queued
 
       # Authorize
@@ -26,14 +22,13 @@ module Api
       context[:policy_used]&.call
 
       # Copy uploaded file to tempfile
-      filename = request.headers['HTTP_CONTENT_DISPOSITION'].match(/filename ?= ?"?([^\\"]*)"?/)[1]
-      raise Api::ApiError, :detail => 'Invalid Content-Disposition header' unless filename
+      filename = params[:qqfilename]
+      raise Api::ApiError, :detail => 'Invalid filename' unless filename
 
       # Create tempfile with proper extension
       file = Tempfile.new ['', ".#{filename.split('.').last}"]
-      file.binmode
-      file.write request.body.read
       file.close
+      FileUtils.cp params[:qqfile].path, file.path
 
       # Create and queue conversion
       conversion.filename = file.path
@@ -45,13 +40,41 @@ module Api
       results = JSONAPI::OperationResults.new
       results.add_result JSONAPI::ResourceOperationResult.new :created, resource
 
-      render_results results
-    rescue Api::ApiError => e
-      e.params[:code] = JSONAPI::UNPROCESSABLE_ENTITY
-      e.params[:status] = :unprocessable_entity
-      e.params[:title] = 'Unprocessable entity'
+      render_upload_results results
+    rescue => e
+      render :json => {
+        :success => false,
+        :error => e.message,
+        :preventRetry => true
+      }
+    end
 
-      raise e
+    private
+
+    def render_upload_results(operation_results)
+      # Adapted from ActsAsResourceController#render_results
+
+      response_doc = create_response_document(operation_results)
+      content = response_doc.contents
+
+      render_options = {}
+      if operation_results.has_errors?
+        render_options[:json] = content
+      else
+        # Bypasing ActiveSupport allows us to use CompiledJson objects for cached response fragments
+        render_options[:body] = JSON.generate(content.merge :success => true)
+      end
+
+      render_options[:location] = content[:data]["links"][:self] if (
+      response_doc.status == :created && content[:data].class != Array
+      )
+
+      # For whatever reason, `render` ignores :status and :content_type when :body is set.
+      # But, we can just set those values directly in the Response object instead.
+      response.status = response_doc.status
+      response.headers['Content-Type'] = JSONAPI::MEDIA_TYPE
+
+      render(render_options)
     end
   end
 end
