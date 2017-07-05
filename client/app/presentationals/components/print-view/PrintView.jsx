abf24c320873d270b6a838d8835c13320f6bfdcc
Luca Dorigo <dorigoluca@gmail.com>
2017-07-05 10:15:45 +0200

    display example printview in the component

--- client/app/presentationals/components/print-view/PrintView.jsx
+++ client/app/presentationals/components/print-view/PrintView.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import convertToPrint from 'lib/convert-to-print/index';
+import { flamesDeck, smallDeck } from 'constants/exampleDecks'; // TODO: only for testing
+
 export default class SlideEditor extends Component {
   componentDidMount() {
     this.props.fetchDeckContent(2);
   }
 
   render() {
-    return <main className="l_main" />;
+    const elements = convertToPrint(flamesDeck);
+    return (
+      <div>
+        {elements}
+      </div>
+    );
   }
 }
 
