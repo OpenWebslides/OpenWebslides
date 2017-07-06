e34f3d6de2cd91b6c054f3737eeb6b63ff4732a3
Luca Dorigo <dorigoluca@gmail.com>
2017-07-06 10:12:02 +0200

    Added userImports to the imports page

--- client/app/presentationals/pages/ImportDeckPage.jsx
+++ client/app/presentationals/pages/ImportDeckPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import ImportDeckForm from 'containers/ImportDeckFormContainer';
+import UserImports from 'containers/UserImportsContainer';
 
 function CreateDeckPage() {
   return (
     <div>
       <ImportDeckForm />
+      <UserImports />
     </div>
   );
 }
