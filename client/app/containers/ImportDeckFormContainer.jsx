c8c846a3843ae1452081f9dba86f8925667ecf38
Luca Dorigo <dorigoluca@gmail.com>
2017-07-06 16:44:02 +0200

    Added actions, reducers and components to display erros when user tries to import a wrong file

--- client/app/containers/ImportDeckFormContainer.jsx
+++ client/app/containers/ImportDeckFormContainer.jsx
@@ -1,15 +1,16 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import ImportDeckForm from 'presentationals/components/deckManagement/ImportDeckForm';
+import { importUploadError } from 'actions/userImportsActions';
 
 function mapStateToProps(state) {
   return {
     authState: state.app.authentication,
-    deckImportState: state.app.deckImport,
+    userImportState: state.app.userImports,
   };
 }
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({}, dispatch);
+  return bindActionCreators({ importUploadError }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ImportDeckForm);
