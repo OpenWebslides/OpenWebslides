f26c6df79552084b6b664adaf3facd7c0db29a3d
Luca Dorigo <dorigoluca@gmail.com>
2017-07-06 10:13:49 +0200

    Added userID as parameter to requestUserImports

--- client/app/actions/userImportsActions.js
+++ client/app/actions/userImportsActions.js
@@ -2,9 +2,10 @@ export const REQUEST_USER_IMPORTS = 'REQUEST_USER_IMPORTS';
 export const REQUEST_USER_IMPORTS_SUCCESS = 'REQUEST_USER_IMPORTS_SUCCESS';
 export const REQUEST_USER_IMPORTS_FAILURE = 'REQUEST_USER_IMPORTS_FAILURE';
 
-export function requestUserImports() {
+export function requestUserImports(userID) {
   return {
     type: REQUEST_USER_IMPORTS,
+    meta: userID,
   };
 }
 export function receiveImports(list) {
