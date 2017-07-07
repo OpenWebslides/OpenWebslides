bffac9e820b4075a5b75210fdbc91066a1b837b5
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-07 14:17:39 +0200

    Add authentication API route specs

--- app/controllers/api/password_controller.rb
+++ app/controllers/api/password_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 module Api
-  class PasswordController < ApiController
-    skip_after_action :enforce_policy_use, :only => %i[create update]
-  end
+  class PasswordController < ApiController; end
 end
