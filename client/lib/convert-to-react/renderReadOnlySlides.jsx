83835f349eb31f49501dac55c8c84945a288f00b
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-07-07 16:44:40 +0200

    Removes implementation of Draft JS

--- client/lib/convert-to-react/renderReadOnlySlides.jsx
+++ client/lib/convert-to-react/renderReadOnlySlides.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
-import convertContent from 'lib/convert-to-react/convertContent';
-import Slide from 'presentationals/components/slide-editor/Slide';
-
 export default function convertToReact(slides) {
   const slideComponentArr = [];
 
-  Object.keys(slides).forEach(key => {
-    const slideContent = convertContent(slides[key].content, { active: false });
-
-    const SlideComponent = <Slide key={key} id={key}>{slideContent}</Slide>;
+  Object.keys(slides).forEach(() => {
+    const SlideComponent = <h4>Placeholder</h4>;
 
     slideComponentArr.push(SlideComponent);
   });
