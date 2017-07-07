810b19d28ad8dd995d9e1b05f55a655ce9bf0cd4
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-07 11:26:13 +0200

    Add AssetPolicy spec

--- app/policies/asset_policy.rb
+++ app/policies/asset_policy.rb
@@ -1,19 +1,35 @@
 # frozen_string_literal: true
 
 class AssetPolicy < ApplicationPolicy
+  ##
+  # Resource
+  #
   def create?
-    # Users can create an asset but only for decks with write access
+    # Users can create but only for updatable decks
     deck_policy.update?
   end
 
+  def show?
+    # Users can show but only for showable decks
+    deck_policy.show?
+  end
+
   def update?
-    # Users can update an asset but only for decks with write access
+    # Users can update but only for updatable decks
     deck_policy.update?
   end
 
   def destroy?
-    # Users can destroy an asset but only for decks with write access
+    # Users can destroy but only for updatable decks
     deck_policy.update?
+  end
+
+  ##
+  # Relationship: deck
+  #
+  def show_deck?
+    # Users can show an asset but only for showable decks
+    deck_policy.show?
   end
 
   # TODO: scope
--- spec/factories/asset_factory.rb
+++ spec/factories/asset_factory.rb
@@ -3,6 +3,5 @@
 FactoryGirl.define do
   factory :asset do
     filename { Faker::File.file_name }
-    deck { build :deck }
   end
 end
