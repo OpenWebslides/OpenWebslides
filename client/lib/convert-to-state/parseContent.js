753d8f49beb70714623bac4180a9d5838bf82f97
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-07-07 17:52:26 +0200

    Add new parser for state

---
+++ client/lib/convert-to-state/parseContent.js
@@ -0,0 +1,63 @@
+// @flow
+import parseInlineProperties from './parseInlineProperties';
+
+export default function convertContentItems(elements: Array<HTMLElement>) {
+  let slideSequence = 0;
+  let contentItemSequence = 0;
+
+  const slides = {};
+  const contentItems = {};
+
+  // Return a deck with one empty slide when no existing slides are present
+  if (elements.length === 0) {
+    slides[slideSequence] = { meta: {}, contentItemIds: [] };
+    slideSequence += 1;
+  }
+
+  elements.forEach(element => {
+    slides[slideSequence] = { meta: {}, contentItemIds: [] };
+
+    slides[slideSequence].contentItemIds = (function addcontentItems(nodeList: NodeList<HTMLElement | Node>) {
+      const contentItemIds = [];
+
+      nodeList.forEach(node => {
+        const id = contentItemSequence;
+
+        const { nodeName, childNodes, textContent } = node;
+
+        if (node.outerHTML === undefined) {
+          return;
+        }
+
+        contentItems[id] = {
+          id,
+          contentType: nodeName,
+        };
+
+        contentItemSequence += 1;
+
+        if (nodeName === 'SECTION' || nodeName === 'ASIDE') {
+          const childItemIds = addcontentItems(childNodes);
+
+          contentItems[id].childItemIds = childItemIds;
+        } else {
+          contentItems[id].text = textContent;
+          contentItems[id].inlineProperties = parseInlineProperties(childNodes);
+        }
+
+        contentItemIds.push(id);
+      });
+
+      return contentItemIds;
+    })(element.childNodes);
+
+    slideSequence += 1;
+  });
+
+  return {
+    slides,
+    contentItems,
+    slideSequence,
+    contentItemSequence,
+  };
+}
