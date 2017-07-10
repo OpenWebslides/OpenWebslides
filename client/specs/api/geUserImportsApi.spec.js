e35d6c05220255b9f0b23e9396b0bb9bf3da0130
Luca Dorigo <dorigoluca@gmail.com>
2017-07-10 17:37:53 +0200

    add tests for imports

---
+++ client/specs/api/geUserImportsApi.spec.js
@@ -0,0 +1,30 @@
+import faker from 'faker';
+
+import asyncFetch from '../../app/api/helpers/asyncFetch';
+
+import getUserImports from '../../app/api/getUserImportsApi';
+
+jest.mock('api/helpers/asyncFetch');
+
+describe('get user imports Api Call', () => {
+  it('has a happy path', async () => {
+    asyncFetch.mockReturnValue({
+      status: 200,
+      json: () => ({ data: 'sample' }),
+    });
+
+    const dummyId = faker.random.number();
+
+    const response = await getUserImports(dummyId);
+    expect(response).toEqual('sample');
+
+    const calledUrl = asyncFetch.mock.calls[0][0];
+
+    expect(calledUrl).toEqual(
+      `http://localhost:3000/api/users/${dummyId}/conversions?sort=-createdAt`,
+    );
+
+    const requestConfig = asyncFetch.mock.calls[0][1];
+    expect(requestConfig.method).toEqual('GET');
+  });
+});
