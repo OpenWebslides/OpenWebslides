32e94dece370c7f6d35294a4b78c267d6a04187c
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-07-12 12:01:06 +0200

    Further naming changes

--- client/app/reducers/entities/index.js
+++ client/app/reducers/entities/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
 
-import contentGroups from 'reducers/entities/content-groups';
+import contentItems from 'reducers/entities/content-items';
 import decks from 'reducers/entities/decks';
 import slides from 'reducers/entities/slides';
 
 const entities = combineReducers({
   decks,
   slides,
-  contentGroups,
+  contentItems,
 });
 
 export default entities;
