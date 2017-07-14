4562f12d8925aefe0e798251319ee14aa1afa381
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-07-14 09:21:54 +0200

    Adds missing reducers

--- client/app/reducers/app/editor/index.js
+++ client/app/reducers/app/editor/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux';
 
 import slides from 'reducers/app/editor/slidesReducer';
 import contentItems from 'reducers/app/editor/contentItemsReducer';
+import activeDeck from 'reducers/app/editor/activeDeckReducer';
+import selection from 'reducers/app/editor/selectionReducer';
 
 const editor = combineReducers({
   contentItems,
   slides,
+  selection,
+  activeDeck,
 });
 
 export default editor;
