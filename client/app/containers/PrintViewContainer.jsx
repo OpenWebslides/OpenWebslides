89607e6df150e534bdd90403adbb44de091efb51
Luca Dorigo <dorigoluca@gmail.com>
2017-07-14 10:38:26 +0200

    Added mapStateToProps to access the state

--- client/app/containers/PrintViewContainer.jsx
+++ client/app/containers/PrintViewContainer.jsx
@@ -5,8 +5,16 @@ import { fetchDeckContent } from 'actions/deckActions';
 
 import PrintView from 'presentationals/components/print-view/PrintView';
 
+function mapStateToProps(state) {
+  const printViewState = state.app.printView;
+  const entities = state.entities;
+  return {
+    printViewState,
+    entities,
+  };
+}
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchDeckContent }, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(PrintView);
+export default connect(mapStateToProps, mapDispatchToProps)(PrintView);
