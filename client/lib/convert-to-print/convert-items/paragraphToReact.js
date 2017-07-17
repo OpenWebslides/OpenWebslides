f7fb4cce751f89e878f7877825b6ea897a652995
Luca Dorigo <dorigoluca@gmail.com>
2017-07-17 08:52:59 +0200

    Split the converter in several files

---
+++ client/lib/convert-to-print/convert-items/paragraphToReact.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function applyProperty(text, property) {
+  return React.createElement('p', null, [
+    React.createElement(
+      'span',
+      null,
+      text.substring(0, property.offsets.start),
+    ),
+    React.createElement(
+      property.type,
+      null,
+      text.substring(property.offsets.start, property.offsets.end),
+    ),
+    React.createElement('span', null, text.substring(property.offsets.end)),
+  ]);
+}
+
+export default function iframeObjectToReact(paragraph) {
+  debugger;
+  if (!paragraph.inlineProperties) {
+    return React.createElement(
+      'p',
+      {
+        className: 'c_print-view__iframe-description',
+      },
+      paragraph.text,
+    );
+  }
+  return applyProperty(paragraph.text, paragraph.inlineProperties[0]); // TODO: support more than 1 property
+}
