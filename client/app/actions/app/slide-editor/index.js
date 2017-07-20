1f14d637976f18edbf75704469549e6e8f4083ba
Dromin <contact@dromin.be>
2017-07-21 01:09:31 +0200

    Moved some actions from other files into the slide-editor actions file. Fixed slide-editor actions naming. Fixed issue in fetchDeckSaga where string action types were used instead of constants. Extracted updateSelectionOffsets() function in ContentEditable component to reduce code repetition.

--- client/app/actions/app/slide-editor/index.js
+++ client/app/actions/app/slide-editor/index.js
@@ -1,5 +1,38 @@
 
+export const SET_ACTIVE_DECK_ID = 'SET_ACTIVE_DECK_ID';
+export const SET_ACTIVE_SLIDE_ID = 'SET_ACTIVE_SLIDE_ID';
+export const SET_ACTIVE_CONTENT_ITEM_ID = 'SET_ACTIVE_CONTENT_ITEM_ID';
+export const SET_SELECTION_OFFSETS = 'SET_SELECTION_OFFSETS';
+
 export const TOGGLE_SLIDE_VIEW = 'TOGGLE_SLIDE_VIEW';
+
+export function setActiveDeckId(deckId) {
+  return {
+    type: SET_ACTIVE_DECK_ID,
+    payload: { deckId },
+  }
+}
+
+export function setActiveSlideId(slideId) {
+  return {
+    type: SET_ACTIVE_SLIDE_ID,
+    payload: { slideId },
+  };
+}
+
+export function setActiveContentItemId(contentItemId) {
+  return {
+    type: SET_ACTIVE_CONTENT_ITEM_ID,
+    payload: { contentItemId },
+  };
+}
+
+export function setSelectionOffsets(selectionOffsets) {
+  return {
+    type: SET_SELECTION_OFFSETS,
+    payload: { selectionOffsets },
+  };
+}
 
 export function toggleSlideView(slideViewType) {
   return {
