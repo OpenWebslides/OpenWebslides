882e0520856d1cba4127135aafa8e3c371a790e2
Dromin <contact@dromin.be>
2017-07-20 22:58:43 +0200

    (Partially) updated actions folder structure to match that of reducers and selectors. Corrected app/editor state key into app/slideEditor.

--- client/app/reducers/app/index.js
+++ client/app/reducers/app/index.js
@@ -2,14 +2,14 @@ import { combineReducers } from 'redux';
 
 import authentication from 'reducers/app/authenticationReducer';
 import confirmEmail from 'reducers/app/confirmEmailReducer';
-import editor from 'reducers/app/editor';
+import slideEditor from 'reducers/app/slide-editor';
 import feed from 'reducers/app/feedReducer';
 import deckManagement from 'reducers/app/deckManagementReducer';
 
 const app = combineReducers({
   authentication,
   confirmEmail,
-  editor,
+  slideEditor,
   feed,
   deckManagement,
 });
