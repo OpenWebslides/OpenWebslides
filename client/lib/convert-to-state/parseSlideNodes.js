79319d36e6107bdfcb6c3bd55d3938d32b6479c0
Dromin <contact@dromin.be>
2017-07-20 12:19:21 +0200

    Refactored contentItem node parsing to group code for similar contentItemTypes together.

--- client/lib/convert-to-state/parseSlideNodes.js
+++ client/lib/convert-to-state/parseSlideNodes.js
@@ -4,6 +4,11 @@ import { contentItemTypes } from 'constants/contentItemTypes';
 import { generateSlideId, generateContentItemId } from './generateIds';
 import parseInlineProperties from './parseInlineProperties';
 
+const headingNodeNames = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
+const plaintextNodeNames = [...headingNodeNames, 'P', 'LI'];
+const listNodeNames = ['UL', 'OL'];
+const containerNodeNames = ['SECTION', 'ASIDE', ...listNodeNames];
+
 function parseTextContent(textContent, trim = true) {
   if (trim) {
     return textContent.replace(/\s+/g, ' ').trim();
@@ -22,105 +27,94 @@ function parseContentItemNode(node, slideId, contentItemSequence) {
   const { nodeName, children, textContent } = node;
   const contentItemId = generateContentItemId(slideId, contentItemSequence);
   let contentItem = { id: contentItemId };
+  let childItemIds = [];
+  let childItemsById = {};
 
-  // Parse the child items, but not if it's a <figure> tag
-  const { contentItemIds: childItemIds, contentItemsById: childItemsById } = nodeName !== 'FIGURE'
-    ? parseContentItemNodes(children, slideId, contentItemSequence + 1)
-    : parseContentItemNodes([], slideId, contentItemSequence + 1)
-
-  switch (nodeName) {
-    case 'FIGURE':
-      const imgNode = node.children[0];
-      const caption = node.children[1] ? node.children[1].textContent : undefined;
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.ILLUSTRATIVE_IMAGE,
-        src: imgNode.src,
-        alt: imgNode.alt,
-        caption,
-      };
-      break;
-    case 'SECTION':
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.SECTION,
-        childItemIds,
-      };
-      break;
-    case 'ASIDE':
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.ASIDE,
-        childItemIds,
-      };
-      break;
-    case 'H1':
-    case 'H2':
-    case 'H3':
-    case 'H4':
-    case 'H5':
-    case 'H6':
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.TITLE,
-        text: parseTextContent(textContent),
-        inlineProperties: parseInlineProperties(children),
-      };
-      break;
-    case 'P':
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.PARAGRAPH,
-        text: parseTextContent(textContent),
-        inlineProperties: parseInlineProperties(children),
-      };
-      break;
-    case 'OL':
+  // SECTION, ASIDE, LIST, etc.
+  if (Array.indexOf(containerNodeNames, nodeName) !== -1) {
+    // Add contentItemType + custom properties for different contentItemTypes.
+    if (Array.indexOf(listNodeNames, nodeName) !== -1) {
       contentItem = {
         ...contentItem,
         contentItemType: contentItemTypes.LIST,
-        ordered: true,
-        childItemIds,
+        ordered: nodeName === 'OL',
       };
-      break;
-    case 'UL':
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.LIST,
-        ordered: false,
-        childItemIds,
-      };
-      break;
-    case 'LI':
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.LIST_ITEM,
-        text: parseTextContent(textContent),
-        inlineProperties: parseInlineProperties(children),
-      };
-      break;
-    case 'IMG':
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.DECORATIVE_IMAGE,
-        src: node.src,
-        altText: node.alt,
-      };
-      break;
-    case 'IFRAME':
-      contentItem = {
-        ...contentItem,
-        contentItemType: contentItemTypes.IFRAME,
-        src: node.src,
-      };
-      break;
-    default:
-      return {
-        contentItemId: null,
-        contentItemsById: {},
-      };
+    } else if (nodeName === 'ASIDE') {
+      contentItem = { ...contentItem, contentItemType: contentItemTypes.ASIDE };
+    } else if (nodeName === 'SECTION') {
+      contentItem = { ...contentItem, contentItemType: contentItemTypes.SECTION };
+    } else {
+      console.error(`Unrecognized container node name: ${nodeName}`);
+    }
+
+    // Parse container childrenItems.
+    ({contentItemIds: childItemIds, contentItemsById: childItemsById } = parseContentItemNodes(
+      children,
+      slideId,
+      contentItemSequence + 1
+    ));
+
+    // Add childItemIds to contentItem.
+    contentItem = {
+      ...contentItem,
+      childItemIds,
+    };
+  }
+  // P, LI, H1, H2, ...
+  else if (Array.indexOf(plaintextNodeNames, nodeName) !== -1) {
+    // Add contentItemType.
+    if (Array.indexOf(headingNodeNames, nodeName) !== -1) {
+      contentItem = { ...contentItem, contentItemType: contentItemTypes.TITLE };
+    } else if (nodeName === 'LI') {
+      contentItem = { ...contentItem, contentItemType: contentItemTypes.LIST_ITEM };
+    } else if (nodeName === 'P') {
+      contentItem = { ...contentItem, contentItemType: contentItemTypes.PARAGRAPH };
+    } else {
+      console.error('Unrecognized plain text node name.');
+    }
+
+    // Add text + inlineProperties.
+    contentItem = {
+      ...contentItem,
+      text: parseTextContent(textContent),
+      inlineProperties: parseInlineProperties(children),
+    };
+  }
+  else if (nodeName === 'IMG') {
+    contentItem = {
+      ...contentItem,
+      contentItemType: contentItemTypes.DECORATIVE_IMAGE,
+      src: node.src,
+      altText: node.alt,
+    };
+  }
+  else if (nodeName === 'FIGURE') {
+    const imgNode = node.children[0];
+    const caption = node.children[1] ? node.children[1].textContent : undefined;
+    contentItem = {
+      ...contentItem,
+      contentItemType: contentItemTypes.ILLUSTRATIVE_IMAGE,
+      src: imgNode.src,
+      alt: imgNode.alt,
+      caption,
+    };
+  }
+  else if (nodeName === 'IFRAME') {
+    contentItem = {
+      ...contentItem,
+      contentItemType: contentItemTypes.IFRAME,
+      src: node.src,
+    };
+  }
+  // Skip unrecognized nodeNames.
+  else {
+    return {
+      contentItemId: null,
+      contentItemsById: {},
+    };
   }
 
+  // Merge new content item and child content items (if there are any) into a single byId object.
   const contentItemsById = {
     [contentItemId]: contentItem,
     ...childItemsById,
