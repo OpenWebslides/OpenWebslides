15eea5055bde06912021ce2554b34f872ae02988
Dromin <contact@dromin.be>
2017-07-22 16:30:33 +0200

    * Cleaned up decks/slides/contentItems actions to have a more consistent interface. * Fixed contentBlock -> contentItem naming in actions / reducers. * Merged DELETE_SLIDE / DELETE_CONTENT_BLOCKS into a single action (DELETE_SLIDE) so that it is easier to undo. * Made sure slides/contentItems action objects are never created directly; action creator functions are now used everywhere. * Fixed bug in deleteSlideSaga where section contentItems would not properly be deleted with their parent slide. * Updated ContentEditable proptypes to better validate their contentItem.

--- client/app/actions/entities/decks/index.js
+++ client/app/actions/entities/decks/index.js
@@ -1,4 +1,4 @@
-// Action types
+
 export const UPDATE_DECK = 'UPDATE_DECK';
 export const UPDATE_DECK_SUCCESS = 'UPDATE_DECK_SUCCESS';
 export const UPDATE_DECK_FAILURE = 'UPDATE_DECK_FAILURE';
@@ -7,16 +7,15 @@ export const FETCH_DECK = 'FETCH_DECK';
 export const FETCH_DECK_SUCCESS = 'FETCH_DECK_SUCCESS';
 export const FETCH_DECK_FAILURE = 'FETCH_DECK_FAILURE';
 
-// Action Creators
+export function updateDeck() {
+  return {
+    type: UPDATE_DECK,
+  };
+}
+
 export function fetchDeck(deckId) {
   return {
     type: FETCH_DECK,
     meta: { deckId },
-  };
-}
-
-export function updateDeck() {
-  return {
-    type: UPDATE_DECK,
   };
 }
