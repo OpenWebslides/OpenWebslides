ab92615bcfdf20d41e009b8f87dc24f6024be84f
Dromin <contact@dromin.be>
2017-07-22 17:05:06 +0200

    Split slide-editor reducers up into different functions for better overview / so that similar functionality can be shared between actions. Removed unnecessary allIds reducers for slides & contentItems.

--- client/app/reducers/entities/content-items/byIdReducer.js
+++ client/app/reducers/entities/content-items/byIdReducer.js
@@ -7,72 +7,78 @@ import { FETCH_DECK_SUCCESS } from 'actions/entities/decks';
 
 const initialState = Immutable({});
 
-function byId(state = initialState, action) {
-  let contentItem;
+function addContentItem(state, action) {
+  const contentItemId = action.payload.contentItemId;
 
-  switch (action.type) {
+  return Immutable.merge(state,
+    {
+      [contentItemId]: {
+        id: contentItemId,
+        contentItemType: action.payload.contentItemType,
+        // #TODO diversify based on contentItemType
+        text: '',
+        inlineProperties: [],
+      },
+    }
+  );
+}
 
-    case ADD_CONTENT_ITEM:
-      const contentItemId = action.payload.contentItemId;
+function updateContentItem(state, action) {
+  const contentItem = state[action.payload.contentItemId];
 
-      return Immutable.merge(state,
-        {
-          [contentItemId]: {
-            id: contentItemId,
-            contentItemType: action.payload.contentItemType,
-            // #TODO diversify based on contentItemType
-            text: '',
-            inlineProperties: [],
-          },
-        }
-      );
+  return Immutable.merge(
+    state,
+    {
+      [contentItem.id]: action.payload.props,
+    },
+    { deep: true },
+  );
+}
 
-    case UPDATE_CONTENT_ITEM:
-      contentItem = state[action.payload.contentItemId];
+function deleteContentItem(state, action) {
+  const contentItem = state[action.payload.contentItemId];
 
-      return Immutable.merge(
-        state,
-        {
-          [contentItem.id]: action.payload.props,
+  // Remove the deleted content item from the byId object.
+  state = _.omit(contentItem.id);
+
+  // If there are descendant items, remove these from the byId object as well.
+  if (action.payload.descendantItemIds.length !== 0) {
+    state = _.omit(state, action.payload.descendantItemIds);
+  }
+
+  // If there is a parent item, remove the deleted contentItem's id from its childItemIds array.
+  if (action.payload.parentItemId !== null) {
+    const parentItem = state[action.payload.parentItemId];
+    state = Immutable.merge(
+      state,
+      {
+        [parentItem.id]: {
+          childItemIds: _.without(parentItem.childItemIds, contentItem.id),
         },
-        { deep: true },
-      );
+      },
+      { deep: true },
+    );
+  }
 
-    case DELETE_CONTENT_ITEM:
-      contentItem = state[action.payload.contentItemId];
+  return state;
+}
 
-      // Remove the deleted content item from the byId object.
-      state = _.omit(contentItem.id);
+function deleteSlide(state, action) {
+  return _.omit(state, action.payload.contentItemIds);
+}
 
-      // If there are descendant items, remove these from the byId object as well.
-      if (action.payload.descendantItemIds.length !== 0) {
-        state = _.omit(state, action.payload.descendantItemIds);
-      }
+function fetchDeckSuccess(state, action) {
+  return action.payload.contentItemsById;
+}
 
-      // If there is a parent item, remove the deleted contentItem's id from its childItemIds array.
-      if (action.payload.parentItemId !== null) {
-        const parentItem = state[action.payload.parentItemId];
-        state = Immutable.merge(
-          state,
-          {
-            [parentItem.id]: {
-              childItemIds: _.without(parentItem.childItemIds, contentItem.id),
-            },
-          },
-          { deep: true },
-        );
-      }
-
-      return state;
-
-    case DELETE_SLIDE:
-      return _.omit(state, action.payload.contentItemIds);
-
-    case FETCH_DECK_SUCCESS:
-      return action.payload.contentItemsById;
-
-    default:
-      return state;
+function byId(state = initialState, action) {
+  switch (action.type) {
+    case ADD_CONTENT_ITEM: return addContentItem(state, action);
+    case UPDATE_CONTENT_ITEM: return updateContentItem(state, action);
+    case DELETE_CONTENT_ITEM: return deleteContentItem(state, action);
+    case DELETE_SLIDE: return deleteSlide(state, action);
+    case FETCH_DECK_SUCCESS: return fetchDeckSuccess(state, action);
+    default: return state;
   }
 }
 
