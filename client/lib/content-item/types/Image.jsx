87b0a8dd50760e0ba25cdc9da43fa28c2e46a154
Dromin <contact@dromin.be>
2017-07-23 00:11:36 +0200

    Removed PropTypes from contentItemType components; PropTypes can be inherited from ContentItem component.

--- client/lib/content-item/types/Image.jsx
+++ client/lib/content-item/types/Image.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 export default function Image(props) {
   const { contentItem } = props;
 
   return <img src={contentItem.src} alt={contentItem.altText} />;
 }
-
-Image.propTypes = {
-  editable: PropTypes.bool,
-  contentItem: PropTypes.objectOf(Object).isRequired,
-};
-
-Image.defaultProps = {
-  editable: false,
-};
