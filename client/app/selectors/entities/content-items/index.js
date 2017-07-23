1fdce3ece834d5bc0fe664cab3cc1c2c41aec8a2
Dromin <contact@dromin.be>
2017-07-23 17:27:12 +0200

    Added ability to delete contentItems by pressing backspace in an empty contentEditable. Moved code to find contentItem descendant ids from sagas into selectors.

--- client/app/selectors/entities/content-items/index.js
+++ client/app/selectors/entities/content-items/index.js
@@ -1,4 +1,18 @@
+import _ from 'lodash';
 import { createSelector } from 'reselect';
+
+const getContentItemDescendantItemIds = (contentItem, contentItemsById) => {
+  let result = [contentItem.id];
+
+  if (contentItem.childItemIds instanceof Array && contentItem.childItemIds.length > 0) {
+    const descendantItemIds = contentItem.childItemIds.map(childItemId => {
+      return getContentItemDescendantItemIds(contentItemsById[childItemId], contentItemsById);
+    });
+    result = result.concat(_.flatten(descendantItemIds));
+  }
+
+  return result;
+};
 
 // Returns a contentItem object containing its nested children.
 const nestChildItemsInsideContentItem = (contentItem, contentItemsById) => {
@@ -34,6 +48,16 @@ export const getContentItemsById = (state) => {
 export const getContentItemById = (state, id) => {
   return state.entities.contentItems.byId[id];
 };
+
+// Get an array of ids for the contentItems that are nested within the contentItem with the given id.
+// (Note: this includes the id of the contentItem itself as the first element in the array.)
+// #TODO does this need re-reselect?
+export const getContentItemDescendantItemIdsById = createSelector(
+  [getContentItemsById, getContentItemById],
+  (contentItemsById, contentItem) => {
+    return getContentItemDescendantItemIds(contentItem, contentItemsById);
+  },
+);
 
 // Get the single contentItem that matches $id, with its child contentItem objects nested inside it.
 export const getNestedContentItemObjectById = createSelector(
