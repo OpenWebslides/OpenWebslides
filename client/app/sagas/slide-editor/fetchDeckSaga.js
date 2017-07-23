102d7a0e02be6563074ee76280ea7d283b8fb0fe
Dromin <contact@dromin.be>
2017-07-23 17:23:50 +0200

    Updated test deck.

--- client/app/sagas/slide-editor/fetchDeckSaga.js
+++ client/app/sagas/slide-editor/fetchDeckSaga.js
@@ -661,9 +661,37 @@ const testDeckEmpty = `
 </head>
 
 <body class="shower list">
-	<section class="slide" id="TestSlideSections">
+	<section class="slide" id="TestEmptySlide">
 		
 	</section>
+	
+	<section class="slide" id="TestNestedSections">
+	  <section>
+      <h2>...</h2>
+      <section>
+        <section>
+          <section>
+            <h3>I Just Want To <strong>Sleep</strong> T__T</h3>
+          </section>
+        </section>
+      </section>
+    </section>
+  </section>
+  
+  <section class="slide" id="TestComplicatedDelete">
+  <h1>Heading level 1</h1>
+  <ul>
+    <li>Unordered item 1</li>
+    <li>Unordered item 2</li>
+    <li>Unordered item 3</li>
+  </ul>
+  <h2>Heading level 2</h2>
+  <ol>
+    <li>Ordered item 1</li>
+    <li>Ordered item 2</li>
+    <li>Ordered item 3</li>
+  </ol>
+</section>
 
 	<div class="progress"></div>
 	<footer class="badge">
