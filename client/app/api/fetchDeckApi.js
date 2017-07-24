a9d80c09a23aa198b3d5e5e1bef92987a4d0d08c
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-07-24 14:21:43 +0200

    Makes sure html string is returned from fetchDeckApi

--- client/app/api/fetchDeckApi.js
+++ client/app/api/fetchDeckApi.js
@@ -3,12 +3,13 @@ import ApiRequest from './helpers/apiHelper';
 async function fetchDeck(deckId) {
   const request = new ApiRequest();
 
-  request
-    .setMethod('GET')
-    .setEndpoint(`api/decks/${deckId}`)
-    .addHeader('Accept', 'text/html');
+  request.setMethod('GET').setEndpoint(`decks/${deckId}`).addHeader('Accept', 'text/html');
 
-  return request.executeRequest();
+  const response = await request.executeRequest();
+
+  const responseBody = await response.text();
+
+  return responseBody;
 }
 
 export default fetchDeck;
