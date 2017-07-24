0ab89583c4c7259bd7b2061c3d71b90c1ad36590
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-24 14:40:36 +0200

    Fix asset routing specs

--- spec/routing/assets_routing_spec.rb
+++ spec/routing/assets_routing_spec.rb
@@ -4,35 +4,24 @@ require 'rails_helper'
 
 RSpec.describe 'assets routing', :type => :routing do
   it 'routes assets endpoint' do
-    route = '/api/assets'
+    route = '/api/decks/foo/assets'
+    params = { :deck_id => 'foo', :relationship => 'assets', :source => 'api/decks' }
 
-    expect(:get => route).not_to be_routable
+    expect(:get => route).to route_to 'api/assets#get_related_resources', params
+    expect(:post => route).to route_to 'api/assets#create', :deck_id => 'foo'
     expect(:patch => route).not_to be_routable
     expect(:put => route).not_to be_routable
-    expect(:post => route).to route_to 'api/assets#create'
     expect(:delete => route).not_to be_routable
   end
 
   it 'routes asset endpoint' do
-    route = '/api/assets/foo'
+    route = '/api/decks/foo/assets/bar'
+    params = { :id => 'bar', :deck_id => 'foo' }
 
-    expect(:get => route).to route_to 'api/assets#show', :id => 'foo'
-    expect(:patch => route).to route_to 'api/assets#update', :id => 'foo'
-    expect(:put => route).to route_to 'api/assets#update', :id => 'foo'
+    expect(:get => route).to route_to 'api/assets#show', params
+    expect(:patch => route).to route_to 'api/assets#update', params
+    expect(:put => route).to route_to 'api/assets#update', params
     expect(:post => route).not_to be_routable
-    expect(:delete => route).to route_to 'api/assets#destroy', :id => 'foo'
-  end
-
-  it 'routes asset deck relationship endpoint' do
-    route = '/api/assets/foo/relationships/deck'
-    params = { :asset_id => 'foo', :relationship => 'deck' }
-
-    expect(:get => '/api/assets/foo/deck').to route_to 'api/decks#get_related_resource', params.merge(:source => 'api/assets')
-
-    expect(:get => route).to route_to 'api/assets#show_relationship', params
-    expect(:patch => route).not_to be_routable
-    expect(:put => route).not_to be_routable
-    expect(:post => route).not_to be_routable
-    expect(:delete => route).not_to be_routable
+    expect(:delete => route).to route_to 'api/assets#destroy', params
   end
 end
