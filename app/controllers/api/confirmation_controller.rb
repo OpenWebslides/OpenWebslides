9a54f6cfe061dd275a210b8bca418efc929fd5ba
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-25 12:12:58 +0200

    Rewrite Confirmation API to support JSONAPI::Utils

--- app/controllers/api/confirmation_controller.rb
+++ app/controllers/api/confirmation_controller.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 module Api
-  class ConfirmationController < ApiController; end
+  class ConfirmationController < ApiController
+    # Authorization
+    after_action :verify_authorized
+
+    def create
+      authorize :confirmation
+
+      @user = User.confirm_by_token resource_params[:confirmation_token]
+
+      if @user.errors.empty?
+        jsonapi_render :json => @user, :status => :created
+      else
+        jsonapi_render_errors :json => @user, :status => :unprocessable_entity
+      end
+    end
+  end
 end
