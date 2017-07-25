4f1ae4614ef4df898089bfbf6787168fed3a1820
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-07-25 14:52:54 +0200

    Adds parsing of state to html for deck updating ( needs to be refactored more)

--- client/app/sagas/updateDeckSaga.js
+++ client/app/sagas/updateDeckSaga.js
@@ -3,57 +3,131 @@ import updateDeckApi from 'api/updateDeckApi';
 
 import { UPDATE_DECK } from 'actions/entities/decks';
 
-function addWrappingHtml(string) {
+import { getActiveDeckId } from 'selectors/app/slide-editor';
+import { getDeckById } from 'selectors/entities/decks';
+import { getSlideById } from 'selectors/entities/slides';
+import { getContentItemById } from 'selectors/entities/content-items';
+
+import { contentItemTypes } from 'constants/contentItemTypes';
+import { getHTMLStringFromInlinePropertiesAndText } from 'lib/content-editable/inlineProperties';
+
+function* convertContentItems(contentItemIds, headingLevel) {
+  let string = '';
+
+  for (let i = 0; i < contentItemIds.length; i += 1) {
+    const contentItemObject = yield select(getContentItemById, contentItemIds[i]);
+
+    switch (contentItemObject.contentItemType) {
+      case contentItemTypes.TITLE: {
+        const heading = `h${headingLevel}`;
+
+        const text = getHTMLStringFromInlinePropertiesAndText(
+          contentItemObject.inlineProperties,
+          contentItemObject.text,
+        );
+
+        string += `<${heading}>${text}</${heading}>`;
+        break;
+      }
+      case contentItemTypes.PARAGRAPH: {
+        const text = getHTMLStringFromInlinePropertiesAndText(
+          contentItemObject.inlineProperties,
+          contentItemObject.text,
+        );
+
+        string += `<p>${text}</p>`;
+        break;
+      }
+      case contentItemTypes.SECTION: {
+        const childContent = yield convertContentItems(contentItemObject.childItemIds, headingLevel + 1);
+
+        string += `<section>${childContent}</section>`;
+        break;
+      }
+      case contentItemTypes.ASIDE: {
+        const childContent = yield convertContentItems(contentItemObject.childItemIds, headingLevel + 1);
+
+        string += `<aside>${childContent}</aside>`;
+        break;
+      }
+      case contentItemTypes.LIST: {
+        const listType = contentItemObject.ordered ? 'ol' : 'ul';
+        const listItems = yield convertContentItems(contentItemObject.childItemIds, headingLevel);
+
+        string += `<${listType}>${listItems}</${listType}>`;
+        break;
+      }
+      case contentItemTypes.LIST_ITEM: {
+        const text = getHTMLStringFromInlinePropertiesAndText(
+          contentItemObject.inlineProperties,
+          contentItemObject.text,
+        );
+
+        string += `<li>${text}</li>`;
+        break;
+      }
+      case contentItemTypes.IFRAME: {
+        const { src, alt } = contentItemObject;
+
+        string += `<iframe src="${src}" alt="${alt}"/>`;
+        break;
+      }
+      case contentItemTypes.ILLUSTRATIVE_IMAGE: {
+        const { src, alt, caption } = contentItemObject;
+
+        string += `<figure><img src="${src}" alt="${alt}"/><figcaption><a href="${src}">${caption}</a></figcaption></figure>`;
+        break;
+      }
+      case contentItemTypes.DECORATIVE_IMAGE: {
+        const { src, alt } = contentItemObject;
+
+        string += `<img src="${src}" alt="${alt}"/>`;
+        break;
+      }
+      default:
+        break;
+    }
+  }
+  return string;
+}
+
+function* convertSlide(slideId) {
+  const slideObject = yield select(getSlideById, slideId);
+  const contentItemsString = yield convertContentItems(slideObject.contentItemIds, 1);
+
+  const slideString = `<div class="slide" data-level="${slideObject.level}">${contentItemsString}</div>`;
+
+  return slideString;
+}
+
+function* convertToHTML(deckObject) {
+  const slideStringArray = yield deckObject.slideIds.map(slideId => call(convertSlide, slideId));
+  const deckString = slideStringArray.join('');
+
   return `
     <header class="caption">
     <h1>Presentation</h1>
     <p><a href="">Author</a></p>
     </header>
-    ${string}
+    ${deckString}
     <div class="progress"></div>
-    <footer><p>Just some random data</p></footer>
   `;
-}
-
-function convertSlidesToHtml(slides, contentBlocks) {
-  const slideIds = Object.keys(slides);
-  let htmlString = '';
-
-  slideIds.forEach(id => {
-    const slideContentHTML = (function convertContentToHTML(slideContent) {
-      let HTMLContent = '';
-
-      slideContent.forEach(content => {
-        if (content.type === 'contentGroup') {
-          const childContent = convertContentToHTML(content.childContent);
-
-          HTMLContent += `<section>${childContent}</section>`;
-        } else {
-          HTMLContent += convertToHTML(contentBlocks[content.id].data.getCurrentContent());
-        }
-      });
-      return HTMLContent;
-    })(slides[id].content);
-
-    htmlString += `<section class="slide">${slideContentHTML}</section>`;
-  });
-  console.log(htmlString);
-  return addWrappingHtml(htmlString);
 }
 
 function* doUpdateDeck() {
   try {
-    const state = yield select();
-    const HTMLString = yield convertSlidesToHtml(state.entities.slides.byId, state.entities.contentItems.byId);
+    const activeDeckId = yield select(getActiveDeckId);
+    const deck = yield select(getDeckById, activeDeckId);
+    const htmlString = yield convertToHTML(deck);
 
-    yield call(updateDeckApi, 2, HTMLString);
+    yield call(updateDeckApi, deck.id, htmlString);
   } catch (e) {
     console.log(e);
   }
 }
 
-function* fetchSlidesWatcher() {
+function* updateDeckWatcher() {
   yield takeLatest(UPDATE_DECK, doUpdateDeck);
 }
 
-export default fetchSlidesWatcher;
+export default updateDeckWatcher;
