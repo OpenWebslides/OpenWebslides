be2940bda5ca2c53a09c32aa989a6d39c17b790b
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-25 12:19:18 +0200

    Rewrite Notifications API to support JSONAPI::Utils

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -2,6 +2,27 @@
 
 module Api
   class NotificationsController < ApiController
+    # Authentication
     after_action :renew_token
+
+    # Authorization
+    after_action :verify_authorized, :only => :show
+    after_action :verify_policy_scoped, :only => :index
+
+    # GET /notifications
+    def index
+      @notifications = policy_scope Notification
+
+      jsonapi_render :json => @notifications
+    end
+
+    # GET /notifications/:id
+    def show
+      @notification = Notification.find params[:id]
+
+      authorize @notification
+
+      jsonapi_render :json => @notification
+    end
   end
 end
