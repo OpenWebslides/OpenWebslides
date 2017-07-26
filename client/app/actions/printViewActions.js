f53c61700e394b6876f40d1912c3bdc27a6f5eed
Luca Dorigo <dorigoluca@gmail.com>
2017-07-26 14:15:47 +0200

    Added toggles to change the display of images

--- client/app/actions/printViewActions.js
+++ client/app/actions/printViewActions.js
@@ -1,5 +1,6 @@
 export const CHANGE_IMAGE_OPTIONS = 'CHANGE_IMAGE_OPTIONS';
 export const CHANGE_IFRAME_OPTIONS = 'CHANGE_IFRAME_OPTIONS';
+export const CHANGE_DECORATIVE_IMAGE_OPTIONS = 'CHANGE_DECORATIVE_IMAGE_OPTIONS';
 
 export function changeImagePref(choice) {
   return {
@@ -11,6 +12,13 @@ export function changeImagePref(choice) {
 export function changeIframePref(choice) {
   return {
     type: CHANGE_IFRAME_OPTIONS,
+    payload: choice,
+  };
+}
+
+export function changeDecorativeImagePref(choice){
+  return {
+    type: CHANGE_DECORATIVE_IMAGE_OPTIONS,
     payload: choice,
   };
 }
