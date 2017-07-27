fbfdc5bc1834349c60ba1c7176585395f7e819cc
Dromin <contact@dromin.be>
2017-07-27 16:18:54 +0200

    Formatting updates after eslint changes.

--- client/lib/state-traversal/contentItems.js
+++ client/lib/state-traversal/contentItems.js
@@ -24,8 +24,9 @@ function findNearestValidContentItemId(
 
   const parentItemId = _.last(ancestorItemIds);
   if (parentItemId !== undefined) {
-      siblingItemIds = contentItemsById[parentItemId].childItemIds;
-  } else {
+    siblingItemIds = contentItemsById[parentItemId].childItemIds;
+  }
+  else {
     siblingItemIds = slideContentItemIds;
   }
 
@@ -33,10 +34,9 @@ function findNearestValidContentItemId(
   const contentItem = contentItemsById[contentItemId];
 
   // First check if the contentItem is a container element.
-  // (Note: we don't do this on the initial call (original contentItem for which
-  // we're looking for a previous contentItem) because then the section children
-  // are not actual previous elements.)
-  if (checkContainerChildren && containerItemValidator(contentItem) === true){
+  // (Note: we don't do this on the initial call (original contentItem for which we're looking for a
+  // previous contentItem) because then the section children are not actual previous elements.)
+  if (checkContainerChildren && containerItemValidator(contentItem) === true) {
     if (debug) console.log(`${contentItemId} isSection`);
     // If it is a container, check its first/last child.
     newContentItemId = direction === directions.UP
@@ -49,15 +49,22 @@ function findNearestValidContentItemId(
   }
   // If the contentItem is the first/last in its list of siblings.
   else if (
-    (direction === directions.UP && indexInSiblingItemIds === 0) ||
-    (direction === directions.DOWN && indexInSiblingItemIds === siblingItemIds.length - 1)
+    (
+      direction === directions.UP &&
+      indexInSiblingItemIds === 0
+    ) ||
+    (
+      direction === directions.DOWN &&
+      indexInSiblingItemIds === siblingItemIds.length - 1
+    )
   ) {
     if (debug) console.log(`${contentItemId} isFirst/Last`);
     // Go up a level.
     newContentItemId = _.last(ancestorItemIds);
     if (newContentItemId !== undefined) {
       newAncestorItemIds = _.dropRight(ancestorItemIds, 1);
-    } else {
+    }
+    else {
       newContentItemId = null;
     }
     // Don't go back down this section.
@@ -83,25 +90,23 @@ function findNearestValidContentItemId(
   if (newContentItemId === null) {
     return null;
   }
-  // If a previous contentItemId was found.
+  // If a previous contentItemId was found, & it was valid, this is the contentItemId we were
+  // looking for.
+  else if (contentItemValidator(contentItemsById[newContentItemId]) === true) {
+    return newContentItemId;
+  }
+  // If the contentItem is not valid, we need to search further.
   else {
-    // If the contentItem is valid, this is the contentItemId we were looking for.
-    if (contentItemValidator(contentItemsById[newContentItemId]) === true) {
-      return newContentItemId;
-    }
-    // If the contentItem is not valid, we need to search further.
-    else {
-      return findNearestValidContentItemId(
-        direction,
-        newContentItemId,
-        newAncestorItemIds,
-        slideContentItemIds,
-        contentItemsById,
-        contentItemValidator,
-        containerItemValidator,
-        newCheckContainerChildren,
-      );
-    }
+    return findNearestValidContentItemId(
+      direction,
+      newContentItemId,
+      newAncestorItemIds,
+      slideContentItemIds,
+      contentItemsById,
+      contentItemValidator,
+      containerItemValidator,
+      newCheckContainerChildren,
+    );
   }
 }
 
@@ -116,13 +121,11 @@ function findNearestAncestorIdWithAtLeastAmountOfChildren(
   }
   // If there are ancestors left.
   else {
-    let newAncestorItemIds;
-
     // Get the parentItem.
     const parentItemId = _.last(ancestorItemIds);
     const parentItem = contentItemsById[parentItemId];
     // Remove the parent from the ancestorItemIds.
-    newAncestorItemIds = _.dropRight(ancestorItemIds, 1);
+    const newAncestorItemIds = _.dropRight(ancestorItemIds, 1);
 
     // Test if the parent is a valid ancestor.
     if (parentItem.childItemIds.length >= amount) {
@@ -145,9 +148,14 @@ function findAllContentItemDescendantItemIds(contentItemId, contentItemsById) {
   let descendantItemIds = [];
 
   // If this contentItem is a container.
-  if (Array.indexOf(containerContentItemTypes, contentItem.contentItemType) !== -1) {
+  if (
+    Array.indexOf(
+      containerContentItemTypes,
+      contentItem.contentItemType,
+    ) !== -1
+  ) {
     // Add all of its children and their descendants to the array.
-    contentItem.childItemIds.forEach(childItemId => {
+    contentItem.childItemIds.forEach((childItemId) => {
       descendantItemIds = descendantItemIds.concat(childItemId);
       descendantItemIds = descendantItemIds.concat(
         findAllContentItemDescendantItemIds(childItemId, contentItemsById),
@@ -161,35 +169,32 @@ function findAllContentItemDescendantItemIds(contentItemId, contentItemsById) {
 // Exported functions ----------------------------------------------------------
 
 /**
- * Finds the previous valid contentItem (in terms of tree leaves) as compared
- * to the contentItem with id $contentItemId. Validity is determined by a
+ * Finds the previous valid contentItem (in terms of tree leaves) as compared to the contentItem
+ * with id $contentItemId. Validity is determined by a
  * validator function.
  *
  * @param contentItemId
  *        The id of the contentItem where we start searching.
  * @param ancestorItemIds
- *        The list of ids of the ancestor items of the contentItem where we
- *        start searching.
+ *        The list of ids of the ancestor items of the contentItem where we start searching.
  * @param slideContentItemIds
- *        The list of ids of contentItems that are direct children of the
- *        containing slide of the contentItem where we start searching. (We need
- *        this because the slide is the highest possible ancestor of a
- *        contentItem and it can't be included in the ancestorItemIds array.)
+ *        The list of ids of contentItems that are direct children of the containing slide of the
+ *        contentItem where we start searching. (We need this because the slide is the highest
+ *        possible ancestor of a contentItem and it can't be included in the ancestorItemIds array.)
  * @param contentItemsById
  *        The contentItemsById object.
  * @param contentItemValidator
- *        The function that decides if a contentItem is considered 'valid'. It
- *        is passed a contentItem as an argument and should return TRUE if this
- *        contentItem is valid, FALSE if it is not.
+ *        The function that decides if a contentItem is considered 'valid'. It is passed a
+ *        contentItem as an argument and should return TRUE if this contentItem is valid, FALSE if
+ *        it is not.
  * @param containerItemValidator
- *        The function that decides if a contentItem is considered a container.
- *        It is passed a contentItem as an argument and should return TRUE if
- *        this contentItem is a container, FALSE if it is not.
- *        (The reason we use a validator function instead of just using the
- *        containerContentItemTypes constant is that for some applications lists
- *        should be considered containers, while for others they should not. By
- *        doing it this way, the caller of this function has full control over
- *        which contentItems are considered containers and which are not.
+ *        The function that decides if a contentItem is considered a container. It is passed a
+ *        contentItem as an argument and should return TRUE if this contentItem is a container,
+ *        FALSE if it is not. (The reason we use a validator function instead of just using the
+ *        containerContentItemTypes constant is that for some applications lists should be
+ *        considered containers, while for others they should not. By doing it this way, the caller
+ *        of this function has full control over which contentItems are considered containers and
+ *        which are not.
  */
 export function getPreviousValidContentItemId(
   contentItemId,
@@ -211,35 +216,32 @@ export function getPreviousValidContentItemId(
 }
 
 /**
- * Finds the next valid contentItem (in terms of tree leaves) as compared
- * to the contentItem with id $contentItemId. Validity is determined by a
+ * Finds the next valid contentItem (in terms of tree leaves) as compared to the contentItem
+ * with id $contentItemId. Validity is determined by a
  * validator function.
  *
  * @param contentItemId
  *        The id of the contentItem where we start searching.
  * @param ancestorItemIds
- *        The list of ids of the ancestor items of the contentItem where we
- *        start searching.
+ *        The list of ids of the ancestor items of the contentItem where we start searching.
  * @param slideContentItemIds
- *        The list of ids of contentItems that are direct children of the
- *        containing slide of the contentItem where we start searching. (We need
- *        this because the slide is the highest possible ancestor of a
- *        contentItem and it can't be included in the ancestorItemIds array.)
+ *        The list of ids of contentItems that are direct children of the containing slide of the
+ *        contentItem where we start searching. (We need this because the slide is the highest
+ *        possible ancestor of a contentItem and it can't be included in the ancestorItemIds array.)
  * @param contentItemsById
  *        The contentItemsById object.
  * @param contentItemValidator
- *        The function that decides if a contentItem is considered 'valid'. It
- *        is passed a contentItem as an argument and should return TRUE if this
- *        contentItem is valid, FALSE if it is not.
+ *        The function that decides if a contentItem is considered 'valid'. It is passed a
+ *        contentItem as an argument and should return TRUE if this contentItem is valid, FALSE if
+ *        it is not.
  * @param containerItemValidator
- *        The function that decides if a contentItem is considered a container.
- *        It is passed a contentItem as an argument and should return TRUE if
- *        this contentItem is a container, FALSE if it is not.
- *        (The reason we use a validator function instead of just using the
- *        containerContentItemTypes constant is that for some applications lists
- *        should be considered containers, while for others they should not. By
- *        doing it this way, the caller of this function has full control over
- *        which contentItems are considered containers and which are not.
+ *        The function that decides if a contentItem is considered a container. It is passed a
+ *        contentItem as an argument and should return TRUE if this contentItem is a container,
+ *        FALSE if it is not. (The reason we use a validator function instead of just using the
+ *        containerContentItemTypes constant is that for some applications lists should be
+ *        considered containers, while for others they should not. By doing it this way, the caller
+ *        of this function has full control over which contentItems are considered containers and
+ *        which are not.
  */
 export function getNextValidContentItemId(
   contentItemId,
@@ -264,13 +266,11 @@ export function getNextValidContentItemId(
  * Finds the ancestor contentItem that has at least $amount of children.
  *
  * @param ancestorItemIds
- *        The list of ids of the ancestor items of the contentItem where we
- *        start searching.
+ *        The list of ids of the ancestor items of the contentItem where we start searching.
  * @param contentItemsById
  *        The contentItemsById object.
  * @param amount
- *        The amount of children that the ancestor we're looking for should
- *        at least have.
+ *        The amount of children that the ancestor we're looking for should at least have.
  */
 export function getNearestAncestorIdWithAtLeastAmountOfChildren(
   ancestorItemIds,
