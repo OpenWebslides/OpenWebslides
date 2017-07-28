22651626543da1bd806179cee9c8afdbedada4c9
Luca Dorigo <dorigoluca@gmail.com>
2017-07-28 10:05:37 +0200

    Some refactoring + added some comments

--- client/app/presentationals/components/print-view/PrintView.jsx
+++ client/app/presentationals/components/print-view/PrintView.jsx
@@ -1,34 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
+
 import { imgOptions, iframeOptions } from 'constants/printViewOptions';
 import { jasperState } from 'constants/exampleState';
 
 import convertToPrint from 'lib/convert-to-print/index';
 
 export default class PrintView extends Component {
-  // componentDidMount() {
-  //   const id = this.props.id;
-  //   if (!(this.props.entities.decks.byId.id && this.props.entities.decks.byId.id.slides)) {
-  //     this.props.fetchDeckContent(id);
-  //   }
-  // }
+  // TODO: This will have to be fixed after we are actually stocking decks in the back end
+  componentDidMount() {
+    const id = this.props.id;
+    if (!_.get(this.props, `entities.decks.byId.${id}.slideIds`, false)) {
+      this.props.fetchDeck(id);
+    }
+  }
 
   render() {
-    const state = jasperState; // TODO: only until rein parser works
+    const entities = this.props.entities;
 
+    // Get the display preferences from the state:
     const imagePref = this.props.printViewState.images;
     const decorativeImagePref = this.props.printViewState.decorativeImages;
     const iframesPref = this.props.printViewState.iframes;
-    let elements;
-    // if (state.entities.decks.byId.id && state.entities.decks.byId.id.slides) {
-    //   elements = convertToPrint(state, this.props.id);
-    // }
-    elements = convertToPrint(state, this.props.id, imagePref, decorativeImagePref, iframesPref);
-    return (
-      <div className="c_print-view">
-        {elements}
-      </div>
-    );
+
+    let toDisplay;
+    if (!_.get(this.props, `entities.decks.byId.${this.props.id}.slideIds`, false)) {
+      toDisplay = <div> <p> Loading ...</p> </div>;
+    } else {
+      // We pass the entities and preferences as argument to the converter function.
+      const elements = convertToPrint(entities, this.props.id, imagePref, decorativeImagePref, iframesPref);
+      toDisplay = (
+        <div className="c_print-view">
+          {elements}
+        </div>
+      );
+    }
+    return toDisplay;
   }
 }
 
@@ -38,6 +46,7 @@ PrintView.propTypes = {
     iframes: PropTypes.oneOf(Object.keys(iframeOptions)),
     decorativeImages: PropTypes.bool,
   }).isRequired,
-  fetchDeckContent: PropTypes.func.isRequired,
+  entities: PropTypes.object,
+  fetchDeck: PropTypes.func.isRequired,
   id: PropTypes.string.isRequired,
 };
