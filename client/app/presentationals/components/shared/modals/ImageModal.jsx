5aa94cbb7166b015cfa87ae92ff94498fff9be6f
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-01 15:55:13 +0200

    Adjust sagas/actions to redux-form promise-based workings

--- client/app/presentationals/components/shared/modals/ImageModal.jsx
+++ client/app/presentationals/components/shared/modals/ImageModal.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Modal from 'react-modal';
 import PropTypes from 'prop-types';
 
-import ImageUploaderContainer from 'containers/ImageUploaderContainer';
+import ImageUploadFormContainer from 'containers/ImageUploaderContainer';
+import ImageUrlFormContainer from 'containers/slide-editor/asset-forms/ImageUrlFormContainer';
+
 
 const customStyles = {
   content: {
@@ -16,69 +18,23 @@ const customStyles = {
 };
 
 export default class ImageModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { imageSource: '', altText: '', imageType: 'ILLUSTRATIVE' };
-
-    this.handleAddContentItem = this.handleAddContentItem.bind(this);
-  }
-
   shouldComponentUpdate(nextProps, nextState) {
     return nextProps !== this.props || nextState !== this.state;
-  }
-
-  handleAddContentItem(event) {
-    event.preventDefault();
-    this.props.onHandleModalSubmit({ src: this.state.imageSource, alt: this.state.altText });
-    this.setState({ imageSource: '', altText: '', imageType: 'ILLUSTRATIVE' });
   }
 
   render() {
     return (
       <Modal
         isOpen={this.props.isOpen}
-        onRequestClose={this.props.onClose}
+        onRequestClose={this.props.closeModal}
         style={customStyles}
         contentLabel="Add Image"
       >
-        <ImageUploaderContainer onDropAccepted={this.props.onClose} />
-        <form onSubmit={this.handleAddContentItem}>
-          <input
-            type="radio"
-            id="imageTypeChoice1"
-            onChange={event => this.setState({ imageType: event.target.value })}
-            name="imageTypeChoice"
-            value="ILLUSTRATIVE"
-            defaultChecked
-          />
-          <label htmlFor="imageTypeChoice1">Illustrative</label>
 
-          <input
-            type="radio"
-            id="imageTypeChoice2"
-            name="imageTypeChoice"
-            value="DECORATIVE"
-            onChange={event => this.setState({ imageType: event.target.value })}
-          />
-          <label htmlFor="imageTypeChoice2">Decorative</label>
+        <ImageUploadFormContainer handleSubmitSuccess={this.props.closeModal} />
 
-          <input
-            autoFocus
-            type="text"
-            value={this.state.imageSource}
-            placeholder="Enter Image URL"
-            onChange={event => this.setState({ imageSource: event.target.value })}
-          />
+        <ImageUrlFormContainer handleSubmitSuccess={this.props.closeModal} />
 
-          <input
-            type="text"
-            value={this.state.altText}
-            placeholder="Enter Image Alt Text"
-            onChange={event => this.setState({ altText: event.target.value })}
-          />
-
-          <button type="submit">Submit</button>
-        </form>
       </Modal>
     );
   }
@@ -86,6 +42,5 @@ export default class ImageModal extends React.Component {
 
 ImageModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
-  onHandleModalSubmit: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
 };
