af049aeb5fbbe83d6840121a5e20b24d50e1228b
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-01 13:00:26 +0200

    Remove Notification immutability

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,8 +7,6 @@ class Notification < ApplicationRecord
   enum :event_type => %i[deck_created deck_updated]
   validates :event_type, :presence => true
 
-  validate :immutable
-
   ##
   # Associations
   #
@@ -21,13 +19,6 @@ class Notification < ApplicationRecord
   ##
   # Methods
   #
-  def immutable
-    return unless changed? && persisted?
-
-    errors.add :base, :immutable
-    reload
-  end
-
   ##
   # Overrides
   #
