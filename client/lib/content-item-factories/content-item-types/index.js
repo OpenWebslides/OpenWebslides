1758f9c1bdfc365af75499269e92577ef8896726
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-03 14:58:22 +0200

    Adds content item type HOC's

---
+++ client/lib/content-item-factories/content-item-types/index.js
@@ -0,0 +1,13 @@
+import Iframe from './Iframe';
+import Image from './Image';
+import ListItem from './ListItem';
+import Paragraph from './Paragraph';
+import Title from './Title';
+
+export default {
+  IFRAME: Iframe,
+  IMAGE: Image,
+  LIST_ITEM: ListItem,
+  PARAGRAPH: Paragraph,
+  TITLE: Title,
+};
