07067c49466ec4958ac7059237142b0ccc017601
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-07 11:06:11 +0200

    Verify Identity dependent associations

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -3,17 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Identity, :type => :model do
-  it 'is valid with attributes' do
-    expect(build :identity, :with_user).to be_valid
+  describe 'attributes' do
+    it { is_expected.not_to allow_value(nil).for(:uid) }
+    it { is_expected.not_to allow_value('').for(:uid) }
+
+    it { is_expected.not_to allow_value(nil).for(:provider) }
+    it { is_expected.not_to allow_value('').for(:provider) }
+
+    it 'is valid with attributes' do
+      expect(build :identity, :with_user).to be_valid
+    end
+
+    it 'is invalid without attributes' do
+      expect(Identity.new).not_to be_valid
+      expect(Identity.new :uid => 'foo').not_to be_valid
+      expect(Identity.new :provider => 'foo').not_to be_valid
+    end
   end
 
-  it 'is invalid without user' do
-    expect(build :identity).not_to be_valid
-  end
-
-  it 'is invalid without attributes' do
-    expect(Identity.new).not_to be_valid
-    expect(Identity.new :uid => 'foo').not_to be_valid
-    expect(Identity.new :provider => 'foo').not_to be_valid
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
   end
 end
