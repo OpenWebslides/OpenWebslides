06662f423b99703d660ac80c14c91b7fc9af8dec
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-07 11:11:04 +0200

    Verify Notifications dependent associations

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -3,28 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Notification, :type => :model do
-  it 'is invalid without attributes' do
-    expect(Notification.new).not_to be_valid
+  describe 'attributes' do
+    it { is_expected.not_to allow_value(nil).for(:event_type) }
+    it { is_expected.not_to allow_value('').for(:event_type) }
+
+    it 'is invalid without attributes' do
+      expect(Notification.new).not_to be_valid
+    end
+
+    it 'is valid with attributes' do
+      notification = build :notification
+      expect(notification).to be_valid
+    end
+
+    it 'has a valid :event_type enum' do
+      expect(%w[deck_created deck_updated]).to eq Notification.event_types.keys
+    end
   end
 
-  it 'is valid with attributes' do
-    notification = build :notification
-    expect(notification).to be_valid
-  end
-
-  it 'is invalid without deck' do
-    notification = build :notification
-    notification.deck = nil
-    expect(notification).not_to be_valid
-  end
-
-  it 'is invalid without user' do
-    notification = build :notification
-    notification.user = nil
-    expect(notification).not_to be_valid
-  end
-
-  it 'has a valid :event_type enum' do
-    expect(%w[deck_created deck_updated]).to eq Notification.event_types.keys
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:deck) }
   end
 end
