5ee5842fa2df87a4d2f12ec91c9ea3c9e413504f
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-07 16:10:39 +0200

    Adds buttons for navigation of slides

--- client/app/presentationals/components/presentation-view/PresentationViewToolbar.jsx
+++ client/app/presentationals/components/presentation-view/PresentationViewToolbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function PresentationToolbar() {
+
+function PresentationToolbar(props) {
   return (
     <div id="presentation-toolbar">
-      <button>Previous</button>
-      <button>Next</button>
+      <button onClick={() => props.viewFirstSlide()}>First</button>
+      <button onClick={() => props.viewPreviousSlide()}>Previous</button>
+      <button onClick={() => props.viewNextSlide()}>Next</button>
+      <button onClick={() => props.viewLastSlide()}>Last</button>
     </div>
   );
 }
+
+PresentationToolbar.propTypes = {
+  viewFirstSlide: PropTypes.func.isRequired,
+  viewPreviousSlide: PropTypes.func.isRequired,
+  viewNextSlide: PropTypes.func.isRequired,
+  viewLastSlide: PropTypes.func.isRequired,
+};
+
 
 export default PresentationToolbar;
