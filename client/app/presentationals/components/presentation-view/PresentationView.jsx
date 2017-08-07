62b342669e6284237ece129a5c70e585c29a1924
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-07 14:38:40 +0200

    Sets up SlideContainer with new viewType

--- client/app/presentationals/components/presentation-view/PresentationView.jsx
+++ client/app/presentationals/components/presentation-view/PresentationView.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import SlideContainer from 'containers/slide-editor/SlideContainer';
+
 
 class PresentationView extends Component {
   componentDidMount() {
@@ -9,16 +11,23 @@ class PresentationView extends Component {
   }
 
   render() {
-    console.log(this.props);
-    return (
-      <h1>{this.props.activeDeckId}</h1>
-    );
+    if (this.props.activeSlideId) {
+      return <SlideContainer id={this.props.activeSlideId} viewType="PRESENTATION" />;
+    }
+
+    return <h1>Loading...</h1>;
   }
 }
 
 PresentationView.propTypes = {
+  activeSlideId: PropTypes.string,
   fetchDeck: PropTypes.func.isRequired,
   match: PropTypes.objectOf(Object).isRequired,
 };
+
+PresentationView.defaultProps = {
+  activeSlideId: null,
+};
+
 
 export default PresentationView;
