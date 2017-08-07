a84be5dd4a7614d3f6170054d5284cb55c620473
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-07 15:24:54 +0200

    Adds toolbar first draft, WITH TEMPORARY CSS

---
+++ client/app/presentationals/components/presentation-view/PresentationToolbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PresentationToolbar() {
+  return (
+    <div id="presentation-toolbar">
+      <button>Previous</button>
+      <button>Next</button>
+    </div>
+  );
+}
+
+export default PresentationToolbar;
