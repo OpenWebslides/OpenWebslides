d9c10561763f419771cbaf8e1d0ab4ab0056d88a
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-10 15:52:08 +0200

    Adds frame for new hoc

---
+++ client/lib/content-item/hoc-wrappers/contentItemHOC.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default function addContentItemHOC(selectedProps) {
+    return class HOC extends Component {
+      render() {
+        
+          ),
+        );
+      }
+    };
+  };
+}
