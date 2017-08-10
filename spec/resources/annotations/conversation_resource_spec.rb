ea9dbc7dd9d10b92daa4bbf58c34a182b9a4e054
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-10 13:05:01 +0200

    Add ConversationResource

---
+++ spec/resources/annotations/conversation_resource_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Annotations::ConversationResource, :type => :resource do
+  let(:conversation) { create :conversation }
+  let(:context) { {} }
+
+  before do
+    create :user
+
+    # Stub out context[:current_user]
+    mock_method described_class, :context do
+      { :current_user => User.first }
+    end
+  end
+
+  subject { described_class.new conversation, context }
+
+  it { is_expected.to have_primary_key :id }
+
+  it { is_expected.to have_attribute :content_item_id }
+  it { is_expected.to have_attribute :comment_type }
+  it { is_expected.to have_attribute :text }
+
+  it { is_expected.to have_one :user }
+  it { is_expected.to have_one :deck }
+  it { is_expected.to have_many :comments }
+
+  describe 'fields' do
+    it 'should have a valid set of fetchable fields' do
+      expect(subject.fetchable_fields).to match_array %i[id content_item_id user deck comment_type text comments]
+    end
+
+    it 'should have a valid set of creatable fields' do
+      expect(described_class.creatable_fields).to match_array %i[content_item_id user deck comment_type text]
+    end
+
+    it 'should have a valid set of updatable fields' do
+      expect(described_class.updatable_fields).to be_empty
+    end
+
+    it 'should have a valid set of sortable fields' do
+      expect(described_class.sortable_fields context).to match_array %i[id content_item_id comment_type text]
+    end
+  end
+
+  describe 'filters' do
+    it 'should have a valid set of filters' do
+      expect(described_class.filters.keys).to match_array %i[id user content_item_id comment_type]
+    end
+  end
+end
