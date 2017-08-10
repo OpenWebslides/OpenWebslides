2da81471314951a4299f0b8b00c04de56b1c7324
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-10 10:47:44 +0200

    Validate Comment scope

---
+++ spec/factories/annotations/annotation_factory.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :annotation, :class => Annotations::Annotation do
+    content_item_id { Faker::Number.number 2 }
+    user { build :user }
+    deck { build :deck }
+
+    factory :conversation, :class => Annotations::Conversation do
+      comment_type { %i[question note].sample }
+      text { Faker::Lorem.sentence(3) }
+    end
+
+    factory :comment, :class => Annotations::Comment do
+      text { Faker::Lorem.sentence(3) }
+
+      conversation { build :conversation, :deck => deck }
+    end
+  end
+end
