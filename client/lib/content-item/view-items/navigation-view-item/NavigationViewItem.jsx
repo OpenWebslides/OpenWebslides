849054655f25e28f2596d53432b2424e43e7d3bd
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-16 15:27:36 +0200

    Adds navigation view item (to be used in navigation pane)

---
+++ client/lib/content-item/view-items/navigation-view-item/NavigationViewItem.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { getHTMLStringFromInlinePropertiesAndText } from 'lib/content-editable/inlineProperties';
+import { contentItemTypes } from 'constants/contentItemTypes';
+
+import contentItemFactories from '../../content-item-factories/components';
+import wrapperFactories from '../../content-item-factories/containers';
+
+import renderChildrenHOC from '../../hocs/renderChildrenHOC';
+import contentItemHOC from '../../hocs/contentItemHOC';
+
+import generateAttributesObject from './helpers/generateAttributesObject';
+
+
+function NavigationViewItem(props) {
+  const { headingLevel, contentItem, contentItem: { contentItemType, childItemIds, ordered } } = props;
+  const attributes = generateAttributesObject(contentItem);
+
+  if (Object.keys(wrapperFactories).includes(contentItemType)) {
+    const ContentItemWrapper = wrapperFactories[contentItemType];
+
+    const ChildComponents = renderChildrenHOC({ childItemIds, headingLevel, ordered })(NavigationViewItem);
+
+    return (
+      <ContentItemWrapper attributes={attributes}>
+        <ChildComponents />
+      </ContentItemWrapper>);
+  }
+
+
+  if (Object.keys(contentItemFactories).includes(contentItemType)) {
+    const ContentItemComponent = contentItemFactories[contentItemType];
+
+    switch (contentItemType) {
+      case contentItemTypes.ILLUSTRATIVE_IMAGE:
+      case contentItemTypes.DECORATIVE_IMAGE:
+        return <ContentItemComponent contentItem={contentItem} attributes={attributes} />;
+
+      case contentItemTypes.IFRAME:
+        return <p>IFRAME PLACEHOLDER</p>;
+
+      default: {
+        const { inlineProperties, text } = contentItem;
+
+        return (
+          <ContentItemComponent
+            contentItem={contentItem}
+            attributes={attributes}
+            headingLevel={headingLevel}
+          >
+            {getHTMLStringFromInlinePropertiesAndText(inlineProperties, text)}
+          </ContentItemComponent>
+        );
+      }
+
+    }
+  }
+  return <h3>Loading...</h3>;
+}
+
+
+NavigationViewItem.propTypes = {
+  contentItem: PropTypes.objectOf(Object).isRequired,
+  headingLevel: PropTypes.number.isRequired,
+
+};
+
+NavigationViewItem.defaultProps = {
+  hasInlineProperties: false,
+  textPropName: '',
+};
+
+export default contentItemHOC(NavigationViewItem);
