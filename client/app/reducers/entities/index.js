cb437156e5e38505a6372748481b5b85359e10a9
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-18 14:22:25 +0200

    Adds fetching of conversations from server

--- client/app/reducers/entities/index.js
+++ client/app/reducers/entities/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import contentItems from 'reducers/entities/content-items';
 import decks from 'reducers/entities/decks';
 import slides from 'reducers/entities/slides';
+import conversations from 'reducers/entities/conversations';
 
 const entities = combineReducers({
   decks,
   slides,
   contentItems,
+  conversations,
 });
 
 export default entities;
---
+++ client/app/selectors/app/annotations/index.js
@@ -0,0 +1,7 @@
+export const getActiveDeckId = (state) => {
+  return state.app.presentationView.activeDeckId;
+};
+
+export const getActiveSlideId = (state) => {
+  return state.app.presentationView.activeSlideId;
+};
