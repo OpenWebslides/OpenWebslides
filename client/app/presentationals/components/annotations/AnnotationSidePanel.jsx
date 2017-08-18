69ad29707c2aa114dfb10a8975b3cd90ff6d46c3
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-18 13:42:00 +0200

    Adds first annotation component

---
+++ client/app/presentationals/components/annotations/AnnotationSidePanel.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class AnnotationSidePanel extends Component {
+
+  componentDidMount() {
+    this.panel.style.width = '400px';
+  }
+
+  render() {
+    return (
+      <div
+        className="side-nav"
+        ref={(panel) => {
+          this.panel = panel;
+        }}
+      >
+        <a href="#" className="close-btn" onClick={() => this.props.closeAnnotationMode()}>&times;</a>
+
+        <h3>Conversations for current slide</h3>
+      </div>
+    );
+  }
+}
+
+AnnotationSidePanel.propTypes = {
+  closeAnnotationMode: PropTypes.func.isRequired,
+};
+
