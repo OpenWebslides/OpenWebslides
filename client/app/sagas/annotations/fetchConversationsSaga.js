eed4c80f9fe4c43377bf9eb5d737daae8e5dc3f3
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-21 13:48:45 +0200

    Adds temporary mocking of conversation fetching

--- client/app/sagas/annotations/fetchConversationsSaga.js
+++ client/app/sagas/annotations/fetchConversationsSaga.js
@@ -11,21 +11,46 @@ import { getActiveDeckId } from 'selectors/app/annotations';
 
 import fetchConversationsApi from 'api/fetchConversationsApi';
 
+const mockConversations = {
+  1: {
+    id: 1,
+    contentItemId: '4',
+    conversationType: 'question',
+    text: 'Is this the only way to do this?',
+    user: { firstName: 'Rein', lastName: 'Van Imschoot' },
+  },
+  2: {
+    id: 2,
+    contentItemId: '4',
+    conversationType: 'note',
+    text: 'This is such a great lecture',
+    user: { firstName: 'Florian', lastName: 'De Jonckheere' },
+  },
+  3: {
+    id: 3,
+    contentItemId: '4',
+    conversationType: 'note',
+    text: 'I found a better solution',
+    user: { firstName: 'Esther', lastName: 'De Loof' },
+  },
+};
+
 export function* doFetchConversations() {
   try {
-    const deckId = yield select(getActiveDeckId);
+    // const deckId = yield select(getActiveDeckId);
 
-    const conversations = yield call(fetchConversationsApi, deckId);
+    // const conversations = yield call(fetchConversationsApi, deckId);
+    // const payload = {};
 
-    const payload = {};
+    // conversations.data.forEach((conversation) => {
+    //   const { id, attributes: { contentItemId, conversationType, text }, relationships } = conversation;
+    //   const userId = relationships.user.data.id;
+    //   const userAttributes = conversations.included.find(user => user.id === userId).attributes;
 
-    conversations.data.forEach((conversation) => {
-      const { id, attributes: { contentItemId, conversationType, text } } = conversation;
+    //   payload[id] = { id, contentItemId, conversationType, text, user: { ...userAttributes } };
+    // });
 
-      payload[id] = { id, contentItemId, conversationType, text };
-    });
-
-    yield put({ type: FETCH_CONVERSATIONS_SUCCESS, payload });
+    yield put({ type: FETCH_CONVERSATIONS_SUCCESS, payload: mockConversations });
   }
   catch (error) {
     yield put({ type: FETCH_CONVERSATIONS_FAILURE });
