f241412e2a12766cf037911a9aad18457e8f8fb3
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-21 09:57:10 +0200

    CommentService: don't inherit from ConversationService

--- app/services/comment_service.rb
+++ app/services/comment_service.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
-class CommentService < ConversationService; end
+class CommentService < AnnotationService
+  def delete
+    # Hide annotation text
+    @annotation.text = I18n.t 'openwebslides.annotations.hidden'
+
+    super
+  end
+end
--- spec/services/comment_service_spec.rb
+++ spec/services/comment_service_spec.rb
@@ -3,7 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe CommentService do
-  let(:annotation) { create :comment }
+  let(:annotation) { create :comment, :text => 'foo' }
 
   let(:subject) { described_class.new annotation }
+
+  describe 'delete' do
+    describe 'public' do
+      it 'is censored if public' do
+        expect(annotation).to receive :hide
+        expect(annotation.text).to eq 'foo'
+
+        subject.delete
+
+        expect(annotation.text).to eq I18n.t 'openwebslides.annotations.hidden'
+      end
+    end
+
+    describe 'public' do
+      before { annotation.protect }
+
+      it 'is destroyed if private' do
+        expect(annotation).to receive :destroy
+        subject.delete
+      end
+    end
+  end
 end
