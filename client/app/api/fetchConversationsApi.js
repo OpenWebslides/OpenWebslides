24b379198ec7715462b357ca65e62c9c1a292dbb
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-21 13:46:52 +0200

    Adds user info request to api call

--- client/app/api/fetchConversationsApi.js
+++ client/app/api/fetchConversationsApi.js
@@ -3,7 +3,7 @@ import ApiRequest from './helpers/apiHelper';
 async function fetchConversations(deckId) {
   const request = new ApiRequest();
 
-  request.setMethod('GET').setEndpoint(`decks/${deckId}/conversations`);
+  request.setMethod('GET').setEndpoint(`decks/${deckId}/conversations?include=user`);
 
   const response = await request.executeRequest();
 
