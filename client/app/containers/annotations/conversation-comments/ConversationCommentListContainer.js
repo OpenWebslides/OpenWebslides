06aad4511399581451edc45b068101f678c07ee7
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 16:04:48 +0200

    Adds first draft for conversation comment inline editing

--- client/app/containers/annotations/conversation-comments/ConversationCommentListContainer.js
+++ client/app/containers/annotations/conversation-comments/ConversationCommentListContainer.js
@@ -3,8 +3,10 @@ import { bindActionCreators } from 'redux';
 
 import ConversationCommentList from 'presentationals/components/annotations/conversation-comments/ConversationCommentList';
 import { getConversationById } from 'selectors/entities/conversations';
+import { getEditableConversationCommentId } from 'selectors/app/annotations';
 
 import { fetchConversationComments, deleteConversationComment } from 'actions/entities/conversation-comments';
+import { setEditableConversationComment } from 'actions/app/annotations';
 
 function mapStateToProps(state) {
   const activeConversationId = state.app.annotations.activeConversationId;
@@ -13,11 +15,12 @@ function mapStateToProps(state) {
     activeConversationId: state.app.annotations.activeConversationId,
     conversationComments: state.entities.conversationComments.byId,
     activeConversation: getConversationById(state, activeConversationId),
+    editableConversationCommentId: getEditableConversationCommentId(state),
   };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchConversationComments, deleteConversationComment }, dispatch);
+  return bindActionCreators({ fetchConversationComments, deleteConversationComment, setEditableConversationComment }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ConversationCommentList);
