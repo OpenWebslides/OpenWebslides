a76f6439693aebb1aec24fcfdc0a612bdba8e717
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 14:30:52 +0200

    Adds sagas for fetching and creating of conversations and comments, leaves mocks for easy edge case testing

--- client/app/sagas/annotations/fetchConversationsSaga.js
+++ client/app/sagas/annotations/fetchConversationsSaga.js
@@ -7,7 +7,7 @@ import {
 } from 'actions/entities/conversations';
 
 import { getActiveDeckId } from 'selectors/app/annotations';
-
+import { getCurrentUserId } from 'selectors/app/auth';
 
 import fetchConversationsApi from 'api/fetchConversationsApi';
 
@@ -16,41 +16,33 @@ const mockConversations = {
     id: 1,
     contentItemId: '4',
     conversationType: 'question',
-    text: 'Is this the only way to do this?',
-    user: { firstName: 'Rein', lastName: 'Van Imschoot' },
-  },
-  2: {
-    id: 2,
-    contentItemId: '4',
-    conversationType: 'note',
-    text: 'This is such a great lecture',
-    user: { firstName: 'Florian', lastName: 'De Jonckheere' },
-  },
-  3: {
-    id: 3,
-    contentItemId: '4',
-    conversationType: 'note',
-    text: 'I found a better solution',
-    user: { firstName: 'Esther', lastName: 'De Loof' },
+    text: 'I dont understand shit of this slide! Like, who is Ruben?? What are webslides????? God, this is HURTING MY BRAAAAINNNNN',
+    user: {
+      firstName: 'Rein',
+      lastName: 'Van Imschoot',
+    },
   },
 };
 
 export function* doFetchConversations() {
   try {
-    // const deckId = yield select(getActiveDeckId);
+    const deckId = yield select(getActiveDeckId);
+    const currentUserId = yield select(getCurrentUserId);
 
-    // const conversations = yield call(fetchConversationsApi, deckId);
-    // const payload = {};
+    const conversations = yield call(fetchConversationsApi, deckId);
+    const payload = {};
 
-    // conversations.data.forEach((conversation) => {
-    //   const { id, attributes: { contentItemId, conversationType, text }, relationships } = conversation;
-    //   const userId = relationships.user.data.id;
-    //   const userAttributes = conversations.included.find(user => user.id === userId).attributes;
+    conversations.data.forEach((conversation) => {
+      const { id, attributes, relationships } = conversation;
+      const userId = relationships.user.data.id;
+      const byCurrentUser = userId === currentUserId;
 
-    //   payload[id] = { id, contentItemId, conversationType, text, user: { ...userAttributes } };
-    // });
+      const userAttributes = conversations.included.find(user => user.id === userId).attributes;
 
-    yield put({ type: FETCH_CONVERSATIONS_SUCCESS, payload: mockConversations });
+      payload[id] = { id, ...attributes, byCurrentUser, user: { ...userAttributes } };
+    });
+
+    yield put({ type: FETCH_CONVERSATIONS_SUCCESS, payload });
   }
   catch (error) {
     yield put({ type: FETCH_CONVERSATIONS_FAILURE });
