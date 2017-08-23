6b03ab8f6a83a39c00c2f556840a37f31b9822f6
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 16:04:28 +0200

    Adds action and reducer to edit conversation comments inline

--- client/app/reducers/app/annotations/index.js
+++ client/app/reducers/app/annotations/index.js
@@ -1,6 +1,11 @@
 import Immutable from 'seamless-immutable';
 
-import { OPEN_ANNOTATION_MODE, CLOSE_ANNOTATION_MODE, SET_ACTIVE_CONVERSATION_ID } from 'actions/app/annotations';
+import {
+  OPEN_ANNOTATION_MODE,
+  CLOSE_ANNOTATION_MODE,
+  SET_ACTIVE_CONVERSATION_ID,
+  SET_EDITABLE_CONVERSATION_COMMENT,
+} from 'actions/app/annotations';
 
 const initialState = Immutable({
   annotationMode: false,
@@ -15,6 +20,8 @@ export default function annotationReducer(state = initialState, action) {
       return state.merge({ annotationMode: false });
     case SET_ACTIVE_CONVERSATION_ID:
       return state.merge({ activeConversationId: action.payload.conversationId });
+    case SET_EDITABLE_CONVERSATION_COMMENT:
+      return state.merge({ editableConversationCommentId: action.payload.conversationCommentId });
     default:
       return state;
   }
