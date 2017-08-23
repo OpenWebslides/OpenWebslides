2514bec9c873f43d1eb6addfdbaf39781dd33223
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 15:15:28 +0200

    Adds conditional delete and edit buttons for comments

--- client/app/presentationals/components/annotations/conversation-comments/ConversationCommentList.jsx
+++ client/app/presentationals/components/annotations/conversation-comments/ConversationCommentList.jsx
@@ -16,6 +16,10 @@ export default class ConversationCommentsList extends Component {
     this.props.fetchConversationComments(activeConversationId);
   }
 
+  scrollToForm() {
+    this.conversationCommentForm.scrollIntoView({ behavior: 'smooth' });
+  }
+
   renderComments() {
     const { conversationComments } = this.props;
 
@@ -24,11 +28,11 @@ export default class ConversationCommentsList extends Component {
         <div>
           <ul className="list-style-none">
             { Object.keys(conversationComments).map((commentId) => {
-              const { rating, text, user, createdTimeAgo, deleted, edited, flagged, secret } = conversationComments[commentId];
+              const { rating, text, user, createdTimeAgo, byCurrentUser, deleted, edited, flagged, secret } = conversationComments[commentId];
 
               return (
                 <li>
-                  <p>{rating}</p>
+                  <span> <p>{rating} </p> {byCurrentUser && <div><button>Edit</button><button>Delete</button></div>}</span>
                   <p><strong>{user.firstName} {user.lastName} {edited ? '(Edited)' : ''} </strong> wrote:</p>
                   <p>{deleted ? '(Deleted)' : text}</p>
                   <p><em>Written <strong>{createdTimeAgo}</strong></em></p>
@@ -44,9 +48,6 @@ export default class ConversationCommentsList extends Component {
     return <h4>lalalala</h4>;
   }
 
-  scrollToForm() {
-    this.conversationCommentForm.scrollIntoView({ behavior: 'smooth' });
-  }
 
   render() {
     const { activeConversation } = this.props;
