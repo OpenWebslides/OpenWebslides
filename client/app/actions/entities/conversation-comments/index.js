f350d409124e4e240e4a01948632fab1d3713151
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 15:31:53 +0200

    Adds deleteConversationComment action and action creator

--- client/app/actions/entities/conversation-comments/index.js
+++ client/app/actions/entities/conversation-comments/index.js
@@ -3,12 +3,15 @@ export const FETCH_CONVERSATION_COMMENTS_SUCCESS = 'FETCH_CONVERSATION_COMMENTS_
 export const FETCH_CONVERSATION_COMMENTS_FAILURE = 'FETCH_CONVERSATION_COMMENTS_FAILURE';
 
 export const ADD_CONVERSATION_COMMENT = 'ADD_CONVERSATION_COMMENT';
+export const DELETE_CONVERSATION_COMMENT = 'DELETE_CONVERSATION_COMMENT';
 
 export function fetchConversationComments(conversationId) {
   return { type: FETCH_CONVERSATION_COMMENTS, meta: { conversationId } };
 }
 
 export function addConversationComment({ resolve, reject, values }) {
-  return { type: ADD_CONVERSATION_COMMENT, meta: { resolve, reject, values },
-  };
+  return { type: ADD_CONVERSATION_COMMENT, meta: { resolve, reject, values } };
+}
+export function deleteConversationComment(conversationCommentId, conversationId) {
+  return { type: DELETE_CONVERSATION_COMMENT, meta: { conversationCommentId, conversationId } };
 }
