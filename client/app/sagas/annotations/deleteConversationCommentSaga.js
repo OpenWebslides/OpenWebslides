7169bd7a3ac252cb789150ee853ad1d7214c6e6e
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 15:32:34 +0200

    Adds delete conversation comment saga and fixes naming

---
+++ client/app/sagas/annotations/deleteConversationCommentSaga.js
@@ -0,0 +1,24 @@
+import { takeLatest, call, put } from 'redux-saga/effects';
+
+import { DELETE_CONVERSATION_COMMENT, FETCH_CONVERSATION_COMMENTS } from 'actions/entities/conversation-comments';
+import deleteConversationCommentApi from 'api/deleteConversationCommentApi';
+
+
+export function* doDeleteConversationComment(action) {
+  try {
+    const { conversationCommentId, conversationId } = yield action.meta;
+
+    yield call(deleteConversationCommentApi, conversationCommentId);
+
+    yield put({ type: FETCH_CONVERSATION_COMMENTS, meta: { conversationId } });
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+function* deleteConversationCommentWatcher() {
+  yield takeLatest(DELETE_CONVERSATION_COMMENT, doDeleteConversationComment);
+}
+
+export default deleteConversationCommentWatcher;
