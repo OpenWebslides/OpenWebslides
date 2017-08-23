e92b26d467174d097647c12e863ec9b8425bb2d0
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 14:32:51 +0200

    Adds conversation comments reducer, further updaters annotations reducer

--- client/app/reducers/entities/index.js
+++ client/app/reducers/entities/index.js
@@ -4,12 +4,14 @@ import contentItems from 'reducers/entities/content-items';
 import decks from 'reducers/entities/decks';
 import slides from 'reducers/entities/slides';
 import conversations from 'reducers/entities/conversations';
+import conversationComments from 'reducers/entities/conversation-comments';
 
 const entities = combineReducers({
   decks,
   slides,
   contentItems,
   conversations,
+  conversationComments,
 });
 
 export default entities;
