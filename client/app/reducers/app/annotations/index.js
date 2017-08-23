1bd3d76e8598aadbaec8a6d0186cb289d45834e6
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 14:32:51 +0200

    Adds conversation comments reducer, further updaters annotations reducer

--- client/app/reducers/app/annotations/index.js
+++ client/app/reducers/app/annotations/index.js
@@ -1,10 +1,10 @@
 import Immutable from 'seamless-immutable';
 
-import { OPEN_ANNOTATION_MODE, CLOSE_ANNOTATION_MODE } from 'actions/app/annotations';
-
+import { OPEN_ANNOTATION_MODE, CLOSE_ANNOTATION_MODE, SET_ACTIVE_CONVERSATION_ID } from 'actions/app/annotations';
 
 const initialState = Immutable({
   annotationMode: false,
+  activeConversationId: null,
 });
 
 export default function annotationReducer(state = initialState, action) {
@@ -13,6 +13,8 @@ export default function annotationReducer(state = initialState, action) {
       return state.merge({ annotationMode: true });
     case CLOSE_ANNOTATION_MODE:
       return state.merge({ annotationMode: false });
+    case SET_ACTIVE_CONVERSATION_ID:
+      return state.merge({ activeConversationId: action.payload.conversationId });
     default:
       return state;
   }
