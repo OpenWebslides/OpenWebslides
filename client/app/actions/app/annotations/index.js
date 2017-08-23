5213b8194bb2127d8b86a01a44f83abf6f8f0150
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-23 14:33:18 +0200

    Adds actions for fetching and creating of comments/conversations

--- client/app/actions/app/annotations/index.js
+++ client/app/actions/app/annotations/index.js
@@ -1,5 +1,7 @@
 export const OPEN_ANNOTATION_MODE = 'OPEN_ANNOTATION_MODE';
 export const CLOSE_ANNOTATION_MODE = 'CLOSE_ANNOTATION_MODE';
+export const SET_ACTIVE_CONVERSATION_ID = 'SET_ACTIVE_CONVERSATION_ID';
+
 
 export function openAnnotationMode() {
   return { type: OPEN_ANNOTATION_MODE };
@@ -7,5 +9,9 @@ export function openAnnotationMode() {
 
 export function closeAnnotationMode() {
   return { type: CLOSE_ANNOTATION_MODE };
+}
+
+export function setActiveConversationId(conversationId) {
+  return { type: SET_ACTIVE_CONVERSATION_ID, payload: { conversationId } };
 }
 
