1a5b834cdbab08722389ef12375624d1f717fd24
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-24 14:24:11 +0200

    Adds a success flow for inline comment editing

--- client/app/actions/entities/conversation-comments/index.js
+++ client/app/actions/entities/conversation-comments/index.js
@@ -5,6 +5,9 @@ export const FETCH_CONVERSATION_COMMENTS_FAILURE = 'FETCH_CONVERSATION_COMMENTS_
 export const ADD_CONVERSATION_COMMENT = 'ADD_CONVERSATION_COMMENT';
 export const DELETE_CONVERSATION_COMMENT = 'DELETE_CONVERSATION_COMMENT';
 
+export const UPDATE_CONVERSATION_COMMENT = 'UPDATE_CONVERSATION_COMMENT';
+export const UPDATE_CONVERSATION_COMMENT_SUCCESS = 'UPDATE_CONVERSATION_COMMENT_SUCCESS';
+
 export function fetchConversationComments(conversationId) {
   return { type: FETCH_CONVERSATION_COMMENTS, meta: { conversationId } };
 }
@@ -12,6 +15,11 @@ export function fetchConversationComments(conversationId) {
 export function addConversationComment({ resolve, reject, values }) {
   return { type: ADD_CONVERSATION_COMMENT, meta: { resolve, reject, values } };
 }
+
 export function deleteConversationComment(conversationCommentId, conversationId) {
   return { type: DELETE_CONVERSATION_COMMENT, meta: { conversationCommentId, conversationId } };
+}
+
+export function updateConversationComment({ resolve, reject, values }) {
+  return { type: UPDATE_CONVERSATION_COMMENT, meta: { resolve, reject, values } };
 }
