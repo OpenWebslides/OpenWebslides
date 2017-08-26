9285162821cbc487c13cfc0cf161e7104b10d39b
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-26 10:39:17 +0200

    Test Comment validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
+  let(:subject) { create :comment }
+
   describe 'attributes' do
     it { is_expected.not_to allow_value(nil).for(:text) }
     it { is_expected.not_to allow_value('').for(:text) }
@@ -42,6 +44,32 @@ RSpec.describe Comment, :type => :model do
                       :content_item_id => conversation.content_item_id
 
       expect(comment).to be_valid
+    end
+
+    context 'hidden parent conversation' do
+      before { conversation.hide }
+
+      it 'is not valid' do
+        comment = build :comment,
+                        :conversation => conversation,
+                        :deck => conversation.deck,
+                        :content_item_id => conversation.content_item_id
+
+        expect(comment).not_to be_valid
+      end
+    end
+
+    context 'flagged parent conversation' do
+      before { conversation.flag }
+
+      it 'is not valid' do
+        comment = build :comment,
+                        :conversation => conversation,
+                        :deck => conversation.deck,
+                        :content_item_id => conversation.content_item_id
+
+        expect(comment).not_to be_valid
+      end
     end
   end
 end
