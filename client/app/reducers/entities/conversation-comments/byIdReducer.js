1e07eb176ab5a6e34a29fb55f18c1f3845b8f3e5
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-28 11:09:38 +0200

    Updates reducers for new features

--- client/app/reducers/entities/conversation-comments/byIdReducer.js
+++ client/app/reducers/entities/conversation-comments/byIdReducer.js
@@ -3,6 +3,7 @@ import Immutable from 'seamless-immutable';
 import {
   FETCH_CONVERSATION_COMMENTS_SUCCESS,
   UPDATE_CONVERSATION_COMMENT_SUCCESS,
+  RATE_CONVERSATION_COMMENT_SUCCESS,
 } from 'actions/entities/conversation-comments';
 
 const initialState = Immutable({});
@@ -12,6 +13,11 @@ function byIdReducer(state = initialState, action) {
 
     case FETCH_CONVERSATION_COMMENTS_SUCCESS:
       return Immutable(action.payload);
+
+    case RATE_CONVERSATION_COMMENT_SUCCESS: {
+      const { id, attributes } = action.payload;
+      return state.merge({ [id]: { ...attributes } }, { deep: true });
+    }
 
     case UPDATE_CONVERSATION_COMMENT_SUCCESS: {
       const { id, attributes } = action.payload;
