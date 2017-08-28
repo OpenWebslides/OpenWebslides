9db417ef0f02afadc6f244f14dbf76d25983a654
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-28 11:07:58 +0200

    Adds api calls for toggling the rating of conversations/comments

---
+++ client/app/api/unrateConversationApi.js
@@ -0,0 +1,21 @@
+import ApiRequest from './helpers/apiHelper';
+
+async function rateConversation(conversationId) {
+  const request = new ApiRequest();
+
+  request
+    .setMethod('DELETE')
+    .setEndpoint(`conversations/${conversationId}/rating`)
+    .setBody({
+      data: {
+        type: 'ratings',
+      },
+    });
+
+  const response = await request.executeRequest();
+  const responseBody = await response.json();
+
+  return responseBody.data;
+}
+
+export default rateConversation;
