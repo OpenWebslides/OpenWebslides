52ea2a4c1ebb3d5dd33087a14efdb2875f411647
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-28 11:06:27 +0200

    Adds actions for toggling the editing of a conversation

--- client/app/actions/app/annotations/index.js
+++ client/app/actions/app/annotations/index.js
@@ -1,6 +1,8 @@
 export const OPEN_ANNOTATION_MODE = 'OPEN_ANNOTATION_MODE';
 export const CLOSE_ANNOTATION_MODE = 'CLOSE_ANNOTATION_MODE';
 export const SET_ACTIVE_CONVERSATION_ID = 'SET_ACTIVE_CONVERSATION_ID';
+export const SET_EDITING_CONVERSATION = 'SET_EDITING_CONVERSATION';
+export const UNSET_EDITING_CONVERSATION = 'UNSET_EDITING_CONVERSATION';
 export const SET_EDITABLE_CONVERSATION_COMMENT = 'SET_EDITABLE_CONVERSATION_COMMENT';
 export const UNSET_EDITABLE_CONVERSATION_COMMENT = 'UNSET_EDITABLE_CONVERSATION_COMMENT';
 
@@ -18,5 +20,9 @@ export function setActiveConversationId(conversationId) {
 
 export function setEditableConversationComment(conversationCommentId) {
   return { type: SET_EDITABLE_CONVERSATION_COMMENT, payload: { conversationCommentId } };
+}
+
+export function setEditingConversation() {
+  return { type: SET_EDITING_CONVERSATION };
 }
 
