86480e78f894ab7eb89f4300eb72ea11e28bd90e
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-08-30 10:46:20 +0200

    Uses consistent naming for boolean isEditingConversation check

--- client/app/actions/app/annotations/index.js
+++ client/app/actions/app/annotations/index.js
@@ -1,8 +1,8 @@
 export const OPEN_ANNOTATION_MODE = 'OPEN_ANNOTATION_MODE';
 export const CLOSE_ANNOTATION_MODE = 'CLOSE_ANNOTATION_MODE';
 export const SET_ACTIVE_CONVERSATION_ID = 'SET_ACTIVE_CONVERSATION_ID';
-export const SET_EDITING_CONVERSATION = 'SET_EDITING_CONVERSATION';
-export const UNSET_EDITING_CONVERSATION = 'UNSET_EDITING_CONVERSATION';
+export const SET_IS_EDITING_CONVERSATION = 'SET_IS_EDITING_CONVERSATION';
+export const UNSET_IS_EDITING_CONVERSATION = 'UNSET_IS_EDITING_CONVERSATION';
 export const SET_EDITABLE_CONVERSATION_COMMENT = 'SET_EDITABLE_CONVERSATION_COMMENT';
 export const UNSET_EDITABLE_CONVERSATION_COMMENT = 'UNSET_EDITABLE_CONVERSATION_COMMENT';
 
@@ -26,11 +26,11 @@ export function unsetEditableConversationComment() {
   return { type: UNSET_EDITABLE_CONVERSATION_COMMENT, payload: { conversationCommentId: null } };
 }
 
-export function setEditingConversation() {
-  return { type: SET_EDITING_CONVERSATION };
+export function setIsEditingConversation() {
+  return { type: SET_IS_EDITING_CONVERSATION };
 }
 
-export function unsetEditingConversation() {
-  return { type: UNSET_EDITING_CONVERSATION };
+export function unsetIsEditingConversation() {
+  return { type: UNSET_IS_EDITING_CONVERSATION };
 }
 
