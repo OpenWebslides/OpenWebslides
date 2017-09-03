49dfeaf283d43d6ff85e1210318e75699b747db1
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-03 13:48:38 +0200

    Remove OAuth providers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  oauth_config = OpenWebslides.config.oauth2
-
   before_callback_phase do |env|
     # TODO: verify state
     query_params = Rack::Utils.parse_nested_query env['QUERY_STRING']
@@ -11,8 +9,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :cas,
            :host => 'login.ugent.be'
-
-  provider :github, oauth_config.github_id, oauth_config.github_secret, :scope => 'user:email'
-  provider :google_oauth2, oauth_config.google_id, oauth_config.google_secret
-  provider :facebook, oauth_config.facebook_id, oauth_config.facebook_secret, :scope => 'email'
 end
