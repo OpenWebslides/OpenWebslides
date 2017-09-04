42be8bc0f854482cab1a7b5f7952e2d0be419e02
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-09-04 15:44:24 +0200

    Adds authentication check to presentation page

--- client/app/presentationals/pages/PresentationViewPage.jsx
+++ client/app/presentationals/pages/PresentationViewPage.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
 import PresentationViewContainer from 'containers/presentation-view/PresentationViewContainer';
+import SigninPage from './SigninPage';
 
 function PresentationPage(props) {
-  return (
-    <PresentationViewContainer {...props} />
-  );
+  const { isAuthenticated } = props;
+
+  if (isAuthenticated) {
+    return (
+      <PresentationViewContainer {...props} />
+    );
+  }
+
+  return <SigninPage />;
 }
 
-export default PresentationPage;
+export default connect(
+  (state) => {
+    return { isAuthenticated: state.app.authentication.isAuthenticated };
+  },
+)(PresentationPage);
+
+PresentationPage.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+};
+
