b1b05cfc5c1754447e61326f43ac98e1a38e9b1b
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-09-05 11:47:29 +0200

    Adds caption to illustrative images

--- client/app/containers/slide-editor/content-item-forms/ImageUploadFormContainer.jsx
+++ client/app/containers/slide-editor/content-item-forms/ImageUploadFormContainer.jsx
@@ -1,18 +1,19 @@
-import { reduxForm } from 'redux-form';
+import { reduxForm, formValueSelector } from 'redux-form';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { uploadAsset } from 'actions/other/assetActions';
 import { updateDeck } from 'actions/entities/decks';
 
-import ImageUploadForm from 'presentationals/components/slide-editor/content-item-forms/ImageUploadForm';
+import ImageUploadForm from
+  'presentationals/components/slide-editor/content-item-forms/ImageUploadForm';
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ updateDeck }, dispatch);
 }
 
 export function validate(values) {
-  const { altText, imageFile, imageType } = values;
+  const { altText, imageFile, imageType, imageCaption } = values;
 
   const errors = {};
 
@@ -28,22 +29,33 @@ export function validate(values) {
     errors.imageType = 'Please select an image type';
   }
 
+  if (imageType === 'ILLUSTRATIVE_IMAGE' && (!imageCaption || imageCaption.trim() === '')) {
+    errors.imageCaption = 'Please add a caption for your image';
+  }
+
   return errors;
 }
 
 function validateAndSubmit(values, dispatch) {
   return new Promise((resolve, reject) => {
-    dispatch(uploadAsset({ assetType: 'IMAGE', values, resolve, reject }));
+    const assetType = values.imageType;
+    dispatch(uploadAsset({ assetType, values, resolve, reject }));
   });
 }
+
 
 const connectedForm = reduxForm({
   form: 'imageUpload',
   validate,
   onSubmit: validateAndSubmit,
   getFormState: state => state.vendor.forms,
-  initialValues: { imageType: 'ILLUSTRATIVE' },
+  initialValues: { imageType: 'ILLUSTRATIVE_IMAGE' },
 })(ImageUploadForm);
 
+const selector = formValueSelector('imageUpload', state => state.vendor.forms);
 
-export default connect(null, mapDispatchToProps)(connectedForm);
+export default connect(
+  (state) => {
+    return { imageTypeValue: selector(state, 'imageType') };
+  },
+  mapDispatchToProps)(connectedForm);
