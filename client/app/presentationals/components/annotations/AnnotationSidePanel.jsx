a35ec9b15c332849719ac59d13434b26d4c542be
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-09-08 15:14:05 +0200

    Adds control for private annotations panel

--- client/app/presentationals/components/annotations/AnnotationSidePanel.jsx
+++ client/app/presentationals/components/annotations/AnnotationSidePanel.jsx
@@ -8,6 +8,7 @@ import { getAnnotationMode } from 'selectors/app/annotations';
 import ConversationList from './conversations/ConversationList';
 import ConversationPanel from './conversations/ConversationPanel';
 import AddConversationPanel from './conversations/AddConversationPanel';
+import PrivateNotePanel from './conversations/PrivateNotePanel';
 
 
 class AnnotationSidePanel extends Component {
@@ -17,12 +18,15 @@ class AnnotationSidePanel extends Component {
     this.openAddConversationPanel = this.openAddConversationPanel.bind(this);
     this.closeAddConversationPanel = this.closeAddConversationPanel.bind(this);
     this.showConversationPanel = this.showConversationPanel.bind(this);
+    this.showPrivateNotePanel = this.showPrivateNotePanel.bind(this);
+    this.closePrivateNotePanel = this.closePrivateNotePanel.bind(this);
     this.closeConversationPanel = this.closeConversationPanel.bind(this);
 
     this.state = {
-
+      showPrivateNoteForm: false,
       showAddConversationPanel: false,
       showConversationPanel: false,
+      showPrivateNotePanel: false,
     };
   }
 
@@ -36,8 +40,17 @@ class AnnotationSidePanel extends Component {
     this.setState({ showAddConversationPanel: true });
   }
 
+  openPrivateNotePanel() {
+    this.setState({ showConversationPanel: false });
+    this.setState({ showPrivateNoteForm: true });
+  }
+
   closeAddConversationPanel() {
     this.setState({ showAddConversationPanel: false });
+  }
+
+  closePrivateNotePanel() {
+    this.setState({ showPrivateNotePanel: false });
   }
 
   closeConversationPanel() {
@@ -47,6 +60,11 @@ class AnnotationSidePanel extends Component {
   showConversationPanel(conversationId) {
     this.props.setActiveConversationId(conversationId);
     this.setState({ showConversationPanel: true });
+  }
+
+  showPrivateNotePanel() {
+    this.setState({ showConversationPanel: false });
+    this.setState({ showPrivateNotePanel: true });
   }
 
   renderContent() {
@@ -68,11 +86,20 @@ class AnnotationSidePanel extends Component {
       );
     }
 
+    if (this.state.showPrivateNotePanel) {
+      return (
+        <PrivateNotePanel
+          closePrivateNotePanel={this.closePrivateNotePanel}
+        />
+      );
+    }
+
     return (
       <div>
         <a href="#" className="close-btn" onClick={() => this.props.toggleAnnotationMode()}>&times;</a>
         <h3><strong>Conversations for current slide</strong></h3>
         <button onClick={this.openAddConversationPanel}>Add conversation</button>
+        <button onClick={this.showPrivateNotePanel}>Add private note</button>
         <ConversationList showConversationPanel={this.showConversationPanel} />
       </div>
     );
