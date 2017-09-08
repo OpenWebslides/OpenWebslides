506341a69104c7702ae7b9927de8e72250fe1e13
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-08 16:18:19 +0200

    Add Repository::Import command

---
+++ app/commands/repository/import.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Repository
+  ##
+  # Import an existing repository in the backing store
+  #
+  class Import < RepoCommand
+    def execute(repository)
+      write_lock do
+        # Create and populate local repository
+        exec Filesystem::Create
+        exec Git::Clone do |c|
+          c.repository = repository
+        end
+
+        Dir[File.join repo_path, 'assets', '*'].each do |asset|
+          @receiver.assets.create :filename => File.basename(asset)
+        end
+
+        return unless OpenWebslides.config.github.enabled
+
+        # Create and sync remote repository
+        exec Remote::Init
+        exec Remote::Sync
+      end
+    end
+  end
+end
