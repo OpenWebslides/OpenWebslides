7c5f6a85b08250e27a8e5912db6a1b6abcd635e3
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-09 15:24:45 +0200

    Fix data-id attributes on assets

--- app/commands/repository/import.rb
+++ app/commands/repository/import.rb
@@ -5,7 +5,11 @@ module Repository
   # Import an existing repository in the backing store
   #
   class Import < RepoCommand
-    def execute(repository)
+    attr_accessor :repository
+
+    def execute
+      raise ArgumentError, 'No repository specified' unless @repository
+
       write_lock do
         # Create and populate local repository
         exec Filesystem::Create
@@ -13,8 +17,31 @@ module Repository
           c.repository = repository
         end
 
+        # Create assets in database and reference them
+        assets = {}
+
         Dir[File.join repo_path, 'assets', '*'].each do |asset|
-          @receiver.assets.create :filename => File.basename(asset)
+          asset_model = @receiver.assets.create :filename => File.basename(asset)
+
+          assets[File.basename asset] = asset_model.id
+        end
+
+        # Fix the `data-id` attribute
+        doc = Nokogiri::HTML5(exec Filesystem::Read)
+        doc.css('img').each do |img|
+          filename = File.basename img.attr 'src'
+
+          img['data-id'] = assets[filename] if assets.include? filename
+        end
+
+        exec Filesystem::Render do |c|
+          c.content = doc.to_html
+        end
+
+        # Commit
+        exec Git::Commit do |c|
+          c.author = @receiver.owner
+          c.message = 'Fix asset references'
         end
 
         return unless OpenWebslides.config.github.enabled
