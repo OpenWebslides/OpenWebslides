fe3569b0d83bea7992940c9e663767b65de0127a
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-11 10:50:15 +0200

    Rewrite Asset::Find command

--- app/commands/repository/asset/find.rb
+++ app/commands/repository/asset/find.rb
@@ -7,6 +7,9 @@ module Repository
     #
     class Find < AssetCommand
       def execute
+        raise OpenWebslides::ArgumentError, 'Filename not specified' unless filename
+        raise OpenWebslides::ArgumentError, 'File does not exist' unless File.exist? asset_file
+
         asset_file
       end
     end
