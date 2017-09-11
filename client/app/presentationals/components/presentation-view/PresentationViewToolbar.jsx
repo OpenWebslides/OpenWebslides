c8d0660b53d0d4dda759088e0926fbbda966b225
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-09-11 11:31:25 +0200

    Add removing of event listener on annotation mode toggle and unmount

--- client/app/presentationals/components/presentation-view/PresentationViewToolbar.jsx
+++ client/app/presentationals/components/presentation-view/PresentationViewToolbar.jsx
@@ -3,17 +3,41 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 class PresentationToolbar extends Component {
+
+  constructor() {
+    super();
+    this.handleNavigation = this.handleNavigation.bind(this);
+  }
   componentDidMount() {
-    window.addEventListener('keydown', this.handleNavigation.bind(this));
+    console.log('ADDED UN MOUNT');
+
+    window.addEventListener('keydown', this.handleNavigation);
+  }
+
+  componentWillUpdate(nextProps) {
+    console.log(nextProps);
+
+    const { annotationMode } = nextProps;
+
+    if (!annotationMode) {
+      console.log('ADDED');
+
+      window.addEventListener('keydown', this.handleNavigation);
+    }
+    else {
+      console.log('REMOVED');
+
+      window.removeEventListener('keydown', this.handleNavigation);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('UNMOUNT');
+
+    window.removeEventListener('keydown', this.handleNavigation);
   }
 
   handleNavigation(e) {
-    const { annotationMode } = this.props;
-
-    if (annotationMode) {
-      return;
-    }
-
     switch (e.keyCode) {
       case 37:
       case 34:
