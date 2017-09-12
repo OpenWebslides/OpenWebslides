d91b5c12872e40798ab8b774a4a8e60c2763a1f8
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-12 10:47:30 +0200

    Rewrite AssetsController

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -7,7 +7,6 @@ class AssetsController < ApplicationController
 
   # Authentication
   before_action :authenticate_user, :only => %i[create destroy]
-  after_action :renew_token, :except => :raw
 
   # Authorization
   after_action :verify_authorized, :except => %i[show_relationship get_related_resources]
@@ -16,64 +15,60 @@ class AssetsController < ApplicationController
 
   upload_action :create
 
-  skip_before_action :jsonapi_request_handling, :only => :raw
+  skip_before_action :jsonapi_request_handling, :only => :show
 
   # POST /assets
   def create
-    @deck = Deck.find params[:deck_id]
-    @asset = Asset.new :deck => @deck,
+    @asset = Asset.new :deck => deck,
                        :filename => File.basename(uploaded_filename)
 
     authorize @asset
 
-    if service.create :author => current_user, :file => uploaded_file
-      jsonapi_render :json => @asset, :status => :created
-    else
-      jsonapi_render_errors :json => @asset, :status => :unprocessable_entity
-    end
+    service.create :author => current_user, :file => uploaded_file
+
+    head :created
   end
 
   # GET /assets/:id
   def show
-    @asset = Asset.find params[:id]
+    @asset = Asset.new :deck => deck,
+                       :filename => params[:id]
 
     authorize @asset
 
-    jsonapi_render :json => @asset
+    begin
+      file = service.find
+    rescue OpenWebslides::FileMissingError
+      raise JSONAPI::Exceptions::RecordNotFound, params[:id]
+    end
+
+    # Send file
+    send_file file
   end
 
   # DELETE /assets/:id
   def destroy
-    @asset = Asset.find params[:id]
+    @asset = Asset.new :deck => deck,
+                       :filename => params[:id]
 
     authorize @asset
 
-    service.delete :author => current_user
+    begin
+      service.delete :author => current_user
+    rescue OpenWebslides::FileMissingError
+      raise JSONAPI::Exceptions::RecordNotFound, params[:id]
+    end
 
     head :no_content
-  end
-
-  # GET /assets/:id/raw
-  def raw
-    @asset = Asset.find params[:asset_id]
-    return head :not_found unless @asset
-
-    # Authenticate from ?token=
-    token = AssetToken.from_token params[:token]
-
-    # Set @jwt for compatibility with jwt-auth's current_user for #authorize
-    @jwt = JWT::Auth::Token.from_user token.subject
-
-    authorize @asset, :show?
-    return head :unauthorized unless token && token.valid?
-
-    # Send file
-    send_file service.find
   end
 
   protected
 
   def service
     AssetService.new @asset
+  end
+
+  def deck
+    @deck ||= Deck.find params[:deck_id]
   end
 end
