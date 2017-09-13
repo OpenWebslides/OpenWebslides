bf88b0f6395ad4c2ebcd39f7c78ebeb3a0927440
Dromin <contact@dromin.be>
2017-09-13 10:52:04 +0200

    Fixed webpack config issue where svg images wouldn't load correctly because they'd get overwritten by the svg font loader.

--- client/config/webpack.js
+++ client/config/webpack.js
@@ -26,7 +26,7 @@ const config = {
         loader: 'file-loader?name=public/icons/[name].[ext]',
       },
       {
-        test: /\.jsx{0,1}$/,
+        test: /\.jsx?$/,
         exclude: /(node_modules)/,
         loader: require.resolve('babel-loader'),
         options: {
@@ -44,6 +44,7 @@ const config = {
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
+        exclude: [/images/], // Prevent font loader from overwriting svg files in the images folder.
         use: ['file-loader'],
       },
       {
