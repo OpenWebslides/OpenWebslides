09580cb4206a5baaff43118b1f45ea05b22e10dd
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-13 11:26:55 +0200

    Revert "UGent CAS: added back signout link"
    This reverts commit 8e088111050e94101c65e7fa3e6625e5e8dbc2f2.
    
    Revert "UGent CAS: removed links and pages to normal signin and added links for ugent CAS"
    
    This reverts commit 8caefe9339b67dfc990ae0dea00e2ff4510b3b0e.
    
    Revert "UGent CAS: disable auth APIs"
    
    This reverts commit 0f957153e31af0c9bf2f532a27187be9c0275ff4.
    
    Revert "UGent CAS: remove other OAuth2 providers"
    
    This reverts commit e4ae2eb246db258ccd6e492690ad5b76d0449c38.
    
    Revert "UGent CAS: add OAuth2 provider"
    
    This reverts commit f47cb9f7cf9190badb219a310099159fbc4175fc.
    
    Revert "UGent CAS: add user id to signin saga"
    
    This reverts commit 1c55e6156da0745373785f6e822c0afcc42930e6.
    
    Revert "Adds ugent cas redirect instead of sign in page"
    
    This reverts commit 628d0d326c2671ad42d8e6081addbfdf5b5fd320.

--- client/routes.jsx
+++ client/routes.jsx
@@ -4,6 +4,11 @@ import { Router, Route, Switch } from 'react-router-dom';
 
 // Pages
 import Dashboard from 'presentationals/pages/Dashboard';
+import SignupPage from 'presentationals/pages/SignupPage';
+import SigninPage from 'presentationals/pages/SigninPage';
+import ForgotPasswordPage from 'presentationals/pages/ForgotPasswordPage';
+import ResetPasswordPage from 'presentationals/pages/ResetPasswordPage';
+import ConfirmEmailPage from 'presentationals/pages/ConfirmEmailPage';
 import OauthCallbackPage from 'presentationals/pages/OauthCallbackPage';
 import SlideEditorPage from 'presentationals/pages/SlideEditorPage';
 import CreateDeckPage from 'presentationals/pages/CreateDeckPage';
@@ -19,9 +24,14 @@ export default (
   <Router history={history}>
     <Switch>
       <Route path="/" exact={true} component={Dashboard} />
+      <Route path="/signup" component={SignupPage} />
+      <Route path="/signin" component={SigninPage} />
       <Route path="/print/:id" component={PrintViewPage} />
       <Route path="/printOnly/:id" component={PrintViewOnlyPage} />
+      <Route path="/confirmation" component={ConfirmEmailPage} />
+      <Route path="/forgot_password" component={ForgotPasswordPage} />
       <Route path="/oauth/omniauth" component={OauthCallbackPage} />
+      <Route path="/reset_password" component={ResetPasswordPage} />
       <Route path="/editor/:deckId" component={SlideEditorPage} />
       <Route path="/presentation/:deckId" component={PresentationViewPage} />
       <Route path="/create_new_deck" component={CreateDeckPage} />
