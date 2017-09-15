6a61c74b6192fc0f3a108459ac59e4eb0e71b2b6
Luca Dorigo <dorigoluca@gmail.com>
2017-09-15 11:06:25 +0200

    Added link to print the course-mode

--- client/app/presentationals/pages/PrintViewPage.jsx
+++ client/app/presentationals/pages/PrintViewPage.jsx
@@ -10,7 +10,7 @@ function PrintViewPage(props) {
     <DefaultLayout
       cssIdentifier="print-view-container"
       components={{
-        'print-view-toolbar': <PrintViewToolbarContainer />,
+        'print-view-toolbar': <PrintViewToolbarContainer id={id} />,
         'print-view-container': <PrintViewContainer id={id} printAndClose={false} />,
       }}
     />
