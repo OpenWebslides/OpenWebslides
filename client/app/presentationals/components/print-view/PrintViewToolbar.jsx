7459d3286446606fd3e50eff4f0ab41968d09005
Luca Dorigo <dorigoluca@gmail.com>
2017-09-15 16:53:50 +0200

    Prevent default print dialog when pressing ctrl-p, redirect to /printOnly/ instead

--- client/app/presentationals/components/print-view/PrintViewToolbar.jsx
+++ client/app/presentationals/components/print-view/PrintViewToolbar.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import history from 'history';
 import { imgOptions, iframeOptions } from 'constants/printViewOptions';
 import SelectImageOptions from './SelectImageOptions';
 import SelectIframeOptions from './SelectIframeOptions';
 import SelectDecorativeImageOptions from './SelectDecorativeImageOptions';
 
-function PrintViewToolbar({ printViewState, changeImagePref, changeIframePref, changeDecorativeImagePref, id }) {
-  const curImagePref = printViewState.images;
-  const curDecorativeImagePref = printViewState.decorativeImages;
-  const curIframePref = printViewState.iframes;
-  return (
-    <div className="c_print-view-toolbar">
-      <SelectImageOptions changeImagePref={changeImagePref} selectedImagePref={curImagePref} />
-      <SelectIframeOptions changeIframePref={changeIframePref} selectedIframePref={curIframePref} />
-      <SelectDecorativeImageOptions
-        changeDecorativeImagePref={changeDecorativeImagePref}
-        selectedDecorativeImagePref={curDecorativeImagePref}
-      />
-      <Link to={`/printOnly/${id}`}> Print </Link>
-    </div>
-  );
+class PrintViewToolbar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handlePrintShortcut = this.handlePrintShortcut.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handlePrintShortcut);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handlePrintShortcut);
+  }
+
+  handlePrintShortcut(e) {
+    // debugger;
+    const id = this.props.id;
+    if (e.keyCode === 80 && (e.metaKey || e.ctrlKey)) {
+      console.log('shortcut detected');
+      e.preventDefault();
+      history.push(`/printOnly/${id}`);
+    }
+  }
+
+  render() {
+    const { printViewState, changeImagePref, changeIframePref, changeDecorativeImagePref, id } = this.props;
+    const curImagePref = printViewState.images;
+
+    const curDecorativeImagePref = printViewState.decorativeImages;
+    const curIframePref = printViewState.iframes;
+    return (
+      <div className="c_print-view-toolbar">
+        <SelectImageOptions changeImagePref={changeImagePref} selectedImagePref={curImagePref} />
+        <SelectIframeOptions changeIframePref={changeIframePref} selectedIframePref={curIframePref} />
+        <SelectDecorativeImageOptions
+          changeDecorativeImagePref={changeDecorativeImagePref}
+          selectedDecorativeImagePref={curDecorativeImagePref}
+        />
+        <Link to={`/printOnly/${id}`}> Print </Link>
+      </div>
+    );
+  }
 }
 
 PrintViewToolbar.propTypes = {
@@ -32,6 +59,7 @@ PrintViewToolbar.propTypes = {
     iframes: PropTypes.oneOf(Object.keys(iframeOptions)),
     decorativeImages: PropTypes.bool,
   }),
+  id: PropTypes.number.isRequired,
 };
 
 PrintViewToolbar.defaultProps = {
