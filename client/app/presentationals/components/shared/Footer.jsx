1ff78a52b94afaf8ea57c8b1802eb6b05a167940
Luca Dorigo <dorigoluca@gmail.com>
2017-09-15 15:51:59 +0200

    Added footer to the default layout

---
+++ client/app/presentationals/components/shared/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="c_footer">
+      <div className="c_footer__wrapper">
+        <p className="c_footer__text"> Having an issue? Have an idea?
+          <a className="c_footer__link" href="mailto:openwebslides@ugent.be"> Send Feedback / Report a problem!</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
