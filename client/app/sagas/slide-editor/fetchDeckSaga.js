089d9de15ecafdc8ece713c9b5c8cf8087555cdc
Dromin <contact@dromin.be>
2017-09-15 10:54:56 +0200

    Updated fetchDeckSaga to allow loading HTML from a local file when setting a variable to TRUE. (Loading local HTML is necessary to be able to test the parser.)

--- client/app/sagas/slide-editor/fetchDeckSaga.js
+++ client/app/sagas/slide-editor/fetchDeckSaga.js
@@ -1,4 +1,4 @@
-import { takeLatest, put , call  } from 'redux-saga/effects';
+import { takeLatest, put, call } from 'redux-saga/effects';
 import { FETCH_DECK, fetchDeckSuccess } from 'actions/entities/decks';
 import { SIGNOUT } from 'actions/signoutActions';
 
@@ -6,20 +6,31 @@ import convertToState from 'lib/convert-to-state';
 import fetchDeckHtmlApi from 'api/fetchDeckHtmlApi';
 import fetchDeckJsonApi from 'api/fetchDeckJsonApi';
 
+import { testDeckFlamesFixed } from 'assets/files/test-decks/flamesFixed';
+
 function* doFetchDeck(action) {
   try {
-    const htmlString = yield call(fetchDeckHtmlApi, action.meta.deckId);
-    const metadata = yield call(fetchDeckJsonApi, action.meta.deckId, { assets: true });
+    const debug = true; // #TODO TURN OFF!!!
 
-    const { included } = metadata;
-
+    let htmlString;
+    let metadata = {};
     const assetLinks = {};
 
-    if (included) {
-      included.forEach((asset) => {
-        const { id, filename, links: { raw } } = asset;
-        assetLinks[id] = { src: raw, filename };
-      });
+    if (debug) {
+      htmlString = testDeckFlamesFixed;
+    }
+    else {
+      htmlString = yield call(fetchDeckHtmlApi, action.meta.deckId);
+      metadata = yield call(fetchDeckJsonApi, action.meta.deckId, { assets: true });
+
+      const { included } = metadata;
+
+      if (included) {
+        included.forEach((asset) => {
+          const { id, filename, links: { raw } } = asset;
+          assetLinks[id] = { src: raw, filename };
+        });
+      }
     }
 
     const {
