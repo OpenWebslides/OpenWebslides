79fd4a99c839dcad66d1a1701e2a1b1a40d935e1
Luca Dorigo <dorigoluca@gmail.com>
2017-09-15 10:12:24 +0200

    Added a page with only the printview

--- client/app/presentationals/pages/PrintViewOnlyPage.jsx
+++ client/app/presentationals/pages/PrintViewOnlyPage.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import PrintViewContainer from 'containers/printView/PrintViewContainer';
 
 function PrintViewPage(props) {
-  const id = props.location.pathname.match(/\/print\/([0-9]+)/)[1];
-  return (<PrintViewContainer id={id} />);
+  const id = props.location.pathname.match(/\/printOnly\/([0-9]+)/)[1];
+  return (<PrintViewContainer id={id} printAndClose={true} />);
 }
 
 export default PrintViewPage;
