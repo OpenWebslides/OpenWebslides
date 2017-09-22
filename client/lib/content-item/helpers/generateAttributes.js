ef21f2387229e4745f711b9e1bb2f4d73adcbedf
Dromin <contact@dromin.be>
2017-09-22 12:08:56 +0200

    Updated updateDeckSaga to save imageContainers; reloading saved file is untested since I can't risk messing up the test decks atm (I have no quick way to restore them).

--- client/lib/content-item/helpers/generateAttributes.js
+++ client/lib/content-item/helpers/generateAttributes.js
@@ -3,13 +3,16 @@ import _ from 'lodash';
 export default function generateAttributesObject(contentItem) {
   const attributesMap = {
     viewType: 'data-view-type',
+    imageType: 'data-image-type',
   };
   const attributes = {};
 
   Object.keys(attributesMap).forEach((key) => {
-    attributes[attributesMap[key]] = _.isString(contentItem[key])
-      ? contentItem[key].toLowerCase()
-      : contentItem[key];
+    if (contentItem[key] !== undefined) {
+      attributes[attributesMap[key]] = _.isString(contentItem[key])
+        ? contentItem[key].toLowerCase()
+        : contentItem[key];
+    }
   });
 
   return attributes;
