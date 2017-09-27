feb12ee9598f690dac5f952f21c3016f98876abe
Dromin <contact@dromin.be>
2017-09-27 18:42:34 +0200

    Multiple images are now displayed next to each other instead of below each other.

--- client/lib/content-item/html-wrappers/containers/ImageContainer.jsx
+++ client/lib/content-item/html-wrappers/containers/ImageContainer.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function ImageContainer({ children, attributes }) {
+export default function ImageContainer({ children, attributes, contentItem }) {
   return (
-    <div className="ows-image-container" {...attributes} >
+    <div className={`ows-image-container has-${contentItem.childItemIds.length}-children`} {...attributes} >
       {children}
     </div>
   );
 }
 
 ImageContainer.propTypes = {
+  contentItem: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    childItemIds: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
   children: PropTypes.oneOfType([PropTypes.arrayOf(Object), PropTypes.objectOf(Object)]).isRequired,
   attributes: PropTypes.objectOf(Object).isRequired,
 };
