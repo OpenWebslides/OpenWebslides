d71564003afd1db8eaa690812aa4f723770d04f8
Dromin <contact@dromin.be>
2017-10-06 20:35:56 +0200

    Refactored component imports out of slideViewTypes constants file; this solves circular dependency issues if constants/slideViewTypes is imported into another constants file.

--- client/app/presentationals/components/slide-editor/SlideViewsPane.jsx
+++ client/app/presentationals/components/slide-editor/SlideViewsPane.jsx
@@ -2,13 +2,22 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { slideViewTypes, slideViewTypesById } from 'constants/slideViewTypes';
+import { slideViewTypes } from 'constants/slideViewTypes';
 
 import SlideViewsMenuContainer
   from 'containers/slide-editor/SlideViewsMenuContainer';
 
+import SlideLiveViewContainer from 'containers/slide-editor/SlideLiveViewContainer';
+import SlideContentViewContainer from 'containers/slide-editor/SlideContentViewContainer';
+import SlideDebugViewContainer from 'containers/slide-editor/SlideDebugViewContainer';
+
+const slideViewComponents = {
+  [slideViewTypes.DEBUG]: SlideDebugViewContainer,
+  [slideViewTypes.CONTENT]: SlideContentViewContainer,
+  [slideViewTypes.LIVE]: SlideLiveViewContainer,
+};
+
 function SlideViewsPane(props) {
-  let slideViewType;
   let SlideViewComponent;
 
   return (
@@ -21,17 +30,19 @@ function SlideViewsPane(props) {
         </div>
         <div className="c_slide-views-pane__views-list">
 
-          {_.values(slideViewTypes).map((slideViewTypeId) => {
-            if (Array.indexOf(props.activeSlideViewTypes, slideViewTypeId) !== -1) {
-              slideViewType = slideViewTypesById[slideViewTypeId];
-              SlideViewComponent = slideViewType.component;
+          {_.values(slideViewTypes)
+            .filter((slideViewTypeId) => {
+              return _.indexOf(props.activeSlideViewTypes, slideViewTypeId) !== -1;
+            })
+            .map((slideViewTypeId) => {
+              SlideViewComponent = slideViewComponents[slideViewTypeId];
               return (
                 <div className="c_slide-views-pane__views-item" key={slideViewTypeId}>
                   <SlideViewComponent />
                 </div>
               );
-            }
-          })}
+            })
+          }
         </div>
       </div>
     </div>
