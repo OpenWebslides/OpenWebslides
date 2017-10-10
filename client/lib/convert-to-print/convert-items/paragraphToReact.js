ea9365090d7d19a3eb68cb5e258ba9bef05d842c
Luca Dorigo <dorigoluca@gmail.com>
2017-10-10 17:35:25 +0200

    Support inline properties (only one per paragraph for now)

--- client/lib/convert-to-print/convert-items/paragraphToReact.js
+++ client/lib/convert-to-print/convert-items/paragraphToReact.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
-function applyProperty(text, property) {
+function applyProperties(text, properties) {
+  // TODO: support more than 1 property
+  const property = properties[0];
   return React.createElement('p', null, [
-    React.createElement('span', null, text.substring(0, property.offsets.start)),
-    React.createElement(property.type, null, text.substring(property.offsets.start, property.offsets.end)),
-    React.createElement('span', null, text.substring(property.offsets.end)),
+    React.createElement('span', null, text.substring(0, property.offSets.start)),
+    React.createElement(property.type, null, text.substring(property.offSets.start, property.offSets.end)),
+    React.createElement('span', null, text.substring(property.offSets.end)),
   ]);
 }
 
 export default function paragraphObjetToReact(paragraph, level) {
-  if (!paragraph.inlineProperties) {
+  if (!paragraph.inlineProperties || paragraph.inlineProperties.length === 0) {
     return React.createElement(
       'p',
       {
-        className: 'c_print-view__iframe-description',
+        className: 'c_print-view__paragraph',
         'data-level': level,
       },
       paragraph.text,
     );
   }
-  return applyProperty(paragraph.text, paragraph.inlineProperties[0]); // TODO: support more than 1 property
+  return applyProperties(paragraph.text, paragraph.inlineProperties);
 }
