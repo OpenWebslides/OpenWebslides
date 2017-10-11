940f1999e49617053830300246f2f04089f2a3f2
Rein Van Imschoot <rein.vanimschoot@gmail.com>
2017-10-11 11:34:06 +0200

    Adds setActiveSlide action creator and reducer case

--- client/app/actions/app/presentation-view/index.js
+++ client/app/actions/app/presentation-view/index.js
@@ -3,6 +3,7 @@ export const VIEW_PREVIOUS_SLIDE = 'VIEW_PREVIOUS_SLIDE';
 export const VIEW_NEXT_SLIDE = 'VIEW_NEXT_SLIDE';
 export const VIEW_FIRST_SLIDE = 'VIEW_FIRST_SLIDE';
 export const VIEW_LAST_SLIDE = 'VIEW_LAST_SLIDE';
+export const SET_ACTIVE_SLIDE = 'SET_ACTIVE_SLIDE';
 
 export function viewPreviousSlide() {
   return {
@@ -15,14 +16,23 @@ export function viewNextSlide() {
     type: VIEW_NEXT_SLIDE,
   };
 }
+
 export function viewFirstSlide() {
   return {
     type: VIEW_FIRST_SLIDE,
   };
 }
+
 export function viewLastSlide() {
   return {
     type: VIEW_LAST_SLIDE,
+  };
+}
+
+export function setActiveSlide(slideId) {
+  return {
+    type: SET_ACTIVE_SLIDE,
+    payload: { slideId },
   };
 }
 
