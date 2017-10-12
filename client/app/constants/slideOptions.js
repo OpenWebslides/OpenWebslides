23e2979a7996e5affdafce63befa4eed65247622
Dromin <contact@dromin.be>
2017-10-12 13:25:18 +0200

    Fixed missing initialHeadingLevel for content view.

--- client/app/constants/slideOptions.js
+++ client/app/constants/slideOptions.js
@@ -4,6 +4,7 @@ import { slideViewTypes } from 'constants/slideViewTypes';
 export const initialHeadingLevels = {
   [slideViewTypes.COURSE]: 1, // #TODO make sure this is actually used
   [slideViewTypes.LIVE]: 1,
+  [slideViewTypes.CONTENT]: 1,
   [slideViewTypes.NAVIGATION]: 1,
   [slideViewTypes.PRESENTATION]: 1,
 };
