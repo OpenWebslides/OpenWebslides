9b99a8ba440fc0726b8cd738bdc63b5c131da5ec
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-10-21 13:17:50 +0200

    Extract relationship authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::API
   include JWT::Auth::Authentication
   include Pundit
 
+  include RelationshipAuthorizable
   include ErrorHandling
 
   protected
@@ -21,35 +22,6 @@ class ApplicationController < ActionController::API
   #
   def base_url
     @base_url ||= "#{request.protocol}#{request.host_with_port}/api"
-  end
-
-  ##
-  # Use Pundit authorize a relationship action
-  #
-  def authorize_relationship(record)
-    query = params[:action].gsub('relationship', params[:relationship]) + '?'
-    authorize record, query
-  end
-
-  ##
-  # Use Pundit to authorize an inverse relationship action
-  #
-  def authorize_inverse_relationship(record)
-    # Lookup the inverse association name
-    model_klass = controller_name.classify.constantize
-    inverse_name = model_klass.reflect_on_association(params[:relationship]).inverse_of.name.to_s
-    query = params[:action].gsub('relationship', inverse_name) + '?'
-    authorize record, query
-  end
-
-  ##
-  # Raises an error if authorization has not been performed, either through a policy or a policy scope
-  #
-  # Use this as an after_action on `show_relationship`, because either a policy or a policy scope
-  # gets authorized in that method, based on the relationship plurality
-  #
-  def verify_authorized_or_policy_scoped
-    raise AuthorizationNotPerformedError, self.class unless pundit_policy_authorized? || pundit_policy_scoped?
   end
 
   ##
---
+++ app/controllers/concerns/relationship_authorizable.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+##
+# Allow authorizing a relationship and its inverse
+#
+module RelationshipAuthorizable
+  extend ActiveSupport::Concern
+
+  ##
+  # Use Pundit authorize a relationship action
+  #
+  def authorize_relationship(record)
+    query = params[:action].gsub('relationship', params[:relationship]) + '?'
+    authorize record, query
+  end
+
+  ##
+  # Use Pundit to authorize an inverse relationship action
+  #
+  def authorize_inverse_relationship(record)
+    # Lookup the inverse association name
+    model_klass = controller_name.classify.constantize
+    inverse_name = model_klass.reflect_on_association(params[:relationship]).inverse_of.name.to_s
+    query = params[:action].gsub('relationship', inverse_name) + '?'
+    authorize record, query
+  end
+
+  ##
+  # Raises an error if authorization has not been performed, either through a policy or a policy scope
+  #
+  # Use this as an after_action on `show_relationship`, because either a policy or a policy scope
+  # gets authorized in that method, based on the relationship plurality
+  #
+  def verify_authorized_or_policy_scoped
+    raise AuthorizationNotPerformedError, self.class unless pundit_policy_authorized? || pundit_policy_scoped?
+  end
+end
